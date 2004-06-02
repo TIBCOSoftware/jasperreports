2e9c364211f05b384c279a2f6ce2c7fdaf645b02
Teodor Danciu <teodord@users.sourceforge.net>
2004-06-02 18:22:50 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@142 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/org/w3c/tools/codec/Base64FormatException.java
+++ jasperreports/src/org/w3c/tools/codec/Base64FormatException.java
@@ -3,21 +3,21 @@
  * Institut National de Recherche en Informatique et en Automatique, Keio University).
  * All Rights Reserved. http://www.w3.org/Consortium/Legal/
  */
-package org.w3c.tools.codec ;
+package org.w3c.tools.codec;
 
 /**
  * Exception for invalid BASE64 streams.
  */
-public class Base64FormatException extends Exception 
+public class Base64FormatException extends Exception
 {
 
 	/**
 	 * Create that kind of exception
 	 * @param msg The associated error message 
 	 */
-	public Base64FormatException(String msg) 
+	public Base64FormatException(String msg)
 	{
-		super(msg) ;
+		super(msg);
 	}
 
 }
