dd4057e6278b8393503ab72438fb831096d18e73
Teodor Danciu <teodord@users.sourceforge.net>
2004-08-29 15:16:33 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@205 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/engine/data/JRBeanCollectionDataSource.java
+++ src/net/sf/jasperreports/engine/data/JRBeanCollectionDataSource.java
@@ -71,9 +71,10 @@
  */
 package net.sf.jasperreports.engine.data;
 
-import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Iterator;
+
+import org.apache.commons.beanutils.PropertyUtils;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
@@ -140,54 +141,21 @@ public class JRBeanCollectionDataSource implements JRRewindableDataSource
 		
 		if (currentBean != null)
 		{
-			Class beanClass = currentBean.getClass();
-
-			String fieldName = jrField.getName();
-			fieldName = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-			
-			String methodName = "get" + fieldName;
-	
-			Method method = null;
-			
 			try
 			{
-				method = beanClass.getMethod(methodName, (Class[])null);
+				value = PropertyUtils.getProperty(currentBean, jrField.getName());
 			}
-			catch (NoSuchMethodException e)
+			catch (java.lang.IllegalAccessException e)
 			{
+				throw new JRException("Error retrieving field value from bean : " + jrField.getName(), e);
 			}
-			
-			if (
-				method == null 
-				&& Boolean.class.getName().equals(
-					jrField.getValueClassName()
-					)
-				)
+			catch (java.lang.reflect.InvocationTargetException e)
 			{
-				try
-				{
-					methodName = "is" + fieldName;
-					method = beanClass.getMethod(methodName, (Class[])null);
-				}
-				catch (NoSuchMethodException e)
-				{
-				}
+				throw new JRException("Error retrieving field value from bean : " + jrField.getName(), e);
 			}
-	
-			if (method == null)
+			catch (java.lang.NoSuchMethodException e)
 			{
-				throw new JRException("Property getter method not found in bean for the field : " + fieldName);
-			}
-			else
-			{
-				try
-				{
-					value = method.invoke(currentBean, (Object[])null);
-				}
-				catch (Exception e)
-				{
-					throw new JRException("Error retrieving field value from bean : " + methodName, e);
-				}
+				throw new JRException("Error retrieving field value from bean : " + jrField.getName(), e);
 			}
 		}
 
