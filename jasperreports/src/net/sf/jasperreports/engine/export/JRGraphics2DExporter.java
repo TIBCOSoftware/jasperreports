b77ea5efd2a5ed3db89725eb751e58bdf9b187b9
Teodor Danciu <teodord@users.sourceforge.net>
2004-11-16 16:15:10 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@269 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JRGraphics2DExporter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRGraphics2DExporter.java
@@ -724,12 +724,26 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 			grx.setStroke(topStroke);
 			grx.setColor(text.getBox().getTopBorderColor() == null ? text.getForecolor() : text.getBox().getTopBorderColor());
 	
-			grx.drawLine(
-				x, 
-				y, 
-				x + width - 1,
-				y
-				);
+			if (topStroke == STROKE_THIN)
+			{
+				grx.translate(-THIN_CORNER_OFFSET, -THIN_CORNER_OFFSET);
+				grx.drawLine(
+						x, 
+						y, 
+						x + width,
+						y
+						);
+				grx.translate(THIN_CORNER_OFFSET, THIN_CORNER_OFFSET);
+			}
+			else
+			{
+				grx.drawLine(
+						x, 
+						y, 
+						x + width - 1,
+						y
+						);
+			}
 		}
 
 		if (leftStroke != null)
@@ -737,12 +751,26 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 			grx.setStroke(leftStroke);
 			grx.setColor(text.getBox().getLeftBorderColor() == null ? text.getForecolor() : text.getBox().getLeftBorderColor());
 	
-			grx.drawLine(
-				x, 
-				y, 
-				x,
-				y + height - 1
-				);
+			if (leftStroke == STROKE_THIN)
+			{
+				grx.translate(-THIN_CORNER_OFFSET, -THIN_CORNER_OFFSET);
+				grx.drawLine(
+						x, 
+						y, 
+						x,
+						y + height
+						);
+				grx.translate(THIN_CORNER_OFFSET, THIN_CORNER_OFFSET);
+			}
+			else
+			{
+				grx.drawLine(
+						x, 
+						y, 
+						x,
+						y + height - 1
+						);
+			}
 		}
 
 		if (bottomStroke != null)
@@ -750,12 +778,26 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 			grx.setStroke(bottomStroke);
 			grx.setColor(text.getBox().getBottomBorderColor() == null ? text.getForecolor() : text.getBox().getBottomBorderColor());
 	
-			grx.drawLine(
-				x, 
-				y + height - 1,
-				x + width - 1,
-				y + height - 1
-				);
+			if (bottomStroke == STROKE_THIN)
+			{
+				grx.translate(-THIN_CORNER_OFFSET, THIN_CORNER_OFFSET);
+				grx.drawLine(
+						x, 
+						y + height - 1,
+						x + width,
+						y + height - 1
+						);
+				grx.translate(THIN_CORNER_OFFSET, -THIN_CORNER_OFFSET);
+			}
+			else
+			{
+				grx.drawLine(
+						x, 
+						y + height - 1,
+						x + width - 1,
+						y + height - 1
+						);
+			}
 		}
 
 		if (rightStroke != null)
@@ -763,12 +805,26 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 			grx.setStroke(rightStroke);
 			grx.setColor(text.getBox().getRightBorderColor() == null ? text.getForecolor() : text.getBox().getRightBorderColor());
 	
-			grx.drawLine(
-				x + width - 1,
-				y,
-				x + width - 1,
-				y + height - 1
-				);
+			if (rightStroke == STROKE_THIN)
+			{
+				grx.translate(THIN_CORNER_OFFSET, -THIN_CORNER_OFFSET);
+				grx.drawLine(
+						x + width - 1,
+						y,
+						x + width - 1,
+						y + height
+						);
+				grx.translate(-THIN_CORNER_OFFSET, THIN_CORNER_OFFSET);
+			}
+			else
+			{
+				grx.drawLine(
+						x + width - 1,
+						y,
+						x + width - 1,
+						y + height - 1
+						);
+			}
 		}
 
 		grx.rotate(-angle, x, y);
@@ -778,6 +834,11 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 	/**
 	 * 
 	 */
+	private static final double THIN_CORNER_OFFSET = 0.25d;
+	private static final Stroke STROKE_THIN = new BasicStroke(0.5f);
+	private static final Stroke STROKE_1_POINT = new BasicStroke(1f);
+	private static final Stroke STROKE_2_POINT = new BasicStroke(2f);
+	private static final Stroke STROKE_4_POINT = new BasicStroke(4f);
 	private static final Stroke STROKE_DOTTED = 
 		new BasicStroke(
 			1f,
@@ -787,10 +848,6 @@ public class JRGraphics2DExporter extends JRAbstractExporter
 			new float[]{5f, 3f},
 			0f
 			);
-	private static final Stroke STROKE_4_POINT = new BasicStroke(4f);
-	private static final Stroke STROKE_2_POINT = new BasicStroke(2f);
-	private static final Stroke STROKE_THIN = new BasicStroke(0.5f);
-	private static final Stroke STROKE_1_POINT = new BasicStroke(1f);
 
 	/**
 	 * 
