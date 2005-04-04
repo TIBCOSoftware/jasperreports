a91129ad749536b4dec90097eaa597ac3e42d5ba
Teodor Danciu <teodord@users.sourceforge.net>
2005-04-04 12:56:56 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@354 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/barbecue/src/net/sf/jasperreports/renderers/BarbecueRenderer.java
@@ -0,0 +1,83 @@
+/*
+ * ============================================================================
+ *                   GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 Teodor Danciu teodord@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Teodor Danciu
+ * 173, Calea Calarasilor, Bl. 42, Sc. 1, Ap. 18
+ * Postal code 030615, Sector 3
+ * Bucharest, ROMANIA
+ * Email: teodord@users.sourceforge.net
+ */
+
+/*
+ * Contributors:
+ * Adrian Jackson - iapetus@users.sourceforge.net
+ * David Taylor - exodussystems@users.sourceforge.net
+ * Lars Kristensen - llk@users.sourceforge.net
+ */
+package net.sf.jasperreports.renderers;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import net.sf.jasperreports.engine.JRAbstractSvgRenderer;
+import net.sourceforge.barbecue.Barcode;
+
+
+/**
+ * A wrapper for the Drawable interface in the JCommon library: you will need the
+ * JCommon classes in your classpath to compile this class. In particular this can be
+ * used to allow JFreeChart objects to be included in the output report in vector form.
+ *
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class BarbecueRenderer extends JRAbstractSvgRenderer
+{
+
+	/**
+	 *
+	 */	
+	private Barcode barcode = null;
+
+
+	/**
+	 *
+	 */	
+	public BarbecueRenderer(Barcode barcode) 
+	{
+		this.barcode = barcode;
+	}
+
+
+	/**
+	 *
+	 */
+	public void render(Graphics2D grx, Rectangle2D rectangle) 
+	{
+		if (barcode != null) 
+		{
+			barcode.draw(grx, (int)rectangle.getX(), (int)rectangle.getY());
+		}
+	}
+
+	
+}
