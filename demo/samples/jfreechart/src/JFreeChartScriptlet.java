7500a8097c247b2e09f6e9b36abde39db0231289
Teodor Danciu <teodord@users.sourceforge.net>
2005-04-04 11:37:09 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@351 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/jfreechart/src/JFreeChartScriptlet.java
@@ -0,0 +1,86 @@
+/*
+ * ============================================================================
+ *                   GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 Teodor Danciu teodord@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Teodor Danciu
+ * 173, Calea Calarasilor, Bl. 42, Sc. 1, Ap. 18
+ * Postal code 030615, Sector 3
+ * Bucharest, ROMANIA
+ * Email: teodord@users.sourceforge.net
+ */
+
+/*
+ * Contributors:
+ * David Gilbert - david.gilbert@object-refinery.com
+ */
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.util.Rotation;
+
+import net.sf.jasperreports.engine.JRDefaultScriptlet;
+import net.sf.jasperreports.engine.JRScriptletException;
+import net.sf.jasperreports.renderers.JCommonDrawableRenderer;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JFreeChartScriptlet extends JRDefaultScriptlet
+{
+
+
+	/**
+	 *
+	 */
+	public void afterReportInit() throws JRScriptletException
+	{
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("Java", new Double(43.2));
+		dataset.setValue("Visual Basic", new Double(10.0));
+		dataset.setValue("C/C++", new Double(17.5));
+		dataset.setValue("PHP", new Double(32.5));
+		dataset.setValue("Perl", new Double(1.0));
+
+		JFreeChart chart = 
+			ChartFactory.createPieChart3D(
+				"Pie Chart 3D Demo 1",
+				dataset,
+				true,
+				true,
+				false
+				);
+
+		PiePlot3D plot = (PiePlot3D) chart.getPlot();
+		plot.setStartAngle(290);
+		plot.setDirection(Rotation.CLOCKWISE);
+		plot.setForegroundAlpha(0.5f);
+		plot.setNoDataMessage("No data to display");
+
+		/*   */
+		this.setVariableValue("Chart", new JCommonDrawableRenderer(chart));
+	}
+
+
+}
