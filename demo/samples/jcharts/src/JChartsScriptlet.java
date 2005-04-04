4447d7f8203ffa9356454eba6547ab873320fc89
Teodor Danciu <teodord@users.sourceforge.net>
2005-04-04 14:00:30 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@359 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/jcharts/src/JChartsScriptlet.java
@@ -0,0 +1,96 @@
+/*
+ * ============================================================================
+ *                   GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 Teodor Danciu teodord@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Teodor Danciu
+ * 173, Calea Calarasilor, Bl. 42, Sc. 1, Ap. 18
+ * Postal code 030615, Sector 3
+ * Bucharest, ROMANIA
+ * Email: teodord@users.sourceforge.net
+ */
+
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.image.BufferedImage;
+
+import net.sf.jasperreports.engine.JRDefaultScriptlet;
+import net.sf.jasperreports.engine.JRScriptletException;
+
+import org.jCharts.axisChart.AxisChart;
+import org.jCharts.chartData.AxisChartDataSet;
+import org.jCharts.chartData.ChartDataException;
+import org.jCharts.chartData.DataSeries;
+import org.jCharts.properties.AreaChartProperties;
+import org.jCharts.properties.AxisProperties;
+import org.jCharts.properties.ChartProperties;
+import org.jCharts.properties.LegendProperties;
+import org.jCharts.types.ChartType;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JChartsScriptlet extends JRDefaultScriptlet 
+{
+
+
+	/**
+	 *
+	 */
+	public void afterReportInit() throws JRScriptletException 
+	{
+		try 
+		{
+			AreaChartProperties areaChartProperties = new AreaChartProperties();
+
+			double[][] data = {{10, 15, 30, 53}, {6, 30, 10, 21}, {20, 25, 20, 8}};
+			Paint[] paints = {new Color( 0, 255, 0, 100 ), new Color( 255, 0, 0, 100 ), new Color( 0, 0, 255, 100 )};
+			String[] legendLabels = {"Games", "Events", "Players" };
+			AxisChartDataSet axisChartDataSet = new AxisChartDataSet(data, legendLabels, paints, ChartType.AREA, areaChartProperties);
+
+			String[] axisLabels = {"January", "March", "May", "June"};
+			DataSeries dataSeries = new DataSeries(axisLabels, "Months", "People", "Popular Events");
+			dataSeries.addIAxisPlotDataSet(axisChartDataSet);
+
+			ChartProperties chartProperties = new ChartProperties();
+			AxisProperties axisProperties = new AxisProperties();
+			axisProperties.setYAxisRoundValuesToNearest(0);
+			LegendProperties legendProperties = new LegendProperties();
+
+			AxisChart axisChart = new AxisChart(dataSeries, chartProperties, axisProperties, legendProperties, 500, 350);
+
+			BufferedImage bufferedImage = new BufferedImage(500, 350, BufferedImage.TYPE_INT_RGB);
+
+			axisChart.setGraphics2D(bufferedImage.createGraphics());
+			axisChart.render();
+
+			super.setVariableValue("ChartImage", bufferedImage);
+		}
+		catch(ChartDataException chartDataException) 
+		{
+			throw new JRScriptletException(chartDataException);
+		}
+	}
+
+
+}
