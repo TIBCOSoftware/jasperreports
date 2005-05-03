dc39dc16687b57ff84a4d822cc4edde38f81438f
Teodor Danciu <teodord@users.sourceforge.net>
2005-05-03 09:08:34 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@382 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/view/save/JRMultipleSheetsXlsSaveContributor.java
+++ jasperreports/src/net/sf/jasperreports/view/save/JRMultipleSheetsXlsSaveContributor.java
@@ -4,7 +4,7 @@
  * ============================================================================
  *
  * JasperReports - Free Java report-generating library.
- * Copyright (C) 2001-2005 Teodor Danciu teodord@users.sourceforge.net
+ * Copyright (C) 2001-2005 JasperSoft Corporation http://www.jaspersoft.com
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,11 +20,10 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  * 
- * Teodor Danciu
- * 173, Calea Calarasilor, Bl. 42, Sc. 1, Ap. 18
- * Postal code 030615, Sector 3
- * Bucharest, ROMANIA
- * Email: teodord@users.sourceforge.net
+ * JasperSoft Corporation
+ * 185, Berry Street, Suite 6200
+ * San Francisco CA 94107
+ * http://www.jaspersoft.com
  */
 package net.sf.jasperreports.view.save;
 
