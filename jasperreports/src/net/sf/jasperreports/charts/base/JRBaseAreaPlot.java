db7cb4a508ffe286f655f5daf8e5c74d93ab8229
Flavius Sana <flavius_sana@users.sourceforge.net>
2005-06-22 09:10:51 +0000

    area chart classes & modifications added
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@472 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/charts/base/JRBaseAreaPlot.java
@@ -0,0 +1,65 @@
+/*
+ * ============================================================================
+ *                   GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 185, Berry Street, Suite 6200
+ * San Francisco CA 94107
+ * http://www.jaspersoft.com
+ */
+
+package net.sf.jasperreports.charts.base;
+
+import net.sf.jasperreports.charts.JRAreaPlot;
+import net.sf.jasperreports.engine.JRExpression;
+import net.sf.jasperreports.engine.base.JRBaseObjectFactory;
+import net.sf.jasperreports.engine.base.JRBaseChartPlot;
+
+/**
+ * @author Flavius Sana (fsana@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JRBaseAreaPlot extends JRBaseChartPlot implements JRAreaPlot {
+
+    private static final long serialVersionUID = 608;
+    
+    protected JRExpression categoryAxisLabelExpression = null;
+    protected JRExpression valueAxisLabelExpression = null;
+    
+    public JRBaseAreaPlot(){
+    }
+    
+    public JRBaseAreaPlot( JRAreaPlot areaPlot, JRBaseObjectFactory factory ){
+        super( areaPlot, factory );
+        
+        categoryAxisLabelExpression = factory.getExpression( areaPlot.getCategoryAxisLabelExpression() );
+        valueAxisLabelExpression = factory.getExpression( areaPlot.getValueAxisLabelExpression() );
+        
+    }
+    
+    public JRExpression getCategoryAxisLabelExpression(){
+        return categoryAxisLabelExpression;
+    }
+    
+    public JRExpression getValueAxisLabelExpression(){
+        return valueAxisLabelExpression;
+    }
+}
