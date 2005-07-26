f0af27f6d3946b92fe4bd0b0476e42ae525a33c0
Ionut Nedelcu <ionutned@users.sourceforge.net>
2005-07-26 09:05:47 +0000

    Plain text exporter
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@647 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRTextExporterParameter.java
@@ -0,0 +1,44 @@
+package net.sf.jasperreports.engine.export;
+
+import net.sf.jasperreports.engine.JRExporterParameter;
+
+/**
+ * Contains parameters useful for export in plain text format.
+ * <p>
+ * The text exporter can send data to a string buffer, output stream, character stream or file on disk. The engine looks
+ * among the export parameters in order to find the selected output type in this order: OUTPUT_STRING_BUFFER, OUTPUT_WRITER,
+ * OUTPUT_STREAM, OUTPUT_FILE, OUTPUT_FILE_NAME.
+ *
+ * @author Ionut Nedelcu
+ * @version $Id
+ */
+public class JRTextExporterParameter extends JRExporterParameter
+{
+	/**
+	 *
+	 */
+	public JRTextExporterParameter(String name)
+	{
+		super(name);
+	}
+
+
+	/**
+	 * An integer representing the page width in characters.
+	 */
+	public static final JRTextExporterParameter PAGE_WIDTH = new JRTextExporterParameter("Page Width");
+
+
+	/**
+	 * An integer representing the page height in characters.
+	 */
+	public static final JRTextExporterParameter PAGE_HEIGHT = new JRTextExporterParameter("Page Width");
+
+
+	/**
+	 * A string representing text that will be inserted between pages of the generated report. By default, JasperReports
+	 * separates pages by two empty lines, but this behaviour can be overriden by this parameter.
+	 */
+	public static final JRTextExporterParameter BETWEEN_PAGES_TEXT = new JRTextExporterParameter("Between Pages Text");
+
+}
