4d2ecd628ccf80b44ce5ff37958ab382e1ff8758
Ionut Nedelcu <ionutned@users.sourceforge.net>
2005-07-26 09:04:49 +0000

    Plain text export sample
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@646 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/demo/samples/text/TextApp.java
@@ -0,0 +1,192 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 185, Berry Street, Suite 6200
+ * San Francisco CA 94107
+ * http://www.jaspersoft.com
+ */
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.JRTextExporter;
+import net.sf.jasperreports.engine.export.JRTextExporterParameter;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class TextApp
+{
+
+
+	/**
+	 *
+	 */
+	private static final String TASK_COMPILE = "compile";
+	private static final String TASK_FILL = "fill";
+	private static final String TASK_PRINT = "print";
+	private static final String TASK_PDF = "pdf";
+	private static final String TASK_TEXT = "text";
+	private static final String TASK_CSV = "csv";
+
+	
+	/**
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		String fileName = null;
+		String taskName = null;
+
+		if(args.length == 0)
+		{
+			usage();
+			return;
+		}
+				
+		int k = 0;
+		while ( args.length > k )
+		{
+			if ( args[k].startsWith("-T") )
+				taskName = args[k].substring(2);
+			if ( args[k].startsWith("-F") )
+				fileName = args[k].substring(2);
+			
+			k++;	
+		}
+
+		try
+		{
+			long start = System.currentTimeMillis();
+			if (TASK_COMPILE.equals(taskName))
+			{
+				JasperCompileManager.compileReportToFile(fileName);
+				System.err.println("Compile time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
+			else if (TASK_FILL.equals(taskName))
+			{
+				//Preparing parameters
+				Map parameters = new HashMap();
+				parameters.put("ReportTitle", "Address Report");
+				parameters.put("FilterClause", "'Boston', 'Chicago', 'Oslo'");
+				parameters.put("OrderClause", "City");
+
+				JasperFillManager.fillReportToFile(fileName, parameters, getConnection());
+				System.err.println("Filling time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
+			else if (TASK_PRINT.equals(taskName))
+			{
+				JasperPrintManager.printReport(fileName, true);
+				System.err.println("Printing time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
+			else if (TASK_TEXT.equals(taskName))
+			{
+				JRTextExporter exporter = new JRTextExporter();
+				File sourceFile = new File(fileName);
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".txt");
+
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+				exporter.setParameter(JRTextExporterParameter.PAGE_WIDTH, new Integer(80));
+				exporter.setParameter(JRTextExporterParameter.PAGE_HEIGHT, new Integer(100));
+				exporter.exportReport();
+
+				System.err.println("Text creation time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
+			else if (TASK_PDF.equals(taskName))
+			{
+				JasperExportManager.exportReportToPdfFile(fileName);
+				System.err.println("PDF creation time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
+			else
+			{
+				usage();
+				System.exit(0);
+			}
+		}
+		catch (JRException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+
+	/**
+	 *
+	 */
+	private static void usage()
+	{
+		System.out.println( "QueryApp usage:" );
+		System.out.println( "\tjava QueryApp -Ttask -Ffile" );
+		System.out.println( "\tTasks : compile | fill1 | fill2 | fill3 | fill4 | print | text | xml | xmlEmbed | html | xls | csv | run" );
+	}
+
+
+	/**
+	 *
+	 */
+	private static Connection getConnection() throws ClassNotFoundException, SQLException
+	{
+		//Change these settings according to your local configuration
+		String driver = "org.hsqldb.jdbcDriver";
+		String connectString = "jdbc:hsqldb:hsql://localhost";
+		String user = "sa";
+		String password = "";
+
+
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(connectString, user, password);
+		return conn;
+	}
+
+
+}
