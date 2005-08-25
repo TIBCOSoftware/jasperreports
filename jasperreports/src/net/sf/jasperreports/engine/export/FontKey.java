9748ea92247b66834f062bb0dd9da9748fb75e55
Ionut Nedelcu <ionutned@users.sourceforge.net>
2005-08-25 08:31:57 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@776 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/engine/export/FontKey.java
@@ -0,0 +1,72 @@
+package net.sf.jasperreports.engine.export;
+
+/**
+ * @author Ionut Nedelcu (ionutned@users.sourceforge.net)
+ * @version $Id
+ */
+public class FontKey
+{
+	private String fontName;
+	private boolean isBold;
+	private boolean isItalic;
+
+	public FontKey(String fontName, boolean bold, boolean italic)
+	{
+		this.fontName = fontName;
+		isBold = bold;
+		isItalic = italic;
+	}
+
+	public String getFontName()
+	{
+		return fontName;
+	}
+
+	public void setFontName(String fontName)
+	{
+		this.fontName = fontName;
+	}
+
+	public boolean isBold()
+	{
+		return isBold;
+	}
+
+	public void setBold(boolean bold)
+	{
+		isBold = bold;
+	}
+
+	public boolean isItalic()
+	{
+		return isItalic;
+	}
+
+	public void setItalic(boolean italic)
+	{
+		isItalic = italic;
+	}
+
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		final FontKey key = (FontKey) o;
+
+		if (isBold != key.isBold) return false;
+		if (isItalic != key.isItalic) return false;
+		if (fontName != null ? !fontName.equals(key.fontName) : key.fontName != null) return false;
+
+		return true;
+	}
+
+	public int hashCode()
+	{
+		int result;
+		result = (fontName != null ? fontName.hashCode() : 0);
+		result = 29 * result + (isBold ? 1 : 0);
+		result = 29 * result + (isItalic ? 1 : 0);
+		return result;
+	}
+}
