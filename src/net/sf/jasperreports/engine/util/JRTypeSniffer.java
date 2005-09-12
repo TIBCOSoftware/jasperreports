e4c52e088abcd803734ee375a5ca757795111979
Flavius Sana <flavius_sana@users.sourceforge.net>
2005-09-12 08:57:38 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@837 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/engine/util/JRTypeSniffer.java
+++ src/net/sf/jasperreports/engine/util/JRTypeSniffer.java
@@ -1,16 +1,42 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 185, Berry Street, Suite 6200
+ * San Francisco CA 94107
+ * http://www.jaspersoft.com
+ */
 package net.sf.jasperreports.engine.util;
-
-import java.io.FileInputStream;
 
 import net.sf.jasperreports.engine.JRImage;
 
-
+/**
+ * @author Flavius Sana (flavius_sana@users.sourceforge.net)
+ * @version $Id$
+ */
 public class JRTypeSniffer
 {
 	/**
 	 * 
-	 * @param data
-	 * @return
 	 */
 	public static boolean isGIF(byte[] data) {
 		// chech if the image data length is less than 3 bytes
@@ -30,8 +56,6 @@ public class JRTypeSniffer
 	
 	/**
 	 * 
-	 * @param data
-	 * @return
 	 */
 	public static boolean isJPEG(byte[] data) {
 		// check if the image data length is less than 2 bytes
@@ -49,8 +73,6 @@ public class JRTypeSniffer
 	
 	/**
 	 * 
-	 * @param data
-	 * @return
 	 */
 	public static boolean isPNG(byte[] data) {
 		if(data.length < 8) {
@@ -73,8 +95,6 @@ public class JRTypeSniffer
 	
 	/**
 	 * 
-	 * @param data
-	 * @return
 	 */
 	public static boolean isTIFF(byte[] data) {
 		if(data.length < 2) {
@@ -91,8 +111,6 @@ public class JRTypeSniffer
 	
 	/**
 	 * 
-	 * @param data
-	 * @return
 	 */
 	public static byte getImageType(byte[] data) {
 		if (isGIF(data)) {
