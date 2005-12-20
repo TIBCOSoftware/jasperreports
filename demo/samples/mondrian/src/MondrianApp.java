61e590879d2801686f040b9a3382b702d6b4942b
Lucian Chirita <lucianc@users.sourceforge.net>
2005-12-20 18:33:58 +0000

    Mondrian query executer
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@1096 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/mondrian/src/MondrianApp.java
@@ -0,0 +1,261 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2005 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 303 Second Street, Suite 450 North
+ * San Francisco, CA 94107
+ * http://www.jaspersoft.com
+ */
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import mondrian.olap.Connection;
+import mondrian.olap.DriverManager;
+import mondrian.olap.Query;
+import mondrian.olap.Result;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRRtfExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.mondrian.JRMondrianQueryExecuterFactory;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class MondrianApp
+{
+
+
+	/**
+	 *
+	 */
+	private static final String TASK_FILL = "fill";
+	private static final String TASK_PRINT = "print";
+	private static final String TASK_PDF = "pdf";
+	private static final String TASK_XML = "xml";
+	private static final String TASK_XML_EMBED = "xmlEmbed";
+	private static final String TASK_HTML = "html";
+	private static final String TASK_RTF = "rtf";
+	private static final String TASK_XLS = "xls";
+	private static final String TASK_CSV = "csv";
+	private static final String TASK_RUN = "run";
+	
+	
+	/**
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		String fileName = null;
+		String taskName = null;
+
+		if(args.length == 0)
+		{
+			usage();
+			return;
+		}
+				
+		int k = 0;
+		while ( args.length > k )
+		{
+			if ( args[k].startsWith("-T") )
+				taskName = args[k].substring(2);
+			if ( args[k].startsWith("-F") )
+				fileName = args[k].substring(2);
+			
+			k++;	
+		}
+
+		try
+		{
+			long start = System.currentTimeMillis();
+			if (TASK_FILL.equals(taskName))
+			{
+				Connection conn = getConnection();
+				try
+				{
+					Query query = conn.parseQuery("select	{[Measures].[Quantity], [Measures].[Unit Price]} ON columns,[Address].members on rows from [Sales]");
+					Result result = conn.execute(query);
+					result.print(new PrintWriter(System.out));
+					
+					Map parameters = new HashMap();
+					parameters.put(JRMondrianQueryExecuterFactory.PARAMETER_MONDRIAN_CONNECTION, conn);
+					
+					JasperFillManager.fillReportToFile(fileName, parameters);
+					System.err.println("Filling time : " + (System.currentTimeMillis() - start));
+				}
+				finally
+				{
+					conn.close();
+				}
+			}
+			else if (TASK_PRINT.equals(taskName))
+			{
+				JasperPrintManager.printReport(fileName, true);
+				System.err.println("Printing time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_PDF.equals(taskName))
+			{
+				JasperExportManager.exportReportToPdfFile(fileName);
+				System.err.println("PDF creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_XML.equals(taskName))
+			{
+				JasperExportManager.exportReportToXmlFile(fileName, false);
+				System.err.println("XML creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_XML_EMBED.equals(taskName))
+			{
+				JasperExportManager.exportReportToXmlFile(fileName, true);
+				System.err.println("XML creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_HTML.equals(taskName))
+			{
+				JasperExportManager.exportReportToHtmlFile(fileName);
+				System.err.println("HTML creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_RTF.equals(taskName))
+			{
+				File sourceFile = new File(fileName);
+		
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+		
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".rtf");
+				
+				JRRtfExporter exporter = new JRRtfExporter();
+				
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+				
+				exporter.exportReport();
+
+				System.err.println("RTF creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_XLS.equals(taskName))
+			{
+				File sourceFile = new File(fileName);
+		
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+		
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".xls");
+				
+				JRXlsExporter exporter = new JRXlsExporter();
+				
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+				exporter.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.TRUE);
+				
+				exporter.exportReport();
+
+				System.err.println("XLS creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_CSV.equals(taskName))
+			{
+				File sourceFile = new File(fileName);
+		
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+		
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".csv");
+				
+				JRCsvExporter exporter = new JRCsvExporter();
+				
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+				
+				exporter.exportReport();
+
+				System.err.println("CSV creation time : " + (System.currentTimeMillis() - start));
+			}
+			else if (TASK_RUN.equals(taskName))
+			{
+				Connection conn = getConnection();
+				try
+				{
+					Map parameters = new HashMap();
+					parameters.put(JRMondrianQueryExecuterFactory.PARAMETER_MONDRIAN_CONNECTION, conn);
+					
+					JasperRunManager.runReportToPdfFile(fileName, parameters);
+					System.err.println("PDF running time : " + (System.currentTimeMillis() - start));
+				}
+				finally
+				{
+					conn.close();
+				}
+			}
+			else
+			{
+				usage();
+			}
+		}
+		catch (JRException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+
+	/**
+	 *
+	 */
+	private static void usage()
+	{
+		System.out.println( "MondrianApp usage:" );
+		System.out.println( "\tjava MondrianApp -Ttask -Ffile" );
+		System.out.println( "\tTasks : fill | print | pdf | xml | xmlEmbed | html | rtf | xls | csv | run" );
+	}
+
+
+	private static Connection getConnection()
+	{
+		Connection connection = 
+			DriverManager.getConnection(
+					"Provider=mondrian;" + 
+					"JdbcDrivers=org.hsqldb.jdbcDriver;" +
+					"Jdbc=jdbc:hsqldb:hsql://localhost;" +
+					"JdbcUser=sa;" +
+					"JdbcPassword=;" +
+					"Catalog=file:schema.xml;", 
+					null, false);
+		
+		return connection;
+	}
+
+}
