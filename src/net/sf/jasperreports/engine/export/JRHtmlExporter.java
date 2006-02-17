280245e2f8c11614287b910be5d7cb2fcbe1350c
Lucian Chirita <lucianc@users.sourceforge.net>
2006-02-17 11:08:36 +0000

    1430880 fix
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@1169 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/engine/export/JRHtmlExporter.java
+++ src/net/sf/jasperreports/engine/export/JRHtmlExporter.java
@@ -562,8 +562,10 @@ public class JRHtmlExporter extends JRAbstractExporter
 	{
 		if (htmlHeader == null)
 		{
-			writer.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
-			writer.write("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+			// no doctype because of bug 1430880
+//			writer.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
+//			writer.write("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+			writer.write("<html>\n");
 			writer.write("<head>\n");
 			writer.write("  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=" + encoding + "\"/>\n");
 			writer.write("  <style type=\"text/css\">\n");
