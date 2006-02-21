b181cea4d68f185aca2f4047a7959bda130e54b3
Lucian Chirita <lucianc@users.sourceforge.net>
2006-02-21 10:00:28 +0000

    Base abstract virtualizer & NPE cleanup fix
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1174 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/fill/JRFileVirtualizer.java
+++ jasperreports/src/net/sf/jasperreports/engine/fill/JRFileVirtualizer.java
@@ -33,22 +33,19 @@
  */
 package net.sf.jasperreports.engine.fill;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import net.sf.jasperreports.engine.JRConstants;
 import net.sf.jasperreports.engine.JRRuntimeException;
 import net.sf.jasperreports.engine.JRVirtualizable;
-import net.sf.jasperreports.engine.JRVirtualizer;
 
-import org.apache.commons.collections.LRUMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -61,40 +58,11 @@ import org.apache.commons.logging.LogFactory;
  * @author John Bindel
  * @version $Id$
  */
-public class JRFileVirtualizer implements JRVirtualizer {
+public class JRFileVirtualizer extends JRAbstractLRUVirtualizer {
 	
-	private static final Log log = LogFactory.getLog(JRBaseFiller.class);
-
-	/**
-	 * This class keeps track of how many objects are currently in memory, and
-	 * when there are too many, it pushes the last touched one to disk.
-	 */
-	private static class Cache extends LRUMap {
-		private static final long serialVersionUID = JRConstants.SERIAL_VERSION_UID;
-
-		private final JRFileVirtualizer virt;
-
-		Cache(int maxSize, JRFileVirtualizer virt) {
-			super(maxSize);
-			
-			this.virt = virt;
-		}
-
-		protected void processRemovedLRU(Object key, Object value)
-		{
-			virt.virtualizeData((JRVirtualizable) value);
-		}
-	}
-
-	private final Cache pagedIn;
-
-	private final HashMap pagedOut;
+	private static final Log log = LogFactory.getLog(JRFileVirtualizer.class);
 
 	private final String directory;
-
-	private JRVirtualizable lastObject;
-
-	private boolean readOnly;
 
 	/**
 	 * Uses the process's working directory as the location to store files.
@@ -116,105 +84,9 @@ public class JRFileVirtualizer implements JRVirtualizer {
 	 *            is to be stored
 	 */
 	public JRFileVirtualizer(int maxSize, String directory) {
-		this.pagedIn = new Cache(maxSize, this);
-		this.pagedOut = new HashMap();
-		this.directory = directory;
-		this.lastObject = null;
-		this.readOnly = false;
-	}
-
-	public void setReadOnly(boolean ro) {
-		this.readOnly = ro;
-	}
-
-	public boolean isReadOnly() {
-		return this.readOnly;
-	}
-
-	public void registerObject(JRVirtualizable o) {
-		Object old = pagedIn.put(o.getUID(), o);
-		if (old != null) {
-			pagedIn.put(o.getUID(), old);
-			throw new IllegalStateException("Wrong object stored with UID \""
-					+ o.getUID() + "\"");
-		}
-		this.lastObject = o;
-	}
-
-	public void deregisterObject(JRVirtualizable o) {
-		String uid = o.getUID();
-		Object old = pagedIn.remove(uid);
-		if (old != null) {
-			if (old != o) {
-				pagedIn.put(uid, old);
-				throw new IllegalStateException(
-						"Wrong object stored with UID \"" + o.getUID() + "\"");
-			}
-		} else {
-			old = pagedOut.remove(uid);
-			if (old != o) {
-				pagedIn.put(uid, old);
-				throw new IllegalStateException(
-						"Wrong object stored with UID \"" + o.getUID() + "\"");
-			}
-		}
-
-		// We don't really care if someone deregisters an object
-		// that's not registered.
-	}
-
-	public void touch(JRVirtualizable o) {
-		// If we just touched this object, don't touch it again.
-		if (this.lastObject != o) {
-			this.lastObject = (JRVirtualizable) pagedIn.get(o.getUID());
-		}
-	}
-
-	public void requestData(JRVirtualizable o) {
-		String uid = o.getUID();
-		if (pagedOut.containsKey(uid)) {
-			try {
-				// unvirtualize
-				filePageIn(o);
-			} catch (IOException ioe) {
-				throw new JRRuntimeException(ioe);
-			} catch (ClassNotFoundException cnfe) {
-				throw new JRRuntimeException(cnfe);
-			}
-
-			pagedOut.remove(uid);
-			pagedIn.put(uid, o);
-			this.lastObject = o;
-		}
-	}
-
-	public void clearData(JRVirtualizable o) {
-		String uid = o.getUID();
-		if (pagedOut.containsKey(uid)) {
-			// remove virtual data
-			fileDispose(o);
-			pagedOut.remove(uid);
-		}
-	}
-
-	public void virtualizeData(JRVirtualizable o) {
-		String uid = o.getUID();
-		if (!pagedOut.containsKey(uid)) {
-			try {
-				// virtualize
-				filePageOut(o);
-			} catch (IOException ioe) {
-				throw new JRRuntimeException(ioe);
-			}
-
-			pagedOut.put(uid, o);
-		}
-	}
-
-	protected void finalize() throws Throwable {
-		cleanup();
+		super(maxSize);
 		
-		super.finalize();
+		this.directory = directory;
 	}
 
 	private String makeFilename(JRVirtualizable o) {
@@ -222,24 +94,27 @@ public class JRFileVirtualizer implements JRVirtualizer {
 		return "virt" + uid;
 	}
 
-	private void filePageOut(JRVirtualizable o) throws IOException {
+	protected void pageOut(JRVirtualizable o) throws IOException {
 		// Store data to a file.
 		String filename = makeFilename(o);
 		File file = new File(directory, filename);
+		
 		if (file.createNewFile()) {
 			file.deleteOnExit();
 
-			ObjectOutputStream oos = null;
-			o.beforeExternalization();
+			FileOutputStream fos = null;
 			try {
-				FileOutputStream fos = new FileOutputStream(file);
-				oos = new ObjectOutputStream(fos);
-				oos.writeObject(o.getIdentityData());
-				oos.writeObject(o.getVirtualData());
-				oos.flush();
-			} finally {
-				if (oos != null) {
-					oos.close();
+				fos = new FileOutputStream(file);
+				BufferedOutputStream bufferedOut = new BufferedOutputStream(fos);
+				writeData(o, bufferedOut);
+			}
+			catch (FileNotFoundException e) {
+				log.error("Error virtualizing object", e);
+				throw new JRRuntimeException(e);
+			}
+			finally {
+				if (fos != null) {
+					fos.close();
 				}
 			}
 		} else {
@@ -249,27 +124,26 @@ public class JRFileVirtualizer implements JRVirtualizer {
 								+ "\" already exists.");
 			}
 		}
-
-		// Wait until we know it worked before tossing the data.
-		o.removeVirtualData();
 	}
 
-	private void filePageIn(JRVirtualizable o) throws IOException,
-			ClassNotFoundException {
+	protected void pageIn(JRVirtualizable o) throws IOException {
 		// Load data from a file.
 		String filename = makeFilename(o);
 		File file = new File(directory, filename);
 
-		ObjectInputStream ois = null;
+		FileInputStream fis = null;
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			ois = new ObjectInputStream(fis);
-			o.setIdentityData(ois.readObject());
-			o.setVirtualData(ois.readObject());
-			o.afterInternalization();
-		} finally {
-			if (ois != null) {
-				ois.close();
+			fis = new FileInputStream(file);
+			BufferedInputStream bufferedIn = new BufferedInputStream(fis);
+			readData(o, bufferedIn);
+		}
+		catch (FileNotFoundException e) {
+			log.error("Error devirtualizing object", e);
+			throw new JRRuntimeException(e);
+		}
+		finally {
+			if (fis != null) {
+				fis.close();
 			}
 		}
 
@@ -279,7 +153,7 @@ public class JRFileVirtualizer implements JRVirtualizer {
 		}
 	}
 
-	private void fileDispose(JRVirtualizable o) {
+	protected void dispose(JRVirtualizable o) {
 		String filename = makeFilename(o);
 		File file = new File(directory, filename);
 		file.delete();
@@ -296,7 +170,7 @@ public class JRFileVirtualizer implements JRVirtualizer {
 		for (Iterator it = pagedOut.entrySet().iterator(); it.hasNext();) {
 			Map.Entry entry = (Map.Entry) it.next();
 			try {
-				fileDispose((JRVirtualizable) entry.getValue());
+				dispose((JRVirtualizable) entry.getValue());
 				it.remove();
 			} catch (Exception e) {
 				log.error("Error cleaning up virtualizer.", e);
@@ -307,12 +181,14 @@ public class JRFileVirtualizer implements JRVirtualizer {
 		for (Iterator it = pagedIn.entrySet().iterator(); it.hasNext();) {
 			Map.Entry entry = (Map.Entry) it.next();
 			try {
-				fileDispose((JRVirtualizable) entry.getValue());
+				dispose((JRVirtualizable) entry.getValue());
 				it.remove();
 			} catch (Exception e) {
 				log.error("Error cleaning up virtualizer.", e);
 				// Do nothing because we want to try to remove all swap files.
 			}
 		}
+		
+		this.readOnly = false;
 	}
 }
