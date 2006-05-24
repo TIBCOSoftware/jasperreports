66fa13fbb55d51412babf05fc6a99e55297a23a8
Lucian Chirita <lucianc@users.sourceforge.net>
2006-05-24 10:20:54 +0000

    *** empty log message ***
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1266 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JRPdfExporter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRPdfExporter.java
@@ -430,7 +430,7 @@ public class JRPdfExporter extends JRAbstractExporter
 
 						pdfContentByte = pdfWriter.getDirectContent();
 
-						pdfContentByte.setLineCap(2);
+						pdfContentByte.setLineCap(2);//PdfContentByte.LINE_CAP_PROJECTING_SQUARE since iText 1.02b
 
 						chunk = new Chunk(" ");
 						chunk.setLocalDestination(JR_PAGE_ANCHOR_PREFIX + reportIndex + "_" + (pageIndex + 1));
