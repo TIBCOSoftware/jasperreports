fae7c5aebc0282bd678d993f1a3ef5d8b0a9d41a
Lucian Chirita <lucianc@users.sourceforge.net>
2006-07-11 11:30:50 +0000

    Documentation fix
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1332 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/query/JRJpaQueryExecuter.java
+++ jasperreports/src/net/sf/jasperreports/engine/query/JRJpaQueryExecuter.java
@@ -85,7 +85,7 @@ import org.apache.commons.logging.LogFactory;
  * &lt;property name="net.sf.jasperreports.ejbql.query.hint.fetchSize" value="100"/&gt;
  * </pre>
  * </code>
- * The name of the query hint need to be prefixed with <code>net.sf.jasperreports.ejbql.query.</code> ({@link net.sf.jasperreports.engine.query.JRJpaQueryExecuterFactory#PROPERTY_JPA_QUERY_HINT_PREFIX}).
+ * The name of the query hint need to be prefixed with <code>net.sf.jasperreports.ejbql.query.hint.</code> ({@link net.sf.jasperreports.engine.query.JRJpaQueryExecuterFactory#PROPERTY_JPA_QUERY_HINT_PREFIX}).
  * Above example will set a query hint with the name <code>fetchSize</code> and the <code>String</code> value <code>100</code>.
  * <p/>
  * Example using map:
