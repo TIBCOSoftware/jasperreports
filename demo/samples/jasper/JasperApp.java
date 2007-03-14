fd6e765b5ce7f0a9576c340629ca1e8749c7ad75
Teodor Danciu <teodord@users.sourceforge.net>
2007-03-14 09:29:42 +0000

    ODT exporter draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@1628 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- demo/samples/jasper/JasperApp.java
+++ demo/samples/jasper/JasperApp.java
@@ -34,7 +34,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import net.sf.jasperreports.engine.JRException;
@@ -49,6 +48,7 @@ import net.sf.jasperreports.engine.export.JRCsvExporter;
 import net.sf.jasperreports.engine.export.JRRtfExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.odt.JROdtExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 
 
@@ -73,6 +73,7 @@ public class JasperApp
 	private static final String TASK_XLS = "xls";
 	private static final String TASK_JXL = "jxl";
 	private static final String TASK_CSV = "csv";
+	private static final String TASK_ODT = "odt";
 	private static final String TASK_RUN = "run";
 	
 	
@@ -234,6 +235,24 @@ public class JasperApp
 				System.err.println("CSV creation time : " + (System.currentTimeMillis() - start));
 				System.exit(0);
 			}
+			else if (TASK_ODT.equals(taskName))
+			{
+				File sourceFile = new File(fileName);
+
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".odt");
+
+				JROdtExporter exporter = new JROdtExporter();
+
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+
+				exporter.exportReport();
+
+				System.err.println("ODT creation time : " + (System.currentTimeMillis() - start));
+				System.exit(0);
+			}
 			else if (TASK_RUN.equals(taskName))
 			{
 				//Preparing parameters
@@ -284,7 +303,7 @@ public class JasperApp
 	{
 		System.out.println( "JasperApp usage:" );
 		System.out.println( "\tjava JasperApp -Ttask -Ffile" );
-		System.out.println( "\tTasks : fill | print | pdf | xml | xmlEmbed | html | rtf | xls | jxl | csv | run" );
+		System.out.println( "\tTasks : fill | print | pdf | xml | xmlEmbed | html | rtf | xls | jxl | csv | odt | run" );
 	}
 
 
