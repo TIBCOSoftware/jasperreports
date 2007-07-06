acd86d1582979c6c72cc8082571620f6f8335757
Lucian Chirita <lucianc@users.sourceforge.net>
2007-07-06 14:28:26 +0000

    Customized color palette (artf2341)
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1776 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JExcelApiExporterParameter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JExcelApiExporterParameter.java
@@ -35,6 +35,9 @@
  */
 package net.sf.jasperreports.engine.export;
 
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.util.JRProperties;
+
 
 
 /**
@@ -46,5 +49,39 @@ public class JExcelApiExporterParameter extends JRXlsAbstractExporterParameter {
 	protected JExcelApiExporterParameter(String name) {
 		super(name);
 	}
+
+	/**
+	 * A boolean value specifying whether the standard color palette should be customized
+	 * so that the XLS result uses the original report colors.
+	 * <p/>
+	 * The default state of this flag is given by the
+	 * {@link #PROPERTY_CREATE_CUSTOM_PALETTE net.sf.jasperreports.export.xls.create.custom.palette} property.
+	 * <p/>
+	 * The colors used in the result XLS are determined in the following manner:
+	 * <ol>
+	 * 	<li>
+	 * If this flag is not set, the nearest color from the standard XLS palette is chosen
+	 * for a report color.
+	 * 	</li>
+	 * 	<li>
+	 * If the flag is set, the nearest not yet modified color from the palette is chosen
+	 * and modified to exactly match the report color.  If all the colors from the palette
+	 * are modified (the palette has a fixed size), the nearest color from the palette is
+	 * chosen for further report colors.
+	 * 	</li>
+	 * </ol> 
+	 * 
+	 * @see #PROPERTY_CREATE_CUSTOM_PALETTE
+	 */
+	public static final JRExporterParameter CREATE_CUSTOM_PALETTE = new JExcelApiExporterParameter("Create Custom Palette");
+
+	/**
+	 * Property whose value is used as default state of the {@link #CREATE_CUSTOM_PALETTE CREATE_CUSTOM_PALETTE} export flag.
+	 * <p/>
+	 * This property is by default not set (<code>false</code>).
+	 * 
+	 * @see JRProperties
+	 */
+	public static final String PROPERTY_CREATE_CUSTOM_PALETTE = JRProperties.PROPERTY_PREFIX + "export.xls.create.custom.palette";
 
 }
