c37ed089f5fb81f0dae305d1d90771cbaf723ef7
Teodor Danciu <teodord@users.sourceforge.net>
2007-08-22 14:04:56 +0000

    fix LGPL license
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1823 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/util/FileBufferedOutputStream.java
+++ jasperreports/src/net/sf/jasperreports/engine/util/FileBufferedOutputStream.java
@@ -1,25 +1,29 @@
 /*
- * Copyright (C) 2005 - 2007 JasperSoft Corporation.  All rights reserved.
- * http://www.jaspersoft.com.
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
  *
- * Unless you have purchased a commercial license agreement from JasperSoft,
- * the following license terms apply:
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
- *
- * This program is distributed WITHOUT ANY WARRANTY; and without the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/licenses/gpl.txt
- * or write to:
- *
- * Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330,
- * Boston, MA  USA  02111-1307
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2006 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 303 Second Street, Suite 450 North
+ * San Francisco, CA 94107
+ * http://www.jaspersoft.com
  */
 package net.sf.jasperreports.engine.util;
 
@@ -40,7 +44,7 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
- * @version $Id: FileBufferedOutputStream.java 8408 2007-05-29 23:29:12Z melih $
+ * @version $Id$
  */
 public class FileBufferedOutputStream extends OutputStream 
 {
