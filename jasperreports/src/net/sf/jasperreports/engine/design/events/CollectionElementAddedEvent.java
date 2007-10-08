39dda83d5cb0f8a59719b8f220f9e44653141973
Lucian Chirita <lucianc@users.sourceforge.net>
2007-10-08 16:32:58 +0000

    Fix 1.3 compatibility
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@1888 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/design/events/CollectionElementAddedEvent.java
+++ jasperreports/src/net/sf/jasperreports/engine/design/events/CollectionElementAddedEvent.java
@@ -27,7 +27,7 @@
  */
 package net.sf.jasperreports.engine.design.events;
 
-import java.beans.IndexedPropertyChangeEvent;
+import java.beans.PropertyChangeEvent;
 
 import net.sf.jasperreports.engine.JRConstants;
 
@@ -36,15 +36,19 @@ import net.sf.jasperreports.engine.JRConstants;
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
  * @version $Id$
  */
-public class CollectionElementAddedEvent extends IndexedPropertyChangeEvent
+public class CollectionElementAddedEvent extends PropertyChangeEvent
 {
 
 	private static final long serialVersionUID = JRConstants.SERIAL_VERSION_UID;
 
+	private final int addedIndex;
+	
 	public CollectionElementAddedEvent(Object source, String propertyName,  
 			Object addedValue, int addedIndex)
 	{
-		super(source, propertyName, null, addedValue, addedIndex);
+		super(source, propertyName, null, addedValue);
+		
+		this.addedIndex = addedIndex;
 	}
 
 	public Object getAddedValue()
@@ -54,7 +58,7 @@ public class CollectionElementAddedEvent extends IndexedPropertyChangeEvent
 	
 	public int getAddedIndex()
 	{
-		return getIndex();
+		return addedIndex;
 	}
 	
 }
