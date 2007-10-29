411f0405a7a88ade15ef49e81fd21c37c6285057
Teodor Danciu <teodord@users.sourceforge.net>
2007-10-29 15:53:56 +0000

    simplified args for sample apps
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@1931 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- demo/samples/alterdesign/AlterDesignApp.java
+++ demo/samples/alterdesign/AlterDesignApp.java
@@ -62,25 +62,14 @@ public class AlterDesignApp
 	 */
 	public static void main(String[] args)
 	{
-		String fileName = null;
-		String taskName = null;
-
 		if(args.length == 0)
 		{
 			usage();
 			return;
 		}
 				
-		int k = 0;
-		while ( args.length > k )
-		{
-			if ( args[k].startsWith("-T") )
-				taskName = args[k].substring(2);
-			if ( args[k].startsWith("-F") )
-				fileName = args[k].substring(2);
-			
-			k++;	
-		}
+		String taskName = args[0];
+		String fileName = args[1];
 
 		try
 		{
@@ -145,7 +134,7 @@ public class AlterDesignApp
 	private static void usage()
 	{
 		System.out.println( "AlterDesignApp usage:" );
-		System.out.println( "\tjava AlterDesignApp -Ttask -Ffile" );
+		System.out.println( "\tjava AlterDesignApp task file" );
 		System.out.println( "\tTasks : fill | print | pdf" );
 	}
 
--- demo/samples/batchexport/BatchExportApp.java
+++ demo/samples/batchexport/BatchExportApp.java
@@ -73,25 +73,14 @@ public class BatchExportApp
 	 */
 	public static void main(String[] args)
 	{
-		String fileName = null;
-		String taskName = null;
-
 		if(args.length == 0)
 		{
 			usage();
 			return;
 		}
 				
-		int k = 0;
-		while ( args.length > k )
-		{
-			if ( args[k].startsWith("-T") )
-				taskName = args[k].substring(2);
-			if ( args[k].startsWith("-F") )
-				fileName = args[k].substring(2);
-			
-			k++;	
-		}
+		String taskName = args[0];
+		String fileName = args[1];
 
 		try
 		{
@@ -252,7 +241,7 @@ public class BatchExportApp
 	private static void usage()
 	{
 		System.out.println( "BatchExportApp usage:" );
-		System.out.println( "\tjava BatchExportApp -Ttask -Ffile" );
+		System.out.println( "\tjava BatchExportApp task file" );
 		System.out.println( "\tTasks : fill | pdf | html | rtf | xls | jxl | csv | odt" );
 	}
 
