e76ff3033a7f97ff407f4763141b0b5bf35e147e
Lucian Chirita <lucianc@users.sourceforge.net>
2008-09-11 14:27:00 +0000

    JCharts component sample
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@2351 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/jchartscomponent/src/jcharts/AxisChartFillFactory.java
@@ -0,0 +1,56 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2006 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 303 Second Street, Suite 450 North
+ * San Francisco, CA 94107
+ * http://www.jaspersoft.com
+ */
+package jcharts;
+
+import net.sf.jasperreports.engine.component.Component;
+import net.sf.jasperreports.engine.component.ComponentFillFactory;
+import net.sf.jasperreports.engine.component.FillComponent;
+import net.sf.jasperreports.engine.fill.JRFillCloneFactory;
+import net.sf.jasperreports.engine.fill.JRFillObjectFactory;
+
+/**
+ * 
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class AxisChartFillFactory implements ComponentFillFactory
+{
+
+	public FillComponent toFillComponent(Component component,
+			JRFillObjectFactory factory)
+	{
+		return new FillAxisChart((AxisChartComponent) component, factory);
+	}
+
+	public FillComponent cloneFillComponent(FillComponent component,
+			JRFillCloneFactory factory)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+}
