7ed0a1e9f2e6a97ca4d2617fdd175c2ab360bb80
Lucian Chirita <lucianc@users.sourceforge.net>
2009-03-06 17:21:31 +0000

    Bug 3498 - escape quotes and backslashes in param/var/field names when used as String literals
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@2647 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/design/JRClassGenerator.java
+++ jasperreports/src/net/sf/jasperreports/engine/design/JRClassGenerator.java
@@ -336,7 +336,7 @@ public class JRClassGenerator
 			sb.append("        parameter_");
 			sb.append(JRStringUtil.getLiteral(parameterName));
 			sb.append(" = (JRFillParameter)pm.get(\"");
-			sb.append(parameterName);
+			sb.append(JRStringUtil.escapeJavaStringLiteral(parameterName));
 			sb.append("\");\n");
 		}
 		if(it.hasNext())
@@ -377,7 +377,7 @@ public class JRClassGenerator
 			sb.append("        field_");
 			sb.append(JRStringUtil.getLiteral(fieldName));
 			sb.append(" = (JRFillField)fm.get(\"");
-			sb.append(fieldName);
+			sb.append(JRStringUtil.escapeJavaStringLiteral(fieldName));
 			sb.append("\");\n");
 		}
 		if(it.hasNext())
@@ -418,7 +418,7 @@ public class JRClassGenerator
 			sb.append("        variable_");
 			sb.append(JRStringUtil.getLiteral(variableName));
 			sb.append(" = (JRFillVariable)vm.get(\"");
-			sb.append(variableName);
+			sb.append(JRStringUtil.escapeJavaStringLiteral(variableName));
 			sb.append("\");\n");
 		}
 		if(it.hasNext())
