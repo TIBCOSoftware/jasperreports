52b0d20837f3cb114279f601d988efd32db32214
Teodor Danciu <teodord@users.sourceforge.net>
2009-03-24 18:41:23 +0000

    fix copyright year and address
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@2697 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/engine/query/JRJpaQueryExecuterFactory.java
+++ src/net/sf/jasperreports/engine/query/JRJpaQueryExecuterFactory.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
  * 
  * JasperSoft Corporation
- * 303 Second Street, Suite 450 North
+ * 539 Bryant Street, Suite 100
  * San Francisco, CA 94107
  * http://www.jaspersoft.com
  */
