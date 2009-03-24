386b50eaf729d2b4895d52b32e8f2e7106ffde3d
Teodor Danciu <teodord@users.sourceforge.net>
2009-03-24 12:47:01 +0000

    save xml chart theme to jar
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@2683 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/simple/XmlChartThemeExtensionsRegistryFactory.java
+++ demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/simple/XmlChartThemeExtensionsRegistryFactory.java
@@ -27,14 +27,22 @@
  */
 package net.sf.jasperreports.chartthemes.simple;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import net.sf.jasperreports.chartthemes.ChartThemeMapBundle;
 import net.sf.jasperreports.engine.JRPropertiesMap;
 import net.sf.jasperreports.engine.util.JRProperties;
+import net.sf.jasperreports.extensions.DefaultExtensionsRegistry;
 import net.sf.jasperreports.extensions.ExtensionsRegistry;
 import net.sf.jasperreports.extensions.ExtensionsRegistryFactory;
 
@@ -46,9 +54,17 @@ public class XmlChartThemeExtensionsRegistryFactory implements
 		ExtensionsRegistryFactory
 {
 
+	/**
+	 * 
+	 */
 	public final static String XML_CHART_THEME_PROPERTY_PREFIX = 
 		JRProperties.PROPERTY_PREFIX + "xml.chart.theme.";
+	public final static String PROPERTY_XML_CHART_THEME_REGISTRY_FACTORY =
+		DefaultExtensionsRegistry.PROPERTY_REGISTRY_FACTORY_PREFIX + "xml.chart.themes";
 	
+	/**
+	 * 
+	 */
 	public ExtensionsRegistry createRegistry(String registryId,
 			JRPropertiesMap properties)
 	{
@@ -67,6 +83,48 @@ public class XmlChartThemeExtensionsRegistryFactory implements
 		ChartThemeMapBundle bundle = new ChartThemeMapBundle();
 		bundle.setThemes(themes);
 		return new ChartThemeBundlesExtensionsRegistry(bundle);
+	}
+
+	/**
+	 * 
+	 */
+	public static void saveToJar(ChartThemeSettings settings, String themeName, File file) throws IOException
+	{
+		FileOutputStream fos = null;
+
+		try
+		{
+			fos = new FileOutputStream(file);
+			ZipOutputStream zipos = new ZipOutputStream(fos);
+			zipos.setMethod(ZipOutputStream.DEFLATED);
+			
+			ZipEntry propsEntry = new ZipEntry("jasperreports_extension.properties");
+			zipos.putNextEntry(propsEntry);
+			Properties props = new Properties();
+			props.put(PROPERTY_XML_CHART_THEME_REGISTRY_FACTORY, XmlChartThemeExtensionsRegistryFactory.class.getName());
+			props.put(XML_CHART_THEME_PROPERTY_PREFIX + themeName, themeName + ".jrctx");
+			props.store(zipos, null);
+
+			ZipEntry jrctxEntry = new ZipEntry(themeName + ".jrctx");
+			zipos.putNextEntry(jrctxEntry);
+			XmlChartTheme.saveSettings(settings, new OutputStreamWriter(zipos));
+
+			zipos.flush();
+			zipos.finish();
+		}
+		finally
+		{
+			if (fos != null)
+			{
+				try
+				{
+					fos.close();
+				}
+				catch (IOException e)
+				{
+				}
+			}
+		}
 	}
 
 }
