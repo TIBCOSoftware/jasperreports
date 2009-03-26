e9bd3139f5847bc555730617b3c34b6989f569a4
Teodor Danciu <teodord@users.sourceforge.net>
2009-03-26 11:48:11 +0000

    jr-3-5-0
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jr-3-5-0@2705 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/crosstabs/base/JRBaseCrosstabBucket.java
@@ -0,0 +1,97 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2009 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 539 Bryant Street, Suite 100
+ * San Francisco, CA 94107
+ * http://www.jaspersoft.com
+ */
+package net.sf.jasperreports.crosstabs.base;
+
+import java.io.Serializable;
+
+import net.sf.jasperreports.crosstabs.JRCrosstabBucket;
+import net.sf.jasperreports.crosstabs.fill.calculation.BucketDefinition;
+import net.sf.jasperreports.engine.JRConstants;
+import net.sf.jasperreports.engine.JRExpression;
+import net.sf.jasperreports.engine.JRRuntimeException;
+import net.sf.jasperreports.engine.base.JRBaseObjectFactory;
+import net.sf.jasperreports.engine.util.JRCloneUtils;
+
+/**
+ * Base read-only implementation of {@link net.sf.jasperreports.crosstabs.JRCrosstabBucket JRCrosstabBucket}.
+ * 
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JRBaseCrosstabBucket implements JRCrosstabBucket, Serializable
+{
+	private static final long serialVersionUID = JRConstants.SERIAL_VERSION_UID;
+
+	protected byte order = BucketDefinition.ORDER_ASCENDING;
+	protected JRExpression expression;
+	protected JRExpression comparatorExpression;
+
+	protected JRBaseCrosstabBucket()
+	{
+	}
+	
+	public JRBaseCrosstabBucket(JRCrosstabBucket bucket, JRBaseObjectFactory factory)
+	{
+		factory.put(bucket, this);
+		
+		this.order = bucket.getOrder();
+		this.expression = factory.getExpression(bucket.getExpression());
+		this.comparatorExpression = factory.getExpression(bucket.getComparatorExpression());
+	}
+
+	public byte getOrder()
+	{
+		return order;
+	}
+
+	public JRExpression getExpression()
+	{
+		return expression;
+	}
+
+	public JRExpression getComparatorExpression()
+	{
+		return comparatorExpression;
+	}
+	
+	public Object clone()
+	{
+		try
+		{
+			JRBaseCrosstabBucket clone = (JRBaseCrosstabBucket) super.clone();
+			clone.expression = (JRExpression) JRCloneUtils.nullSafeClone(expression);
+			clone.comparatorExpression = (JRExpression) JRCloneUtils.nullSafeClone(comparatorExpression);
+			return clone;
+		}
+		catch (CloneNotSupportedException e)
+		{
+			// never
+			throw new JRRuntimeException(e);
+		}
+	}
+}
