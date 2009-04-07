e2804a508a7325c2552ced5eeda66522a6070063
Lucian Chirita <lucianc@users.sourceforge.net>
2009-04-07 15:23:17 +0000

    Escape String literals in $R{..}
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@2723 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/compilers/JRBshGenerator.java
+++ src/net/sf/jasperreports/compilers/JRBshGenerator.java
@@ -470,7 +470,7 @@ public class JRBshGenerator
 						jrParameter = (JRParameter)parametersMap.get(chunkText);
 	
 						sbuffer.append("super.evaluator.str(\"");
-						sbuffer.append(chunkText);
+						sbuffer.append(JRStringUtil.escapeJavaStringLiteral(chunkText));
 						sbuffer.append("\")");
 	
 						break;
