24501a6bf87b32cf9fc30dd9635069536e503302
Lucian Chirita <lucianc@users.sourceforge.net>
2009-05-20 09:13:50 +0000

    Bug 3954 - removed Java 1.5 dependency
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@2792 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/fill/DefaultChartTheme.java
+++ jasperreports/src/net/sf/jasperreports/engine/fill/DefaultChartTheme.java
@@ -1742,7 +1742,7 @@ public class DefaultChartTheme implements ChartTheme
 		JRValueDisplay display = jrPlot.getValueDisplay();
 		String displayVisibility = display != null && getChart().hasProperties() 
 			? getChart().getPropertiesMap().getProperty(PROPERTY_DIAL_VALUE_DISPLAY_VISIBLE) : "false";
-		if(Boolean.parseBoolean(displayVisibility))
+		if(Boolean.valueOf(displayVisibility).booleanValue())
 		{
 			DialValueIndicator dvi = new DialValueIndicator(0);
 			dvi.setBackgroundPaint(TRANSPARENT_PAINT);
