64b6119b08d5bb6f50b53333a4d2416f0c7ceca8
Sanda Zaharia <shertage@users.sourceforge.net>
2009-07-29 11:16:34 +0000

    docx exporter added to save contributors list
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@2979 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/view/save/JRDocxSaveContributor.java
@@ -0,0 +1,114 @@
+/*
+ * ============================================================================
+ * GNU Lesser General Public License
+ * ============================================================================
+ *
+ * JasperReports - Free Java report-generating library.
+ * Copyright (C) 2001-2009 JasperSoft Corporation http://www.jaspersoft.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * JasperSoft Corporation
+ * 539 Bryant Street, Suite 100
+ * San Francisco, CA 94107
+ * http://www.jaspersoft.com
+ */
+package net.sf.jasperreports.view.save;
+
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRRtfExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.view.JRSaveContributor;
+
+/**
+ * @author sanda zaharia (shertage@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JRDocxSaveContributor extends JRSaveContributor 
+{
+
+	/**
+	 * 
+	 */
+	private static final String EXTENSION_DOCX = ".docx";
+
+	/**
+	 * 
+	 */
+	public JRDocxSaveContributor(Locale locale, ResourceBundle resBundle)
+	{
+		super(locale, resBundle);
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean accept(File file)
+	{
+		if(file.isDirectory()){
+			return true;
+		}
+		return file.getName().toLowerCase().endsWith(EXTENSION_DOCX);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public String getDescription()
+	{
+		return getBundleString("file.desc.docx");
+	}
+	
+	/**
+	 * 
+	 */
+	public void save(JasperPrint jasperPrint, File file) throws JRException
+	{
+		if(!file.getName().toLowerCase().endsWith(EXTENSION_DOCX))
+		{
+			file = new File(file.getAbsolutePath() + EXTENSION_DOCX);
+		}
+		
+		if (
+			!file.exists() ||
+			JOptionPane.OK_OPTION == 
+				JOptionPane.showConfirmDialog(
+					null, 
+					MessageFormat.format(
+						getBundleString("file.exists"),
+						new Object[]{file.getName()}
+						), 
+					getBundleString("save"), 
+					JOptionPane.OK_CANCEL_OPTION
+					)
+			)
+		{
+			JRDocxExporter exporter = new JRDocxExporter();
+			exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+			exporter.setParameter(JRExporterParameter.OUTPUT_FILE, file);
+			exporter.exportReport();
+		}
+	}
+}
