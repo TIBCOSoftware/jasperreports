5251d96be33b74dc0dc973919f8f508bf379e3cd
Teodor Danciu <teodord@users.sourceforge.net>
2009-08-27 12:05:03 +0000

    LGPL v3 update
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@3035 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/olap/mapping/MappingLexer.java
+++ src/net/sf/jasperreports/olap/mapping/MappingLexer.java
@@ -13,7 +13,7 @@
  * JasperReports is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- *(at your option) any later version.
+ * (at your option) any later version.
  *
  * JasperReports is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
