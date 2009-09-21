ded0143cbc3c1afbb4ff56ad662d428541282968
Teodor Danciu <teodord@users.sourceforge.net>
2009-09-21 14:12:37 +0000

    xlsx exporter
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@3063 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/demo/samples/datasource/DataSourceApp.java
+++ jasperreports/demo/samples/datasource/DataSourceApp.java
@@ -43,6 +43,7 @@ import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 import net.sf.jasperreports.engine.export.oasis.JROdtExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 
 
@@ -72,6 +73,7 @@ public class DataSourceApp
 	private static final String TASK_CSV = "csv";
 	private static final String TASK_ODT = "odt";
 	private static final String TASK_DOCX = "docx";
+	private static final String TASK_XLSX = "xlsx";
 	private static final String TASK_XHTML = "xhtml";
 	private static final String TASK_RUN = "run";
 	
@@ -262,6 +264,23 @@ public class DataSourceApp
 
 				System.err.println("DOCX creation time : " + (System.currentTimeMillis() - start));
 			}
+			else if (TASK_XLSX.equals(taskName))
+			{
+				File sourceFile = new File(fileName);
+		
+				JasperPrint jasperPrint = (JasperPrint)JRLoader.loadObject(sourceFile);
+		
+				File destFile = new File(sourceFile.getParent(), jasperPrint.getName() + ".xlsx");
+				
+				JRXlsxExporter exporter = new JRXlsxExporter();
+				
+				exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+				exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, destFile.toString());
+				
+				exporter.exportReport();
+
+				System.err.println("XLSX creation time : " + (System.currentTimeMillis() - start));
+			}
 			else if (TASK_XHTML.equals(taskName))
 			{
 				File sourceFile = new File(fileName);
@@ -311,7 +330,7 @@ public class DataSourceApp
 	{
 		System.out.println( "DataSourceApp usage:" );
 		System.out.println( "\tjava DataSourceApp task file" );
-		System.out.println( "\tTasks : fill1 | fill2 | fill3 | fill4 | print | pdf | xml | xmlEmbed | html | rtf | xls | jxl | csv | odt | docx | xhtml | run" );
+		System.out.println( "\tTasks : fill1 | fill2 | fill3 | fill4 | print | pdf | xml | xmlEmbed | html | rtf | xls | jxl | csv | odt | docx | xlsx | xhtml | run" );
 	}
 
 
