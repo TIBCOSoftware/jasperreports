d3186a390149efc3bcdb52d6f2b2251d8cf9909c
Teodor Danciu <teodord@users.sourceforge.net>
2010-02-03 13:35:11 +0000

    sample refactoring
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@3369 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/engine/util/AbstractSampleApp.java
@@ -0,0 +1,109 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.util;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id: Barcode4JApp.java 3348 2010-02-01 12:35:35Z teodord $
+ */
+public abstract class AbstractSampleApp
+{
+
+
+	/**
+	 *
+	 */
+	public abstract String usage();
+
+	
+	/**
+	 *
+	 */
+	public void executeTask(String taskName)
+	{
+		try
+		{
+			Method method = getClass().getMethod(taskName, new Class[]{});
+			method.invoke(this, new Object[]{});
+		}
+		catch (InvocationTargetException e)
+		{
+			e.getCause().printStackTrace();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 *
+	 */
+	protected File[] getFiles(File parentFile, String extension)
+	{
+		List fileList = new ArrayList();
+		String[] files = parentFile.list();
+		for(int i = 0; i < files.length; i++)
+		{
+			String reportFile = files[i];
+			if (reportFile.endsWith("." + extension))
+			{
+				fileList.add(new File(parentFile, reportFile)); 
+			}
+		}
+		return (File[])fileList.toArray(new File[fileList.size()]);
+	}
+	
+	
+	/**
+	 *
+	 */
+	public static void main(AbstractSampleApp app, String[] args)
+	{
+		try
+		{
+			if(args.length != 1)
+			{
+				System.out.println(app.usage());
+				return;
+			}
+					
+			app.executeTask(args[0]);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+}
