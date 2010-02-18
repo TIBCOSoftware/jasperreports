c5c4e6054d9e2cef9bdf7e40cf1a851f63572040
Teodor Danciu <teodord@users.sourceforge.net>
2010-02-18 11:00:10 +0000

    support for type enum
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@3429 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/olap/mapping/MappingLexer.java
+++ src/net/sf/jasperreports/olap/mapping/MappingLexer.java
@@ -25,34 +25,25 @@
  */
 package net.sf.jasperreports.olap.mapping;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import net.sf.jasperreports.olap.mapping.*;
-
 import java.io.InputStream;
+import java.io.Reader;
+import java.util.Hashtable;
+
+import antlr.ANTLRHashString;
+import antlr.ByteBuffer;
+import antlr.CharBuffer;
+import antlr.CharStreamException;
+import antlr.CharStreamIOException;
+import antlr.InputBuffer;
+import antlr.LexerSharedInputState;
+import antlr.NoViableAltForCharException;
+import antlr.RecognitionException;
+import antlr.Token;
+import antlr.TokenStream;
 import antlr.TokenStreamException;
 import antlr.TokenStreamIOException;
 import antlr.TokenStreamRecognitionException;
-import antlr.CharStreamException;
-import antlr.CharStreamIOException;
-import antlr.ANTLRException;
-import java.io.Reader;
-import java.util.Hashtable;
-import antlr.CharScanner;
-import antlr.InputBuffer;
-import antlr.ByteBuffer;
-import antlr.CharBuffer;
-import antlr.Token;
-import antlr.CommonToken;
-import antlr.RecognitionException;
-import antlr.NoViableAltForCharException;
-import antlr.MismatchedCharException;
-import antlr.TokenStream;
-import antlr.ANTLRHashString;
-import antlr.LexerSharedInputState;
 import antlr.collections.impl.BitSet;
-import antlr.SemanticException;
 
 /**
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
