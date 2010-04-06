6e482fe2e12c0db8923e6450b1b0bf98dbbfe26b
Sanda Zaharia <shertage@users.sourceforge.net>
2010-04-06 09:30:20 +0000

    warning fix
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@3686 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/simple/LegendSettings.java
+++ demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/simple/LegendSettings.java
@@ -260,7 +260,7 @@ public class LegendSettings implements JRChangeEventsSupport, Serializable
 	/**
 	 * These fields are only for serialization backward compatibility.
 	 */
-	private int PSEUDO_SERIAL_VERSION_UID = JRConstants.PSEUDO_SERIAL_VERSION_UID;
+	private int PSEUDO_SERIAL_VERSION_UID = JRConstants.PSEUDO_SERIAL_VERSION_UID; //NOPMD
 	private Byte position = null;
 	
 	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
