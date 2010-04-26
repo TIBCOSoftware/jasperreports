58ef2dd139a27f9b0555700183119979bb522d72
Lucian Chirita <lucianc@users.sourceforge.net>
2010-04-26 11:40:09 +0000

    Fixed initialization threading bug
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@3767 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/util/JRGraphEnvInitializer.java
+++ jasperreports/src/net/sf/jasperreports/engine/util/JRGraphEnvInitializer.java
@@ -46,7 +46,7 @@ public final class JRGraphEnvInitializer
 	/**
 	 *
 	 */
-	public static void initializeGraphEnv()
+	public static synchronized void initializeGraphEnv()
 	{
 		if (AVAILABLE_FONT_FACE_NAMES == null)
 		{
