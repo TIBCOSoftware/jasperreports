11046fbfac4edc71bd7db08c9e4ff08c15844204
Sanda Zaharia <shertage@users.sourceforge.net>
2010-07-20 10:42:19 +0000

    reusing the evaluateRenderer() method
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@3907 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/components/spiderchart/SpiderChartDesignConverter.java
+++ src/net/sf/jasperreports/components/spiderchart/SpiderChartDesignConverter.java
@@ -23,36 +23,14 @@
  */
 package net.sf.jasperreports.components.spiderchart;
 
-import java.awt.BasicStroke;
-import java.awt.Font;
-import java.awt.geom.Rectangle2D;
-import java.util.Locale;
-import java.util.TimeZone;
-
-import net.sf.jasperreports.charts.type.EdgeEnum;
-import net.sf.jasperreports.charts.util.ChartUtil;
-import net.sf.jasperreports.engine.JRChart;
 import net.sf.jasperreports.engine.JRComponentElement;
 import net.sf.jasperreports.engine.JRPrintElement;
-import net.sf.jasperreports.engine.JRRenderable;
 import net.sf.jasperreports.engine.base.JRBasePrintImage;
 import net.sf.jasperreports.engine.component.ComponentDesignConverter;
 import net.sf.jasperreports.engine.convert.ReportConverter;
 import net.sf.jasperreports.engine.type.OnErrorTypeEnum;
 import net.sf.jasperreports.engine.type.ScaleImageEnum;
 import net.sf.jasperreports.engine.util.JRExpressionUtil;
-import net.sf.jasperreports.engine.util.JRFontUtil;
-import net.sf.jasperreports.engine.util.JRProperties;
-
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
-import org.jfree.chart.labels.StandardCategoryToolTipGenerator;
-import org.jfree.chart.plot.SpiderWebPlot;
-import org.jfree.chart.title.LegendTitle;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.RectangleEdge;
 
 /**
  * Spider Chart preview converter.
@@ -63,16 +41,7 @@ import org.jfree.ui.RectangleEdge;
 public class SpiderChartDesignConverter implements ComponentDesignConverter
 {
 
-	/**
-	 *
-	 */
-	private static DefaultCategoryDataset sampleDataset;
-	
-	/**
-	 *
-	 */
-	private static final Double MAX_VALUE = Double.valueOf(10);
-	
+	private static final SpiderChartDesignEvaluator designEvaluator = new SpiderChartDesignEvaluator();
 	/**
 	 *
 	 */
@@ -95,253 +64,10 @@ public class SpiderChartDesignConverter implements ComponentDesignConverter
 		printImage.setBookmarkLevel(chartSettings.getBookmarkLevel());
 		printImage.setLinkType(chartSettings.getLinkType());
 		printImage.setOnErrorType(OnErrorTypeEnum.ICON);
-		printImage.setRenderer(getRenderer(reportConverter, element));
+		printImage.setRenderer(designEvaluator.evaluateRenderer(reportConverter, element));
 		printImage.setScaleImage(ScaleImageEnum.CLIP);
 		
 		return printImage;
-	}
-
-	/**
-	 * 
-	 */
-	private JRRenderable getRenderer(ReportConverter reportConverter, JRComponentElement element)
-	{
-		SpiderChartComponent chartComponent = (SpiderChartComponent) element.getComponent();
-		ChartSettings chartSettings = chartComponent.getChartSettings();
-		SpiderPlot plot = chartComponent.getPlot();
-		
-		SpiderWebPlot spiderWebPlot = new SpiderWebPlot(getDataset());
-
-        if(plot.getAxisLineColor() != null)
-        {
-        	spiderWebPlot.setAxisLinePaint(plot.getAxisLineColor());
-        }
-        if(plot.getAxisLineWidth() != null)
-        {
-        	spiderWebPlot.setAxisLineStroke(new BasicStroke(plot.getAxisLineWidth()));
-        }
-        if(plot.getBackcolor() != null)
-        {
-        	spiderWebPlot.setBackgroundPaint(plot.getBackcolor());
-        }
-        if(plot.getBackgroundAlpha() != null)
-        {
-        	spiderWebPlot.setBackgroundAlpha(plot.getBackgroundAlpha());
-        }
-        if(plot.getForegroundAlpha() != null)
-        {
-        	spiderWebPlot.setForegroundAlpha(plot.getForegroundAlpha());
-        }
-        if(plot.getHeadPercent() != null)
-        {
-        	spiderWebPlot.setHeadPercent(plot.getHeadPercent());
-        }
-        if(plot.getInteriorGap() != null)
-        {
-        	spiderWebPlot.setInteriorGap(plot.getInteriorGap());
-        }
-        if(plot.getLabelColor() != null)
-        {
-        	spiderWebPlot.setLabelPaint(plot.getLabelColor());
-        }
-        if(plot.getLabelFont() != null)
-        {
-        	spiderWebPlot.setLabelFont(JRFontUtil.getAwtFont(plot.getLabelFont(), Locale.getDefault()));
-        }
-        if(plot.getLabelGap() != null)
-        {
-        	spiderWebPlot.setAxisLabelGap(plot.getLabelGap());
-        }
-        
-        spiderWebPlot.setMaxValue(MAX_VALUE);
-        
-        if(plot.getRotation() != null)
-        {
-        	spiderWebPlot.setDirection(plot.getRotation().getRotation());
-        }
-        if(plot.getStartAngle() != null)
-        {
-        	spiderWebPlot.setStartAngle(plot.getStartAngle());
-        }
-        if(plot.getTableOrder() != null)
-        {
-        	spiderWebPlot.setDataExtractOrder(plot.getTableOrder().getOrder());
-        }
-        if(plot.getWebFilled() != null)
-        {
-        	spiderWebPlot.setWebFilled(plot.getWebFilled());
-        }
-
-        spiderWebPlot.setToolTipGenerator(new StandardCategoryToolTipGenerator());
-        spiderWebPlot.setLabelGenerator(new StandardCategoryItemLabelGenerator());
-        
-        String titleText = JRExpressionUtil.getExpressionText(chartSettings.getTitleExpression());
-        
-        Font titleFont = chartSettings.getTitleFont() != null 
-        	? JRFontUtil.getAwtFont(chartSettings.getTitleFont(), Locale.getDefault())
-        	: TextTitle.DEFAULT_FONT;
-        	
-        JFreeChart jfreechart = new JFreeChart(titleText, titleFont, spiderWebPlot, true);
-
-		if(chartSettings.getBackcolor() != null)
-		{
-			jfreechart.setBackgroundPaint(chartSettings.getBackcolor());
-		}
-		
-        RectangleEdge titleEdge = getEdge(chartSettings.getTitlePosition(), RectangleEdge.TOP);
-		
-		if (titleText != null)
-		{
-			TextTitle title = jfreechart.getTitle();
-			title.setText(titleText);
-			if(chartSettings.getTitleColor() != null)
-			{
-				title.setPaint(chartSettings.getTitleColor());
-			}
-			
-			title.setFont(titleFont);
-			title.setPosition(titleEdge);
-			jfreechart.setTitle(title);
-		}
-
-        String subtitleText = JRExpressionUtil.getExpressionText(chartSettings.getSubtitleExpression());
-		if (subtitleText != null)
-		{
-			TextTitle subtitle = new TextTitle(subtitleText);
-			subtitle.setText(subtitleText);
-			if(chartSettings.getSubtitleColor() != null)
-			{
-				subtitle.setPaint(chartSettings.getSubtitleColor());
-			}
-
-			if(chartSettings.getSubtitleColor() != null)
-			{
-		        Font subtitleFont = chartSettings.getSubtitleFont() != null 
-	        	? JRFontUtil.getAwtFont(chartSettings.getSubtitleFont(), Locale.getDefault())
-	        	: TextTitle.DEFAULT_FONT;
-				subtitle.setFont(subtitleFont);
-			}
-			
-			subtitle.setPosition(titleEdge);
-
-			jfreechart.addSubtitle(subtitle);
-		}
-
-		// Apply all of the legend formatting options
-		LegendTitle legend = jfreechart.getLegend();
-		if (Boolean.TRUE.equals(chartSettings.getShowLegend()) && legend != null)
-		{
-			legend.setVisible(true);
-			if(chartSettings.getLegendColor() != null)
-			{
-				legend.setItemPaint(chartSettings.getLegendColor());
-			}
-			if (chartSettings.getLegendBackgroundColor() != null)
-			{
-				legend.setBackgroundPaint(chartSettings.getLegendBackgroundColor());
-			}
-
-			if(chartSettings.getLegendFont() != null)
-			{
-				legend.setItemFont(JRFontUtil.getAwtFont(chartSettings.getLegendFont(), Locale.getDefault()));
-			}
-			legend.setPosition(getEdge(chartSettings.getLegendPosition(), RectangleEdge.BOTTOM));
-		}
-			
-		Rectangle2D rectangle = new Rectangle2D.Double(0, 0, element.getWidth(), element.getHeight());
-
-		String renderType = chartSettings.getRenderType();
-		if(renderType == null)
-		{
-			renderType = JRProperties.getProperty(reportConverter.getReport(), JRChart.PROPERTY_CHART_RENDER_TYPE);
-		}
-
-		return 
-			ChartUtil.getChartRendererFactory(renderType).getRenderer(
-				jfreechart, 
-				null,
-				rectangle
-				);
-	}
-	
-	public CategoryDataset getDataset()
-	{
-		if (sampleDataset == null)
-		{
-			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-			dataset.addValue(1.0, "Series 1", "Category 1");
-			dataset.addValue(5.0, "Series 1", "Category 2");
-			dataset.addValue(4.0, "Series 1", "Category 3");
-			dataset.addValue(3.0, "Series 1", "Category 4");
-			dataset.addValue(6.0, "Series 1", "Category 5");
-			dataset.addValue(4.0, "Series 1", "Category 6");
-			dataset.addValue(3.0, "Series 1", "Category 7");
-			dataset.addValue(5.0, "Series 2", "Category 1");
-			dataset.addValue(7.0, "Series 2", "Category 2");
-			dataset.addValue(8.0, "Series 2", "Category 3");
-			dataset.addValue(6.0, "Series 2", "Category 4");
-			dataset.addValue(9.0, "Series 2", "Category 5");
-			dataset.addValue(8.0, "Series 2", "Category 6");
-			dataset.addValue(7.0, "Series 2", "Category 7");
-			dataset.addValue(5.0, "Series 3", "Category 1");
-			dataset.addValue(4.0, "Series 3", "Category 2");
-			dataset.addValue(6.0, "Series 3", "Category 3");
-			dataset.addValue(3.0, "Series 3", "Category 4");
-			dataset.addValue(2.0, "Series 3", "Category 5");
-			dataset.addValue(7.0, "Series 3", "Category 6");
-			dataset.addValue(5.0, "Series 3", "Category 7");
-			
-			sampleDataset = dataset;
-		}
-		
-		return sampleDataset;
-	}
-
-	public Object getLabelGenerator() {
-		return null;
-	}
-
-	public Locale getLocale() {
-		return null;//FIXMETHEME
-	}
-
-	public TimeZone getTimeZone() {
-		return null;
-	}
-
-	/**
-	 *
-	 */
-	private static RectangleEdge getEdge(EdgeEnum position, RectangleEdge defaultPosition)
-	{
-		RectangleEdge edge = defaultPosition;
-		if(position != null)
-		{
-			switch (position)
-			{
-				case TOP :
-				{
-					edge = RectangleEdge.TOP;
-					break;
-				}
-				case BOTTOM :
-				{
-					edge = RectangleEdge.BOTTOM;
-					break;
-				}
-				case LEFT :
-				{
-					edge = RectangleEdge.LEFT;
-					break;
-				}
-				case RIGHT :
-				{
-					edge = RectangleEdge.RIGHT;
-					break;
-				}
-			}
-		}
-		return edge;
 	}
 
 }
