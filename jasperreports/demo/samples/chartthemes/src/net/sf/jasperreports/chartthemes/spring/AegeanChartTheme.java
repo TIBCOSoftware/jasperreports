e9652e4917d60e71e73baea8a2d568551b95fd09
Sanda Zaharia <shertage@users.sourceforge.net>
2010-11-03 12:21:31 +0000

    mercury color backward compatibility fix
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4049 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/spring/AegeanChartTheme.java
+++ jasperreports/demo/samples/chartthemes/src/net/sf/jasperreports/chartthemes/spring/AegeanChartTheme.java
@@ -571,9 +571,18 @@ public class AegeanChartTheme extends GenericChartTheme
 		// any defined ranges.
 		List seriesPaints = (List)getDefaultValue(defaultChartPropertiesMap, ChartThemesConstants.SERIES_COLORS);
 		
-		Paint paint = (jrPlot.getMercuryColor() != null ? (Paint)jrPlot.getMercuryColor() : (Paint)seriesPaints.get(0));
+		Paint paint = jrPlot.getMercuryColor();
+		if(paint != null)
+		{
+			chartPlot.setUseSubrangePaint(false);
+		}
+		else
+		{
+			//it has no effect, but is kept for backward compatibility reasons
+			paint = (Paint)seriesPaints.get(0);
+		}
+		
 		chartPlot.setMercuryPaint(paint);
-		chartPlot.setUseSubrangePaint(false);
 
 		chartPlot.setThermometerPaint(THERMOMETER_COLOR);
 		chartPlot.setThermometerStroke(new BasicStroke(2f));
