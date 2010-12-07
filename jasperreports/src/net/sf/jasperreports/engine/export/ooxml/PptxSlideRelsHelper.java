8d33d583c46df5854e6bf834682462066efab9ed
Teodor Danciu <teodord@users.sourceforge.net>
2010-12-07 10:07:15 +0000

    bugzilla #21303: fix ampersand in hyperlink for xlsx export
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4074 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/ooxml/PptxSlideRelsHelper.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/ooxml/PptxSlideRelsHelper.java
@@ -29,6 +29,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import net.sf.jasperreports.engine.util.JRStringUtil;
+
 
 /**
  * @author Teodor Danciu (teodord@users.sourceforge.net)
@@ -92,7 +94,7 @@ public class PptxSlideRelsHelper extends BaseHelper
 	{
 		write(" <Relationship Id=\"rIdLnk" 
 			+ index + "\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink\" Target=\"" 
-			+ href + "\" TargetMode=\"External\"/>\n");
+			+ JRStringUtil.xmlEncode(href) + "\" TargetMode=\"External\"/>\n");
 	}
 	
 	/**
