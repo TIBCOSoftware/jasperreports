009bf0114359dcfa8fc05b36c2c06c14ac05fc0f
Narcis Marcu <narcism@users.sourceforge.net>
2010-12-10 13:30:23 +0000

    set svn:eol-style and svn:keywords
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4081 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/j2ee/servlets/PptxServlet.java
+++ jasperreports/src/net/sf/jasperreports/j2ee/servlets/PptxServlet.java
@@ -32,7 +32,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.jasperreports.engine.JRAbstractExporter;
 import net.sf.jasperreports.engine.JRConstants;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
