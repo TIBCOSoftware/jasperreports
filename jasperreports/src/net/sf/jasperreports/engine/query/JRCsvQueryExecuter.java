eb189d42a6c7be0dbcfed297ffb32d10a243cd47
Veaceslav Chicu <schicu@users.sourceforge.net>
2011-05-31 14:02:34 +0000

    column names exposed, and report_max_count parameter implemented
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4355 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/query/JRCsvQueryExecuter.java
+++ jasperreports/src/net/sf/jasperreports/engine/query/JRCsvQueryExecuter.java
@@ -215,6 +215,9 @@ public class JRCsvQueryExecuter extends JRAbstractQueryExecuter {
 					datasource.setTimeZone(csvTimezoneId);
 				}
 			}
+			Integer reportMaxCount = (Integer) getParameterValue(JRParameter.REPORT_MAX_COUNT);
+			if(reportMaxCount != null)
+				datasource.setReportMaxCount(reportMaxCount);
 		}
 		
 		return datasource;
