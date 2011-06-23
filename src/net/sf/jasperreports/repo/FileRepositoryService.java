6bfff1fcaaf7fb9d6335d0432aa62e27565ba6a9
Teodor Danciu <teodord@users.sourceforge.net>
2011-06-23 14:54:09 +0000

    save repository resource
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@4433 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/repo/FileRepositoryService.java
+++ src/net/sf/jasperreports/repo/FileRepositoryService.java
@@ -23,6 +23,7 @@
  */
 package net.sf.jasperreports.repo;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -33,6 +34,9 @@ import net.sf.jasperreports.engine.JRRuntimeException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.util.JRResourcesUtil;
+import net.sf.jasperreports.engine.util.JRSaver;
+import net.sf.jasperreports.engine.util.SimpleFileResolver;
 
 import org.exolab.castor.mapping.Mapping;
 import org.xml.sax.InputSource;
@@ -102,7 +106,21 @@ public class FileRepositoryService extends DefaultRepositoryService
 	@Override
 	public void saveResource(String uri, Resource resource)
 	{
-		//FIXMEREPO
+		if (ReportResource.class.getName().equals(resource.getClass().getName()))
+		{
+			SimpleFileResolver fileResolver = (SimpleFileResolver)JRResourcesUtil.getFileResolver(null);
+			File rootFolder = fileResolver.getFolders().get(0);
+			File jasperFile = new File(rootFolder, uri);
+			
+			try
+			{
+				JRSaver.saveObject(((ReportResource)resource).getValue(), jasperFile);
+			}
+			catch (JRException e)
+			{
+				throw new JRRuntimeException(e);
+			}
+		}
 	}
 	
 	@Override
