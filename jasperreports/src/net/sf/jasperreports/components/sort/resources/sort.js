18cad763c69629285413eed7730d9e2a427bfb91
Narcis Marcu <narcism@users.sourceforge.net>
2011-07-27 15:29:17 +0000

    javascript namespace refactoring
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4506 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/components/sort/resources/sort.js
+++ jasperreports/src/net/sf/jasperreports/components/sort/resources/sort.js
@@ -1,62 +1,97 @@
-jQuery(document).ready(function() {
+/**
+ * Defines 'sort' module in JasperReports namespace
+ */
+(function(global) {
+	if (typeof global.JasperReports.modules.sort !== 'undefined') {
+		return;
+	}
 	
-	// disable browser contextual menu when right-clicking
-	jQuery(document).bind("contextmenu",function(e){  
-        return false;  
-    });
+	var js = {};
+	
+	js.init = function() {
+		var gm = global.JasperReports.modules.global;
+		
+		// disable browser contextual menu when right-clicking
+		jQuery(document).bind("contextmenu",function(e){  
+	        return false;  
+	    });
 
-	// add event for clickable sortlinks (up/down arrows)
-	jQuery('a').live('click', function(event){
-		event.preventDefault();
-		var currentHref = jQuery(this).attr("href");
-		var ctx = getExecutionContext(this, currentHref, null);
-		if (ctx) {
-			ctx.run();
-		}
-		
-		
-	});
-	
-	jQuery('.hidefilter').live('click', function(event){
-		jQuery(this).parent().hide();
-	});
-	
-	jQuery('.sortlink').live('mousedown', function(event) {
-	    if (event.which == 3) {
-	    	var filterDiv = jQuery('.filterdiv',jQuery(this).parent());
-	    	
-	    	// hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
-	    	jQuery('.filterdiv').filter(':visible').each(function (index, element) {
-	    		jQuery(element).hide();
-	    	});
-	    	
-	    	filterDiv.css({
-	    			position: 'absolute',
-	    			'z-index': 999998,
-	    			left: (event.pageX - jQuery(this).offset().left)  + "px",
-	    			top: (event.pageY - jQuery(this).offset().top) + "px"
-	    	});
-	    	
-	    	filterDiv.draggable();
-	    	filterDiv.show();
-	    	
-	    }
-	});
-	
-	jQuery('.submitFilter').live('click', function(event){
-		var params = new Object();
-		var parentForm = jQuery(this).parent();
-		
-		// extract form params
-		jQuery('.postable', parentForm).each(function(){
-			params[this.name] = this.value;
+		// add event for clickable sortlinks (up/down arrows)
+		jQuery('a').live('click', function(event){
+			event.preventDefault();
+			var currentHref = jQuery(this).attr("href"),
+				ctx = gm.getExecutionContext(this, currentHref, null);
+
+			if (ctx) {
+				ctx.run();
+			}
 		});
 		
-		var currentHref = jQuery(parentForm).attr("action");
-		var ctx = getExecutionContext(this, currentHref, params);
-		if (ctx) {
-			ctx.run();
-		}		
-	});
+		jQuery('.hidefilter').live('click', function(event){
+			jQuery(this).parent().hide();
+		});
+		
+		
+		jQuery('.filterdiv')
+			.draggable()
+			.bind('mouseleave', function(){
+				jQuery(this).hide();
+			})
+			.bind('keypress', function(event) {
+				var target = jQuery(event.target),
+					filterDiv = jQuery(this);
+				
+				// Disable 'Enter' key press for filter value
+				if (target.is('.filterValue') && event.keyCode == 13) {
+					event.preventDefault();
+					return false;
+				}
+			});
+		
+		/**
+		 * Show filter div when right-clicking the table header
+		 */
+		jQuery('.sortlink').live('mousedown', function(event) {
+		    if (event.which == 3) {
+		    	var filterDiv = jQuery('.filterdiv',jQuery(this).parent());
+		    	
+		    	// hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
+		    	jQuery('.filterdiv').filter(':visible').each(function (index, element) {
+		    		jQuery(element).hide();
+		    	});
+		    	
+		    	filterDiv.css({
+	    			position: 'absolute',
+	    			'z-index': 999998,
+	    			left: (event.pageX - jQuery(this).offset().left -1)  + "px",
+	    			top: (event.pageY - jQuery(this).offset().top -1) + "px"
+		    	});
+		    	
+		    	filterDiv.show();
+		    }
+		});
+		
+		jQuery('.submitFilter').live('click', function(event){
+			var params = {},
+				parentForm = jQuery(this).parent(),
+				currentHref = parentForm.attr("action");
+
+			// extract form params
+			jQuery('.postable', parentForm).each(function(){
+				params[this.name] = this.value;
+			});
+			
+			var ctx = gm.getExecutionContext(this, currentHref, params);
+			
+			if (ctx) {
+				ctx.run();
+			}		
+		});
+	};
 	
+	global.JasperReports.modules.sort = js;
+} (this));
+
+jQuery(document).ready(function() {
+	JasperReports.modules.sort.init();
 });
