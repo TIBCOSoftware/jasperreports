98de1fee01b0c2faa40235f6c5963b3aa1bbe164
Narcis Marcu <narcism@users.sourceforge.net>
2011-08-25 14:21:34 +0000

    prevent init event from being called multiple times; minor adjustments
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4561 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/components/sort/resources/sort.js
+++ jasperreports/src/net/sf/jasperreports/components/sort/resources/sort.js
@@ -6,11 +6,15 @@
 		return;
 	}
 	
-	var js = {};
+	var js = {
+				filters: {
+					filterContainerId: "jasperreports_filters"
+				}
+		};
 	
 	js.createFilterDiv = function (uniqueId, arrFilterDiv) {
 		var gm = global.JasperReports.modules.global,
-			filterContainerId = "jasperreports_filters",
+			filterContainerId = js.filters.filterContainerId,
 			filterContainerDiv = "<div id='" + filterContainerId + "'></div>",
 			fcuid = '#' + filterContainerId,
 			uid = '#' + uniqueId;
@@ -30,7 +34,6 @@
 				jQuery(this).parent().hide();
 			});
 			
-			
 			filterDiv
 				.draggable()
 				.bind('keypress', function(event) {
@@ -44,21 +47,22 @@
 					}
 				});
 			
-			
-			
 			jQuery('.submitFilter', filterDiv).live(('createTouch' in document) ? 'touchend' : 'click', function(event){
 				var params = {},
 					parentForm = jQuery(this).parent(),
-					currentHref = parentForm.attr("action");
+					currentHref = parentForm.attr("action"),
+					parentFilterDiv = jQuery(this).closest('.filterdiv'),
+					contextStartPoint = jQuery('.' + parentFilterDiv.attr('data-forsortlink') + ':first');
 				
 				// extract form params
 				jQuery('.postable', parentForm).each(function(){
 					params[this.name] = this.value;
 				});
 				
-				var ctx = gm.getExecutionContext(this, currentHref, params);
+				var ctx = gm.getExecutionContext(contextStartPoint, currentHref, params);
 				
 				if (ctx) {
+					parentFilterDiv.hide();
 					ctx.run();
 				}		
 			});
@@ -75,113 +79,118 @@
 
 	js.init = function() { 
 		var gm = global.JasperReports.modules.global,
-			eventName = gm.events.SORT_INIT;
+			sortEvent = gm.events.SORT_INIT;
 		
-		// disable browser contextual menu when right-clicking
-		jQuery(document).bind("contextmenu",function(e){  
-	        return false;  
-	    });
-
-		if ('createTouch' in document) {
-			var sortlinks = jQuery('.sortlink');
-
-            jQuery('document').bind("touchmove",function(event){
-                js.touchStartOn = undefined;
-            });
-			sortlinks.live('click', function(event){
-                event.preventDefault();
-                event.stopPropagation();
-                return false;
-            });
-			/*
-			 * Capture long touch on touchstart. Also prevent default.
-			 */
-            sortlinks.bind("touchstart",function(event){
-                event.preventDefault();
-
-                !event.isStartData && (js.touchStartOn = {
-                    element: event.target,
-                    timeStamp: event.timeStamp
-                });
-                event.isStartData = true;
-            });
-			/*
-             * If long touch do not trigger anchor link.
-             */
-            sortlinks.live('touchend', function(event){
-                event.preventDefault();
-
-                if(('createTouch' in document) && js.isLongTouch(event)) {
-                    event.stopPropagation();
-                    return false;
-                }
-
-                var currentHref = jQuery(this).attr("href"),
-                    ctx = gm.getExecutionContext(this, currentHref, null);
-
-                if (ctx) {
-                    ctx.run();
-                }
-            });
-			/*
-             * Show filter div on long touch
-             */
-            sortlinks.bind('touchend', function(event) {
-                event.preventDefault();
-                if (js.isLongTouch(event) || event.which == 3) {
-                    var filterDiv = jQuery('#' + jQuery(this).attr('data-filterid'));
-
-                    // hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
-                    jQuery('.filterdiv').filter(':visible').each(function (index, element) {
-                        jQuery(element).hide();
-                    });
-
-                    var touch = event.changedTouches ? event.changedTouches[0] : event.originalEvent.changedTouches[0];
-                    filterDiv.css({
-                        position: 'absolute',
-                        'z-index': 999998,
-                        left: touch.pageX  + "px",
-                        top: touch.pageY + "px"
-                    });
-
-                    filterDiv.show();
-                }
-            });
-		} else {
-			// add event for clickable sortlinks (up/down arrows)
-            jQuery('a').live('click', function(event){
-                event.preventDefault();
-                var currentHref = jQuery(this).attr("href"),
-                    ctx = gm.getExecutionContext(this, currentHref, null);
-
-                if (ctx) {
-                    ctx.run();
-                }
-            });
-            /**
-             * Show filter div when right-clicking the table header
-             */
-            jQuery('.sortlink').live('mousedown', function(event) {
-                if (event.which == 3) {
-                    var filterDiv = jQuery('#' + jQuery(this).attr('data-filterid'));
-
-                    // hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
-                    jQuery('.filterdiv').filter(':visible').each(function (index, element) {
-                        jQuery(element).hide();
-                    });
-
-                    filterDiv.css({
-                        position: 'absolute',
-                        'z-index': 999998,
-                        left: (event.pageX)  + "px",
-                        top: (event.pageY) + "px"
-                    });
-
-                    filterDiv.show();
-                }
-            });
+		// init should be done only once
+		if (sortEvent.status === 'default') {
+			// disable browser contextual menu when right-clicking
+			jQuery(document).bind("contextmenu",function(e){  
+		        return false;  
+		    });
+	
+			if ('createTouch' in document) {
+				var sortlinks = jQuery('.sortlink');
+	
+	            jQuery('document').bind("touchmove",function(event){
+	                js.touchStartOn = undefined;
+	            });
+				sortlinks.live('click', function(event){
+	                event.preventDefault();
+	                event.stopPropagation();
+	                return false;
+	            });
+				/*
+				 * Capture long touch on touchstart. Also prevent default.
+				 */
+	            sortlinks.bind("touchstart",function(event){
+	                event.preventDefault();
+	
+	                !event.isStartData && (js.touchStartOn = {
+	                    element: event.target,
+	                    timeStamp: event.timeStamp
+	                });
+	                event.isStartData = true;
+	            });
+				/*
+	             * If long touch do not trigger anchor link.
+	             */
+	            sortlinks.live('touchend', function(event){
+	                event.preventDefault();
+	
+	                if(js.isLongTouch(event)) {
+	                    event.stopPropagation();
+	                    return false;
+	                }
+	
+	                var currentHref = jQuery(this).attr("href"),
+	                    ctx = gm.getExecutionContext(this, currentHref, null);
+	
+	                if (ctx) {
+	                    ctx.run();
+	                }
+	            });
+				/*
+	             * Show filter div on long touch
+	             */
+	            sortlinks.bind('touchend', function(event) {
+	                event.preventDefault();
+	                if (js.isLongTouch(event) || event.which == 3) {
+	                    var filterDiv = jQuery('#' + jQuery(this).attr('data-filterid'));
+	
+	                    // hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
+	                    jQuery('.filterdiv').filter(':visible').each(function (index, element) {
+	                        jQuery(element).hide();
+	                    });
+	
+	                    var touch = event.changedTouches ? event.changedTouches[0] : event.originalEvent.changedTouches[0];
+	                    filterDiv.css({
+	                        position: 'absolute',
+	                        'z-index': 999998,
+	                        left: touch.pageX  + "px",
+	                        top: touch.pageY + "px"
+	                    });
+	
+	                    filterDiv.show();
+	                }
+	            });
+			} else {
+				// add event for clickable sortlinks (up/down arrows)
+//				jQuery('.sortlink').live('click', function(event){
+				jQuery('a').live('click', function(event) {
+	                event.preventDefault();
+	                var currentHref = jQuery(this).attr("href"),
+	                    ctx = gm.getExecutionContext(this, currentHref, null);
+	
+	                if (ctx) {
+	                    ctx.run();
+	                }
+	            });
+	            /**
+	             * Show filter div when right-clicking the table header
+	             */
+	            jQuery('.sortlink').live('mousedown', function(event) {
+	                if (event.which == 3) {
+	                    var filterDiv = jQuery('#' + jQuery(this).attr('data-filterid'));
+	
+	                    // hide all other open filters FIXMEJIVE: this will close all visible filters from all reports on the same page
+	                    jQuery('.filterdiv').filter(':visible').each(function (index, element) {
+	                        jQuery(element).hide();
+	                    });
+	
+	                    filterDiv.css({
+	                        position: 'absolute',
+	                        'z-index': 999998,
+	                        left: (event.pageX)  + "px",
+	                        top: (event.pageY) + "px"
+	                    });
+	
+	                    filterDiv.show();
+	                }
+	            });
+			}
+			sortEvent.status = 'finished';
+			gm.processEvent(sortEvent.name);
 		}
-		gm.processEvent(eventName);
 	};
 	
 	global.JasperReports.modules.sort = js;
