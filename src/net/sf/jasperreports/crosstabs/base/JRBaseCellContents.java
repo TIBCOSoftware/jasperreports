578a734059f05ab9d5fb9155f82efbfbada3c116
Teodor Danciu <teodord@users.sourceforge.net>
2011-09-08 15:55:10 +0000

    copyright notice update
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@4595 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/crosstabs/base/JRBaseCellContents.java
+++ src/net/sf/jasperreports/crosstabs/base/JRBaseCellContents.java
@@ -1,6 +1,6 @@
 /*
  * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
+ * Copyright (C) 2001 - 2011 Jaspersoft Corporation. All rights reserved.
  * http://www.jaspersoft.com
  *
  * Unless you have purchased a commercial license agreement from Jaspersoft,
@@ -17,7 +17,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
  */
