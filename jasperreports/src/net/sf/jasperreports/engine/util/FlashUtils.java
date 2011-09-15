5f2e5072c9dfb6966eab096ed3651f6b4ac0c24d
Lucian Chirita <lucianc@users.sourceforge.net>
2011-09-15 11:16:29 +0000

    bugzilla 24360 - escape FlashVars in generic Flash elements
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4608 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/engine/util/FlashUtils.java
@@ -0,0 +1,61 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2011 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import net.sf.jasperreports.engine.JRRuntimeException;
+
+/**
+ * Utility methods related to Flash objects.
+ * 
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class FlashUtils
+{
+
+	/**
+	 * Encodes a text used as Flash variable.
+	 * 
+	 * @param text the text to encode
+	 * @return the encoded text
+	 */
+	public static String encodeFlashVariable(String text)
+	{
+		try
+		{
+			// URL/percent encoding is used for FlashVars
+			// always using UTF-8 for now
+			return URLEncoder.encode(text, "UTF-8");
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// never
+			throw new JRRuntimeException(e);
+		}
+	}
+	
+}
