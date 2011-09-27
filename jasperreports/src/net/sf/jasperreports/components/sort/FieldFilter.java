1158aee14277cbe2f730409c2165a3371816b3e2
Narcis Marcu <narcism@users.sourceforge.net>
2011-09-27 12:24:10 +0000

    added filter operators
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@4633 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/components/sort/FieldFilter.java
+++ jasperreports/src/net/sf/jasperreports/components/sort/FieldFilter.java
@@ -23,12 +23,18 @@
  */
 package net.sf.jasperreports.components.sort;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import net.sf.jasperreports.engine.DatasetFilter;
 import net.sf.jasperreports.engine.EvaluationType;
 import net.sf.jasperreports.engine.fill.DatasetFillContext;
+import net.sf.jasperreports.engine.util.JRDateLocaleConverter;
+import net.sf.jasperreports.engine.util.JRFloatLocaleConverter;
 
+import org.apache.commons.beanutils.locale.LocaleConvertUtilsBean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -43,29 +49,42 @@ public class FieldFilter implements DatasetFilter
 	
 	private static final Log log = LogFactory.getLog(FieldFilter.class);
 
+	private LocaleConvertUtilsBean convertBean;
+	private final String DEFAULT_NUMBER_PATTERN = "#,##0.##";
+	private final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+	
 	private final String field;
-	private String filterValue;
+
+	private String filterValueStart;
+	private String filterValueEnd;
+	
+	private String filterType;
+	private String filterTypeOperator;
 	
 	private DatasetFillContext context;
 	private Locale locale;
+	
+	FilterTypesEnum filterTypeEnum;
 
 	/**
 	 * Creates a field filter.
 	 * 
-	 * @param field the field name
-	 * @param filterValue the value to search for
+	 * 
 	 */
-	public FieldFilter(String field, String filterValue)
+	public FieldFilter(String field, String filterValueStart, String filterValueEnd, String filterType, String filterTypeOperator)
 	{
 		this.field = field;
-		this.filterValue = filterValue;
+		this.filterValueStart = filterValueStart;
+		this.filterValueEnd = filterValueEnd;
+		this.filterType = filterType;
+		this.filterTypeOperator = filterTypeOperator;
 	}
 	
 	public void init(DatasetFillContext context)
 	{
 		this.context = context;
 		this.locale = context.getLocale();
-		filterValue = filterValue.toLowerCase(locale);
+		this.filterTypeEnum = FilterTypesEnum.getByName(filterType);
 	}
 
 	public boolean matches(EvaluationType evaluation)
@@ -76,19 +95,211 @@ public class FieldFilter implements DatasetFilter
 			return false;
 		}
 		
-		if (!(value instanceof String))
-		{
-			if (log.isDebugEnabled())
-			{
-				log.debug("Not filtering non-String value for " + field);
-			}
-			return true;
+		boolean result = true;
+		
+		switch (filterTypeEnum) {
+			case DATE:
+				result = dateMatch(value, FilterTypeDateOperatorsEnum.getByName(filterTypeOperator));
+				break;
+			case NUMERIC:
+				result = numericMatch(value, FilterTypeNumericOperatorsEnum.getByName(filterTypeOperator));
+				break;
+			case TEXT:
+				result = textMatch(value, FilterTypeTextOperatorsEnum.getByName(filterTypeOperator));
+				break;
 		}
 		
-		String fieldValue = (String) value;
-		fieldValue = fieldValue.toLowerCase();
+		return result;
+	}
+	
+	protected boolean numericMatch(Object value, FilterTypeNumericOperatorsEnum numericEnum) {
+		Number compareStart = null, compareEnd = null;
+		if (filterValueStart != null && filterValueStart.length() > 0){
+			compareStart = (Number) convertStringValue(filterValueStart, value.getClass());
+		}
+		if (filterValueEnd != null && filterValueEnd.length() > 0){
+			compareEnd =  (Number) convertStringValue(filterValueEnd, value.getClass());
+		}
+		return compareNumbers((Number) value, compareStart, compareEnd, numericEnum);
+	}
+	
+	protected boolean dateMatch(Object value, FilterTypeDateOperatorsEnum dateEnum) {
+		Date compareStart = null, compareEnd = null;
+		if (filterValueStart != null && filterValueStart.length() > 0){
+			compareStart = (Date) convertStringValue(filterValueStart, value.getClass());
+		}
+		if (filterValueEnd != null && filterValueEnd.length() > 0){
+			compareEnd =  (Date) convertStringValue(filterValueEnd, value.getClass());
+		}
+		return compareDates((Date) value, compareStart, compareEnd, dateEnum);
+	}
+
+	protected boolean textMatch(Object value, FilterTypeTextOperatorsEnum textEnum) {
+		return compareText((String) value, filterValueStart, textEnum);
+	}
+	
+	protected Object convertStringValue(String text, Class<?> valueClass)
+	{
+		Object value = null;
+		if (String.class.equals(valueClass))
+		{
+			value = text;
+		}
+		else if (Number.class.isAssignableFrom(valueClass))
+		{
+			value = getConvertBean().convert(text.trim(), valueClass, locale, DEFAULT_NUMBER_PATTERN);
+		}
+		else if (Date.class.isAssignableFrom(valueClass))
+		{
+			value = getConvertBean().convert(text.trim(), valueClass, locale, DEFAULT_DATE_PATTERN);
+		}
+		return value;
+	}
+	
+	protected LocaleConvertUtilsBean getConvertBean() 
+	{
+		if (convertBean == null)
+		{
+			convertBean = new LocaleConvertUtilsBean();
+			if (locale != null)
+			{
+				convertBean.setDefaultLocale(locale);
+				convertBean.deregister();
+				//convertBean.lookup();
+			}
+			convertBean.register(
+				new JRDateLocaleConverter(TimeZone.getDefault()), 
+				java.util.Date.class,
+				locale
+				);
+			
+			// fix for https://issues.apache.org/jira/browse/BEANUTILS-351
+			// remove on upgrade to BeanUtils 1.8.1
+			JRFloatLocaleConverter floatConverter = new JRFloatLocaleConverter(
+					locale == null ? Locale.getDefault() : locale);
+			convertBean.register(floatConverter, Float.class, locale);
+			convertBean.register(floatConverter, Float.TYPE, locale);
+		}
+		return convertBean;
+	}
+	
+	protected boolean compareNumbers(Number toCompare, Number compareStart, Number compareEnd, FilterTypeNumericOperatorsEnum numericEnum) {
+		boolean defaultResult = true,
+				result = defaultResult,
+				resultPart1 = true, 
+				resultPart2 = true;
+
+		BigDecimal dbA = new BigDecimal(toCompare.toString());
+		BigDecimal dbStart = compareStart != null ? new BigDecimal(compareStart.toString()) : null;
+		BigDecimal dbEnd = compareEnd != null ? new BigDecimal(compareEnd.toString()) : null;
+					
+		switch (numericEnum) {
+			case DOES_NOT_EQUAL:
+				result = dbStart != null ? dbA.compareTo(dbStart) != 0 : defaultResult;
+				break;
+			case EQUALS:
+				result = dbStart != null ? dbA.compareTo(dbStart) == 0 : defaultResult;
+				break;
+			case GREATER_THAN:
+				result = dbStart != null ? dbA.compareTo(dbStart) > 0 : defaultResult;
+				break;
+			case GREATER_THAN_EQUAL_TO:
+				result = dbStart != null ? dbA.compareTo(dbStart) >= 0 : defaultResult;
+				break;
+			case IS_BETWEEN:
+				resultPart1 = dbStart != null ? dbA.compareTo(dbStart) >= 0 : defaultResult;
+				resultPart2 = dbEnd != null ? dbA.compareTo(dbEnd) <= 0 : defaultResult;
+				result = resultPart1 && resultPart2;
+				break;
+			case IS_NOT_BETWEEN:
+				resultPart1 = dbStart != null ? dbA.compareTo(dbStart) >= 0 : defaultResult;
+				resultPart2 = dbEnd != null ? dbA.compareTo(dbEnd) <= 0 : defaultResult;
+				result = !(resultPart1 && resultPart2);
+				break;
+			case LESS_THAN:
+				result = dbStart != null ? dbA.compareTo(dbStart) < 0 : defaultResult;
+				break;
+			case LESS_THAN_EQUAL_TO:
+				result = dbStart != null ? dbA.compareTo(dbStart) <= 0 : defaultResult;
+				break;
+		}
 		
-		return fieldValue.contains(filterValue);
+		return result;
+	}
+	
+	protected boolean compareDates(Date toCompare, Date compareStart, Date compareEnd, FilterTypeDateOperatorsEnum dateEnum) {
+		boolean defaultResult = true,
+				result = defaultResult,
+				resultPart1 = true, 
+				resultPart2 = true;
+		
+		switch (dateEnum) {
+			case EQUALS:
+				result = compareStart != null ? toCompare.compareTo(compareStart) == 0 : defaultResult;
+				break;
+			case IS_AFTER:
+				result = compareStart != null ? toCompare.compareTo(compareStart) > 0 : defaultResult;
+				break;
+			case IS_BEFORE:
+				result = compareStart != null ? toCompare.compareTo(compareStart) < 0 : defaultResult;
+				break;
+			case IS_BETWEEN:
+				resultPart1 = compareStart != null ? toCompare.compareTo(compareStart) >= 0 : defaultResult;
+				resultPart2 = compareEnd != null ? toCompare.compareTo(compareEnd) <= 0 : defaultResult;
+				result = resultPart1 && resultPart2;
+				break;
+			case IS_NOT_BETWEEN:
+				resultPart1 = compareStart != null ? toCompare.compareTo(compareStart) >= 0 : defaultResult;
+				resultPart2 = compareEnd != null ? toCompare.compareTo(compareEnd) <= 0 : defaultResult;
+				result = !(resultPart1 && resultPart2);
+				break;
+			case IS_NOT_EQUAL_TO:
+				result = compareStart != null ? toCompare.compareTo(compareStart) != 0 : defaultResult;
+				break;
+			case IS_ON_OR_AFTER:
+				result = compareStart != null ? toCompare.compareTo(compareStart) >= 0 : defaultResult;
+				break;
+			case IS_ON_OR_BEFORE:
+				result = compareStart != null ? toCompare.compareTo(compareStart) <= 0 : defaultResult;
+				break;
+		}
+		
+		return result;
+	}
+	
+	protected boolean compareText(String toCompare, String compareWith, FilterTypeTextOperatorsEnum textEnum) {
+		boolean defaultResult = true,
+				result = defaultResult,
+				gotCompareWith = compareWith != null && compareWith.length() > 0;
+		
+		switch (textEnum) {
+			case CONTAINS:
+				result = gotCompareWith ? toCompare.contains(compareWith) : defaultResult;
+				break;
+			case DOES_NOT_CONTAIN:
+				result = gotCompareWith ? !toCompare.contains(compareWith) : defaultResult;
+				break;
+			case DOES_NOT_END_WITH:
+				result = gotCompareWith ? !toCompare.endsWith(compareWith) : defaultResult;
+				break;
+			case DOES_NOT_START_WITH:
+				result = gotCompareWith ? !toCompare.startsWith(compareWith) : defaultResult;
+				break;
+			case ENDS_WITH:
+				result = gotCompareWith ? toCompare.endsWith(compareWith) : defaultResult;
+				break;
+			case EQUALS:
+				result = gotCompareWith ? toCompare.equals(compareWith) : defaultResult;
+				break;
+			case IS_NOT_EQUAL_TO:
+				result = gotCompareWith ? !toCompare.equals(compareWith) : defaultResult;
+				break;
+			case STARTS_WITH:
+				result = gotCompareWith ? toCompare.startsWith(compareWith) : defaultResult;
+				break;
+		}
+		
+		return result;
 	}
 
 }
