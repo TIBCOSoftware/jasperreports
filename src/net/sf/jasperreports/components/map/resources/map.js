30f7d37ac8383cd7b07448fc7960e3a3aa9ae921
Teodor Danciu <teodord@users.sourceforge.net>
2012-03-29 14:35:20 +0000

    fix javascript namespace in map component
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@5182 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/components/map/resources/map.js
+++ src/net/sf/jasperreports/components/map/resources/map.js
@@ -1,19 +1,19 @@
 /**
- * Defines 'map' module in JasperReports namespace
+ * Defines 'map' module in jasperreports namespace
  */
 (function(global) {
-	if (typeof global.JasperReports.modules.map !== 'undefined') {
+	if (typeof global.jasperreports.map !== 'undefined') {
 		return;
 	}
 	var jm = {
 		callback: {}	// defined in template
 	};
 	jm.init = function() {
-		var googleScriptUri = 'http://maps.google.com/maps/api/js?sensor=false&callback=JasperReports.modules.map.callback';
+		var googleScriptUri = 'http://maps.google.com/maps/api/js?sensor=false&callback=jasperreports.map.callback';
 		
 		if (!global.google) {
-			JasperReports.modules.global.loadScript('_googleApi', googleScriptUri);
+			jasperreports.global.loadScript('_googleApi', googleScriptUri);
 		}
 	};
-	global.JasperReports.modules.map = jm;
+	global.jasperreports.map = jm;
 } (this));
