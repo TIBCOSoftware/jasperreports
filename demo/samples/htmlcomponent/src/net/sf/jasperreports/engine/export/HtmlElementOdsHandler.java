546e840f93f475debf82051c31ae080d2e3134f0
Narcis Marcu <narcism@users.sourceforge.net>
2012-08-21 09:29:22 +0000

    added support for ods, odt, rtf, xlsx and xml formats
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@5592 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/htmlcomponent/src/net/sf/jasperreports/engine/export/HtmlElementOdsHandler.java
@@ -0,0 +1,69 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2011 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.export;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRGenericPrintElement;
+import net.sf.jasperreports.engine.JRPrintImage;
+import net.sf.jasperreports.engine.export.oasis.GenericElementOdsHandler;
+import net.sf.jasperreports.engine.export.oasis.JROdsExporter;
+import net.sf.jasperreports.engine.export.oasis.JROdsExporterContext;
+import net.sf.jasperreports.engine.util.HtmlPrintElement;
+import net.sf.jasperreports.engine.util.HtmlPrintElementUtils;
+
+/**
+ * @author Narcis Marcu (narcism@users.sourceforge.net)
+ * @version $Id$
+ */
+public class HtmlElementOdsHandler implements GenericElementOdsHandler
+{
+	public void exportElement(
+		JROdsExporterContext exporterContext,
+		JRGenericPrintElement element,
+		JRExporterGridCell gridCell
+		)
+	{
+		try
+		{
+			JROdsExporter exporter = (JROdsExporter)exporterContext.getExporter();
+			exporter.exportImage(exporterContext.getTableBuilder(), getImage(exporterContext, element), gridCell);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public JRPrintImage getImage(JROdsExporterContext exporterContext,
+			JRGenericPrintElement element) throws JRException {
+		HtmlPrintElement htmlPrintElement = HtmlPrintElementUtils.getHtmlPrintElement();
+		return htmlPrintElement.createImageFromElement(element);
+	}
+
+	public boolean toExport(JRGenericPrintElement element) {
+		return true;
+	}
+
+}
