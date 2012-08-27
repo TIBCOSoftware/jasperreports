0e9deaa37eabbfcf022969b98d1488e0b582dfb8
Narcis Marcu <narcism@users.sourceforge.net>
2012-08-27 09:42:01 +0000

    fix for non-interactive columns
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@5608 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/components/table/fill/TableReport.java
+++ src/net/sf/jasperreports/components/table/fill/TableReport.java
@@ -550,6 +550,7 @@ public class TableReport implements JRReport
 			if (!TableUtil.isSortableAndFilterable(sortTextField)) {
 				genericElement.getPropertiesMap().setProperty(HeaderToolbarElement.PROPERTY_CAN_FILTER, Boolean.FALSE.toString());
 				genericElement.getPropertiesMap().setProperty(HeaderToolbarElement.PROPERTY_CAN_SORT, Boolean.FALSE.toString());
+				interactiveColumn = false;
 			} else {
 				genericElement.getPropertiesMap().setProperty(HeaderToolbarElement.PROPERTY_CAN_FILTER, Boolean.TRUE.toString());
 				genericElement.getPropertiesMap().setProperty(HeaderToolbarElement.PROPERTY_CAN_SORT, Boolean.TRUE.toString());
