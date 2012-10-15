f65802fa7074a6211c936520059bc9ed0e911109
Narcis Marcu <narcism@users.sourceforge.net>
2012-10-15 15:27:33 +0000

    fix to support multiple maps in same report
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@5719 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/components/map/resources/map.js
+++ src/net/sf/jasperreports/components/map/resources/map.js
@@ -5,17 +5,31 @@
 	if (typeof global.jasperreports.map !== 'undefined') {
 		return;
 	}
-	var jm = {
-		callback: {}	// defined in template
+	global.jasperreports.map = {
+		data: {},
+		initGoogleMaps: function() {
+			if (!global.google) {
+				jasperreports.global.loadScript('_googleApi', 'http://maps.google.com/maps/api/js?sensor=false&callback=jasperreports.map.init');
+			}
+		},
+		init: function() {
+			jasperreports.events.registerEvent('jasperreports.map.init').trigger();
+		},
+		addMapData: function(canvasId, latitude, longitude, zoom) {
+			this.data[canvasId] = {
+					lat: latitude,
+					long: longitude,
+					zoom: zoom
+			};
+		},
+		showMap: function(canvasId, latitude, longitude, zoom, mapType) {
+			var gg = google.maps,
+				myOptions = {
+					zoom: zoom,
+					center: new gg.LatLng(latitude, longitude), 
+					mapTypeId: gg.MapTypeId[mapType]
+				},
+				map = new gg.Map(document.getElementById(canvasId), myOptions);
+		},
 	};
-	jm.init = function() {
-		var googleScriptUri = 'http://maps.google.com/maps/api/js?sensor=false&callback=jasperreports.map.callback';
-		
-		if (!global.google) {
-			jasperreports.global.loadScript('_googleApi', googleScriptUri);
-		} else {
-			jm.callback();
-		}
-	};
-	global.jasperreports.map = jm;
 } (this));
