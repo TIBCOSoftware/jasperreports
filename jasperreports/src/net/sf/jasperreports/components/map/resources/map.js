5067cbe4ed6fee7f0f32f1b155001b8f5fb9e66e
Sanda Zaharia <shertage@users.sourceforge.net>
2013-04-01 10:56:23 +0000

    support for marker/shadow icons
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6039 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/components/map/resources/map.js
+++ jasperreports/src/net/sf/jasperreports/components/map/resources/map.js
@@ -43,8 +43,122 @@
 					        position: markerLatLng,
 					        map: map
 					    };
+				    
+				    if(markerProps['icon.url'] && markerProps['icon.url'].length > 0) {
+				    	var width, height, originX, originY, anchorX, anchorY;
+				    	var iconSize;
+				    	var iconOrigin, iconAnchor;
+				    	var iconUrl = markerProps['icon.url'];
+				    	
+				    	if(markerProps['icon.width']) {
+				    		width = new Number(markerProps['icon.width']);
+				    		height = 0;
+				    	}
+				    	if(markerProps['icon.height']) {
+				    		height = new Number(markerProps['icon.height']);
+				    		if(!width) {
+				    			width = 0;
+				    		}
+				    	}
+				    	if(markerProps['icon.origin.x']) {
+				    		originX = new Number(markerProps['icon.origin.x']);
+				    		originY = 0;
+				    	}
+				    	if(markerProps['icon.origin.y']) {
+				    		originY = new Number(markerProps['icon.origin.y']);
+				    		if(!originX) {
+				    			originX = 0;
+				    		}
+				    	}
+				    	if(markerProps['icon.anchor.x']) {
+				    		anchorX = new Number(markerProps['icon.anchor.x']);
+				    		anchorY = 0;
+				    	}
+				    	if(markerProps['icon.anchor.y']) {
+				    		anchorY = new Number(markerProps['icon.anchor.y']);
+				    		if(!anchorX) {
+				    			anchorX = 0;
+				    		}
+				    	}
+				    	if(width || height) {
+				    		iconSize = new google.maps.Size(width,height);
+				    	}
+				    	if(originX || originY) {
+				    		iconOrigin = new google.maps.Point(originX,originY);
+				    	}
+				    	if(anchorX || anchorY) {
+				    		iconAnchor = new google.maps.Point(anchorX,anchorY);
+				    	}
+				    	var customIcon = {
+				    			url: iconUrl,
+				    			size: iconSize,
+				    			origin: iconOrigin,
+				    			anchor: iconAnchor
+				    	};
+				    	markerOptions['icon'] = customIcon;
+				    }
+				    
+				    if(markerProps['shadow.url'] && markerProps['shadow.url'].length > 0) {
+				    	var width, height, originX, originY, anchorX, anchorY;
+				    	var shadowSize;
+				    	var shadowOrigin, shadowAnchor;
+				    	var shadowUrl = markerProps['shadow.url'];
+				    	
+				    	if(markerProps['shadow.width']) {
+				    		width = new Number(markerProps['shadow.width']);
+				    		height = 0;
+				    	}
+				    	if(markerProps['shadow.height']) {
+				    		height = new Number(markerProps['shadow.height']);
+				    		if(!width) {
+				    			width = 0;
+				    		}
+				    	}
+				    	if(markerProps['shadow.origin.x']) {
+				    		originX = new Number(markerProps['shadow.origin.x']);
+				    		originY = 0;
+				    	}
+				    	if(markerProps['shadow.origin.y']) {
+				    		originY = new Number(markerProps['shadow.origin.y']);
+				    		if(!originX) {
+				    			originX = 0;
+				    		}
+				    	}
+				    	if(markerProps['shadow.anchor.x']) {
+				    		anchorX = new Number(markerProps['shadow.anchor.x']);
+				    		anchorY = 0;
+				    	}
+				    	if(markerProps['shadow.anchor.y']) {
+				    		anchorY = new Number(markerProps['shadow.anchor.y']);
+				    		if(!anchorX) {
+				    			anchorX = 0;
+				    		}
+				    	}
+			    		if(width || height) {
+			    			shadowSize = new google.maps.Size(width,height);
+			    		}
+			    		if(originX || originY) {
+			    			shadowOrigin = new google.maps.Point(originX,originY);
+			    		}
+			    		if(anchorX || anchorY) {
+			    			shadowAnchor = new google.maps.Point(anchorX,anchorY);
+			    		}
+			    		var customShadow = {
+				    		url: shadowUrl,
+				    		size: shadowSize,
+				    		origin: shadowOrigin,
+				    		anchor: shadowAnchor
+				    	};
+				    	markerOptions['shadow'] = customShadow;
+				    }
+				    
 				    for (j in markerProps) {
-						if (markerProps.hasOwnProperty(j) && !markerOptions.hasOwnProperty(j)) {
+						if (
+							j.indexOf("icon.") < 0 
+							&& j.indexOf("shadow.") < 0
+							&& markerProps.hasOwnProperty(j) 
+							&& !markerOptions.hasOwnProperty(j)
+							) {
 								markerOptions[j] = markerProps[j];
 						}
 					}
