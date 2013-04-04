d86a2a102fb5561ce72b7669a37062bf6b91c189
Lucian Chirita <lucianc@users.sourceforge.net>
2013-04-04 11:24:33 +0000

    key/label value wrappers (used in jrs for now)
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6047 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/engine/util/DisplayComparableValue.java
@@ -0,0 +1,70 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.util;
+
+/**
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class DisplayComparableValue<T extends Comparable<T>> extends DisplayValue<T> 
+		implements Comparable<DisplayComparableValue<T>>
+{
+
+	public DisplayComparableValue(T key, String label)
+	{
+		super(key, label);
+	}
+
+	@Override
+	public int compareTo(DisplayComparableValue<T> o)
+	{
+		T otherKey = o.key;
+		// considering nulls greater than non null values
+		int order;
+		if (key == null)
+		{
+			if (otherKey == null)
+			{
+				order = 0;
+			}
+			else
+			{
+				order = -1;
+			}
+		}
+		else
+		{
+			if (otherKey == null)
+			{
+				order = 1;
+			}
+			else
+			{
+				order = key.compareTo(otherKey);
+			}
+		}
+		return order;
+	}
+
+}
---
+++ src/net/sf/jasperreports/engine/util/DisplayValue.java
@@ -0,0 +1,75 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.util;
+
+/**
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class DisplayValue<T>
+{
+	protected final T key;
+	protected final String label;
+	
+	public DisplayValue(T key, String label)
+	{
+		this.key = key;
+		this.label = label;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return key == null ? 0 : key.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (!(obj instanceof DisplayValue))
+		{
+			return false;
+		}
+		
+		Object otherKey = ((DisplayValue<?>) obj).key;
+		return key == null ? otherKey == null
+				: (otherKey != null && key.equals(otherKey));
+	}
+
+	@Override
+	public String toString()
+	{
+		return label;
+	}
+
+	public T key()
+	{
+		return key;
+	}
+
+	public String label()
+	{
+		return label;
+	}
+}
