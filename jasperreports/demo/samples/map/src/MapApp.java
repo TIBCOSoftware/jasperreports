3562e06261c56dafb66aab7f38ef22b89b97e686
Sanda Zaharia <shertage@users.sourceforge.net>
2013-04-10 14:26:44 +0000

    sample update with new custom marker features (icons, shadows, info window); initial draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6055 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/demo/samples/map/src/MapApp.java
+++ jasperreports/demo/samples/map/src/MapApp.java
@@ -92,7 +92,7 @@ public class MapApp extends AbstractSampleApp
 	public void fill() throws JRException
 	{
 		long start = System.currentTimeMillis();
-		JasperFillManager.fillReportToFile("build/reports/MapReport.jasper", null, new JREmptyDataSource(1));
+		JasperFillManager.fillReportToFile("build/reports/MapReport.jasper", null, new JREmptyDataSource(5));
 		System.err.println("Filling time : " + (System.currentTimeMillis() - start));
 	}
 	
