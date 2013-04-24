ff27701cc44e006a24b16888d3b99105c94c1200
Teodor Danciu <teodord@users.sourceforge.net>
2013-04-24 18:20:24 +0000

    functions draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6104 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/functions/FunctionsUtil.java
@@ -0,0 +1,95 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.functions;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import net.sf.jasperreports.engine.JasperReportsContext;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id: CastorUtil.java 5880 2013-01-07 20:40:06Z teodord $
+ */
+public class FunctionsUtil
+{
+	/**
+	 * 
+	 */
+	private JasperReportsContext jasperReportsContext;
+
+
+	/**
+	 *
+	 */
+	private FunctionsUtil(JasperReportsContext jasperReportsContext)
+	{
+		this.jasperReportsContext = jasperReportsContext;
+	}
+	
+	
+	/**
+	 *
+	 */
+	public static FunctionsUtil getInstance(JasperReportsContext jasperReportsContext)
+	{
+		return new FunctionsUtil(jasperReportsContext);
+	}
+	
+	/**
+	 * 
+	 */
+	public List<FunctionsBundle> getAllFunctionBundles()
+	{
+		List<FunctionsBundle> bundles = jasperReportsContext.getExtensions(FunctionsBundle.class);
+		return bundles;
+	}
+	
+	/**
+	 * 
+	 */
+	public Class<?> getClass4Function(String functionName)
+	{
+		List<FunctionsBundle> bundles = jasperReportsContext.getExtensions(FunctionsBundle.class);
+		for (FunctionsBundle bundle : bundles)
+		{
+			List<Class<?>> classes = bundle.getFunctionClasses();
+			for (Class<?> clazz : classes)
+			{
+				Method[] methods = clazz.getMethods();
+				for (Method method : methods)
+				{
+					if (functionName.equals(method.getName()))
+					{
+						return clazz;
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+}
