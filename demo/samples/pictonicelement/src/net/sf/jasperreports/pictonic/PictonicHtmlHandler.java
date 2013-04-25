a8bd3978c611bec0242d8fd3ee7bdc864b81e8ed
Narcis Marcu <narcism@users.sourceforge.net>
2013-04-25 11:44:48 +0000

    added pictonicelement sample
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6114 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ demo/samples/pictonicelement/src/net/sf/jasperreports/pictonic/PictonicHtmlHandler.java
@@ -0,0 +1,123 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.pictonic;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.sf.jasperreports.engine.JRGenericPrintElement;
+import net.sf.jasperreports.engine.JasperReportsContext;
+import net.sf.jasperreports.engine.ReportContext;
+import net.sf.jasperreports.engine.export.GenericElementWithResourcesHtmlHandler;
+import net.sf.jasperreports.engine.export.JRHtmlExporterContext;
+import net.sf.jasperreports.pictonic.render.ResourceHandler;
+import net.sf.jasperreports.web.WebReportContext;
+import net.sf.jasperreports.web.util.VelocityUtil;
+import net.sf.jasperreports.web.util.WebUtil;
+
+/**
+ * @author Narcis Marcu (narcism@users.sourceforge.net)
+ * @version $Id$
+ */
+public class PictonicHtmlHandler implements GenericElementWithResourcesHtmlHandler
+{
+
+	private static final String PICTONIC_ELEMENT_HTML_TEMPLATE = "net/sf/jasperreports/pictonic/resources/PictonicElementHtmlTemplate.vm";
+
+	private ResourceHandler resourceHandler;
+	private Map<String, String> resources;
+	
+	private Map<String, Object> contextMap;
+	private boolean isExportToDir;
+	private File exportDir;
+	
+	
+	public void prepareForExport(JRHtmlExporterContext context, boolean isExportToDir, File exportDir, String exportDirUri)
+	{
+		this.isExportToDir = isExportToDir;
+		this.exportDir = exportDir;
+		
+		contextMap = new HashMap<String, Object>();
+		JasperReportsContext jrContext = context.getJasperReportsContext();
+		WebUtil webUtil = WebUtil.getInstance(jrContext);
+		
+		ReportContext rc = context.getExporter().getReportContext();
+		String contextPath = null;
+		if (rc != null) {
+			contextPath = (String)rc.getParameterValue(WebReportContext.APPLICATION_CONTEXT_PATH);
+		}
+		
+		for (Entry<String, String> entry: resources.entrySet()) {
+			if (isExportToDir) {
+				contextMap.put(entry.getKey(), exportDirUri + resourceHandler.getResourceName(entry.getValue()));
+			} else {
+				contextMap.put(entry.getKey(), webUtil.getResourcePath((contextPath != null ? contextPath : "") + webUtil.getResourcesBasePath(), entry.getValue()));
+			}
+		}
+	}
+
+	public String getHtmlFragment(JRHtmlExporterContext context, JRGenericPrintElement element)
+	{
+		contextMap.put("pictonicCssClass", element.getParameterValue(PictonicElement.PARAM_CSS_CLASS));
+		contextMap.put("pictonicFontSize", element.getParameterValue(PictonicElement.PARAM_FONT_SIZE));
+		return VelocityUtil.processTemplate(PictonicHtmlHandler.PICTONIC_ELEMENT_HTML_TEMPLATE, contextMap);
+	}
+
+	@Override
+	public boolean toExport(JRGenericPrintElement element) {
+		return true;
+	}
+
+	public Map<String, String> getResources() {
+		return resources;
+	}
+
+	public void setResources(Map<String, String> resources) {
+		this.resources = resources;
+	}
+	
+	public void exportResources() {
+		if (!isExportToDir) {
+			return;
+		}
+		
+		if (!exportDir.exists()) {
+			exportDir.mkdir();
+		}
+		
+		for (Entry<String, String> entry: resources.entrySet()) {
+			resourceHandler.exportResource(entry.getValue(), exportDir);
+		}
+	}
+
+	public ResourceHandler getResourceHandler() {
+		return resourceHandler;
+	}
+
+	public void setResourceHandler(ResourceHandler resourceHandler) {
+		this.resourceHandler = resourceHandler;
+	}
+}
