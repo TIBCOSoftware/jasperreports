7419bbe520866d7b2871e3c46d1d042323e96d9d
Narcis Marcu <narcism@users.sourceforge.net>
2013-04-25 11:32:58 +0000

    refactored ResourceServlet to use WebResourceHandler and ContetTypeMapping plugged in as extensions
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6113 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/web/servlets/ResourceServlet.java
+++ src/net/sf/jasperreports/web/servlets/ResourceServlet.java
@@ -24,19 +24,16 @@
 package net.sf.jasperreports.web.servlets;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRConstants;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.util.JRLoader;
-import net.sf.jasperreports.engine.util.MessageUtil;
-import net.sf.jasperreports.web.util.VelocityUtil;
+import net.sf.jasperreports.web.util.ContentTypeMapping;
+import net.sf.jasperreports.web.util.DefaultWebResourceHandler;
+import net.sf.jasperreports.web.util.WebResourceHandler;
 import net.sf.jasperreports.web.util.WebUtil;
 
 
@@ -55,51 +52,45 @@ public class ResourceServlet extends AbstractServlet
 	{
 		WebUtil webUtil = WebUtil.getInstance(getJasperReportsContext());
 		String resource = webUtil.getResourceUri(request);
-		boolean isDynamicResource = webUtil.isDynamicResource(request);
-		String resourceBundleName = webUtil.getResourceBundleForResource(request);
-		Locale locale = webUtil.getResourceLocale(request);
 		
-		try {
-			byte[] bytes = null;
-			
-			if(
-				resource != null 
-				&& resource.indexOf(".vm.") != -1 
-				&& (isDynamicResource || resourceBundleName != null || locale != null)
-				) 
-			{
-				Map<String, Object> contextMap = new HashMap<String, Object>();
-				contextMap.put("path", request.getContextPath() + webUtil.getResourcesBasePath());
-				locale = locale == null ? Locale.getDefault() : locale;
-				contextMap.put("msgProvider", MessageUtil.getInstance(getJasperReportsContext()).getLocalizedMessageProvider(resourceBundleName, locale)); 
-				String resourceString = VelocityUtil.processTemplate(resource, contextMap);
-				if (resourceString != null) {
-					bytes = resourceString.getBytes("UTF-8");
-				}
-			} else 
-			{
-				bytes = JRLoader.loadBytesFromResource(resource);
-			}
-			
-			if (resource.endsWith(".js")) {//FIXMEJIVE revisit this
-				response.setContentType("text/javascript; charset=UTF-8");
-			} else if (resource.endsWith(".css")) {
-				response.setContentType("text/css");
-			}
-			
-			// Set to expire far in the past.
-			response.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
-			// Set standard HTTP/1.1 no-cache headers.
-			response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-			// Set IE extended HTTP/1.1 no-cache headers (use addHeader).
-			response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-			// Set standard HTTP/1.0 no-cache header.
-			response.setHeader("Pragma", "no-cache");
+		WebResourceHandler webResourceHandler = null;
 
-			response.getOutputStream().write(bytes);
-		} catch (JRException e) {
-			e.printStackTrace();
+		List<WebResourceHandler> resourceHandlers = getJasperReportsContext().getExtensions(WebResourceHandler.class);
+		if (resourceHandlers != null) {
+			for (WebResourceHandler handler: resourceHandlers) {
+				if (handler.hadlesResource(resource)) {
+					webResourceHandler = handler;
+					break;
+				}
+			}
 		}
+		
+		if (webResourceHandler == null) {
+			webResourceHandler = DefaultWebResourceHandler.getInstance();
+		}
+		
+		byte[] bytes = webResourceHandler.getData(resource, request, getJasperReportsContext());
+		String resourceType = webResourceHandler.getResourceType(resource);
+		
+		if (resourceType != null) {
+			List<ContentTypeMapping> contentTypeMappings = getJasperReportsContext().getExtensions(ContentTypeMapping.class);
+			for (ContentTypeMapping contentTypeMapping : contentTypeMappings) {
+				if (resourceType.equals(contentTypeMapping.getFileType())) {
+					response.setContentType(contentTypeMapping.getContentType());
+				}
+			}
+		}
+
+		// Set to expire far in the past.
+		response.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
+		// Set standard HTTP/1.1 no-cache headers.
+		response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+		// Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+		response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+		// Set standard HTTP/1.0 no-cache header.
+		response.setHeader("Pragma", "no-cache");
+
+		response.getOutputStream().write(bytes);
 	}
 	
 }
---
+++ src/net/sf/jasperreports/web/util/ContentTypeMappingExtensionsRegistryFactory.java
@@ -0,0 +1,63 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.web.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.jasperreports.engine.JRPropertiesMap;
+import net.sf.jasperreports.engine.JRPropertiesUtil;
+import net.sf.jasperreports.engine.JRPropertiesUtil.PropertySuffix;
+import net.sf.jasperreports.extensions.DefaultExtensionsRegistry;
+import net.sf.jasperreports.extensions.ExtensionsRegistry;
+import net.sf.jasperreports.extensions.ExtensionsRegistryFactory;
+import net.sf.jasperreports.extensions.ListExtensionRegistry;
+
+/**
+ * @author Narcis Marcu (narcism@users.sourceforge.net)
+ * @version $Id$
+ */
+public class ContentTypeMappingExtensionsRegistryFactory implements ExtensionsRegistryFactory {
+
+	/**
+	 * 
+	 */
+	public final static String CONTENT_TYPE_MAPPING_PROPERTY_PREFIX = DefaultExtensionsRegistry.PROPERTY_REGISTRY_PREFIX + "content.type.mapping.";
+
+	/**
+	 * 
+	 */
+	public ExtensionsRegistry createRegistry(String registryId, JRPropertiesMap properties) {
+		List<PropertySuffix> contentTypeMappingProperties = JRPropertiesUtil.getProperties(properties, CONTENT_TYPE_MAPPING_PROPERTY_PREFIX);
+		List<ContentTypeMapping> contentTypeMappings = new ArrayList<ContentTypeMapping>();
+		for (Iterator<PropertySuffix> it = contentTypeMappingProperties.iterator(); it.hasNext();) {
+			PropertySuffix contentTypeMappingProp = it.next();
+			contentTypeMappings.add(new ContentTypeMapping(contentTypeMappingProp.getSuffix(),contentTypeMappingProp.getValue()));
+		}
+		
+		return new ListExtensionRegistry<ContentTypeMapping>(ContentTypeMapping.class, contentTypeMappings);
+	}
+
+}
