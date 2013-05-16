8ba7d8ed6db4239e505ffb40fa2e77bf10615cae
Sanda Zaharia <shertage@users.sourceforge.net>
2013-05-16 11:05:31 +0000

    moving functions into jasperreports.map
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6165 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/components/map/resources/map.js
+++ src/net/sf/jasperreports/components/map/resources/map.js
@@ -28,8 +28,41 @@ var infowindow;
 					zoom: zoom
 			};
 		},
+		configureImage: function (pk, pp, po) {
+			var width, height, originX, originY, anchorX, anchorY;
+			
+			width = pp[pk + '.width'] ? parseInt(pp[pk + '.width']) : null;
+			height = pp[pk + '.height'] ? parseInt(pp[pk + '.height']) : null;
+			
+			originX = pp[pk + '.origin.x'] ? parseInt(pp[pk + '.origin.x']) : 0;
+			originY = pp[pk + '.origin.y'] ? parseInt(pp[pk + '.origin.y']) : 0;
+
+			anchorX = pp[pk + '.anchor.x'] ? parseInt(pp[pk + '.anchor.x']) : 0;
+			anchorY = pp[pk + '.anchor.y'] ? parseInt(pp[pk + '.anchor.y']) : 0;
+			
+			po[pk] = {
+				url: pp[pk + '.url'],
+				size: width && height ? new google.maps.Size(width, height) : null,
+				origin: new google.maps.Point(originX,originY),
+				anchor: new google.maps.Point(anchorX,anchorY)
+			};
+		},
+		createInfo: function (pp) {
+			if(pp['infowindow.content'] && pp['infowindow.content'].length > 0) {
+				var gg= google.maps,
+			    po = {
+					content: pp['infowindow.content']	
+				};
+				if (pp['infowindow.pixelOffset']) po['pixelOffset'] = pp['infowindow.pixelOffset'];
+				if (pp['infowindow.latitude'] && pp['infowindow.longitude']) po['position'] = new gg.LatLng(pp['infowindow.latitude'], pp['infowindow.longitude']);
+				if (pp['infowindow.maxWidth']) po['maxWidth'] = pp['infowindow.maxWidth'];
+				return new gg.InfoWindow(po);	
+			}
+			return null;
+		},
 		showMap: function(canvasId, latitude, longitude, zoom, mapType, markers) {
 			var gg = google.maps,
+				jm = global.jasperreports.map,
 				myOptions = {
 					zoom: zoom,
 					center: new gg.LatLng(latitude, longitude), 
@@ -46,16 +79,16 @@ var infowindow;
 					        position: markerLatLng,
 					        map: map
 					    };
-				    if(markerProps['icon.url'] && markerProps['icon.url'].length > 0) configureImage('icon', markerProps, markerOptions);
+				    if(markerProps['icon.url'] && markerProps['icon.url'].length > 0) jm.configureImage('icon', markerProps, markerOptions);
 				    else if (markerProps['icon'] && markerProps['icon'].length > 0) markerOptions['icon'] = markerProps['icon'];
 				    else if (markerProps['color'] && markerProps['color'].length > 0) markerOptions['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7C' + markerProps['color'];
-				    if(markerProps['shadow.url'] && markerProps['shadow.url'].length > 0) configureImage('shadow', markerProps, markerOptions);
+				    if(markerProps['shadow.url'] && markerProps['shadow.url'].length > 0) jm.configureImage('shadow', markerProps, markerOptions);
 				    for (j in markerProps) {
 						if (j.indexOf(".") < 0 && markerProps.hasOwnProperty(j) && !markerOptions.hasOwnProperty(j)) markerOptions[j] = markerProps[j];
 					}
 				    var marker = new google.maps.Marker(markerOptions);
-				    marker['info'] = createInfo(markerProps);
-					google.maps.event.addListener(marker, 'click', function() {
+				    marker['info'] = jm.createInfo(markerProps);
+					gg.event.addListener(marker, 'click', function() {
 						if(map.autocloseinfo && infowindow) infowindow.close();
 						if(this['info']) {
 							infowindow = this['info'];
@@ -67,37 +100,3 @@ var infowindow;
 		}
 	};
 } (this));
-
-function configureImage (pk, pp, po) {
-	var width, height, originX, originY, anchorX, anchorY;
-	
-	width = pp[pk + '.width'] ? parseInt(pp[pk + '.width']) : null;
-	height = pp[pk + '.height'] ? parseInt(pp[pk + '.height']) : null;
-	
-	originX = pp[pk + '.origin.x'] ? parseInt(pp[pk + '.origin.x']) : 0;
-	originY = pp[pk + '.origin.y'] ? parseInt(pp[pk + '.origin.y']) : 0;
-
-	anchorX = pp[pk + '.anchor.x'] ? parseInt(pp[pk + '.anchor.x']) : 0;
-	anchorY = pp[pk + '.anchor.y'] ? parseInt(pp[pk + '.anchor.y']) : 0;
-	
-	po[pk] = {
-		url: pp[pk + '.url'],
-		size: width && height ? new google.maps.Size(width, height) : null,
-		origin: new google.maps.Point(originX,originY),
-		anchor: new google.maps.Point(anchorX,anchorY)
-	};
-}
-
-function createInfo(pp) {
-	if(pp['infowindow.content'] && pp['infowindow.content'].length > 0) {
-		var gg= google.maps,
-	    po = {
-			content: pp['infowindow.content']	
-		};
-		if (pp['infowindow.pixelOffset']) po['pixelOffset'] = pp['infowindow.pixelOffset'];
-		if (pp['infowindow.latitude'] && pp['infowindow.longitude']) po['position'] = new gg.LatLng(pp['infowindow.latitude'], pp['infowindow.longitude']);
-		if (pp['infowindow.maxWidth']) po['maxWidth'] = pp['infowindow.maxWidth'];
-		return new gg.InfoWindow(po);	
-	}
-	return null;
-}
