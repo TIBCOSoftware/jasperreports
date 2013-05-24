ae4c8761f29d71615a580603cc5ff98e09a44e28
Teodor Danciu <teodord@users.sourceforge.net>
2013-05-24 20:09:08 +0000

    secrets storage extension added
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6180 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/util/SecretsUtil.java
@@ -0,0 +1,78 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Contributors:
+ * Gaganis Giorgos - gaganis@users.sourceforge.net
+ */
+package net.sf.jasperreports.util;
+
+import java.util.List;
+
+import net.sf.jasperreports.engine.JRRuntimeException;
+import net.sf.jasperreports.engine.JasperReportsContext;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id: MessageUtil.java 5878 2013-01-07 20:23:13Z teodord $
+ */
+public final class SecretsUtil
+{
+	private final JasperReportsContext jasperReportsContext;
+	
+	/**
+	 * 
+	 */
+	private SecretsUtil(JasperReportsContext jasperReportsContext)
+	{
+		this.jasperReportsContext = jasperReportsContext;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public static final SecretsUtil getInstance(JasperReportsContext jasperReportsContext)
+	{
+		return new SecretsUtil(jasperReportsContext);
+	}
+	
+	/**
+	 *
+	 */
+	public String getSecret(String category, String key)
+	{
+		List<SecretsStorageFactory> factories = jasperReportsContext.getExtensions(SecretsStorageFactory.class);
+		for (SecretsStorageFactory factory : factories)
+		{
+			SecretsStorage storage = factory.getSecretsStorage(category);
+			if (storage != null && storage.hasSecret(key))
+			{
+				return storage.getSecret(key);
+			}
+		}
+		throw new JRRuntimeException("Secret value for '" + key + "' key not found in any '" + category + "' category storage.");
+	}
+}
