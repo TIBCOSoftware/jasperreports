00721418b5ba90db7a6fdee626dad71b22ff29cb
Narcis Marcu <narcism@users.sourceforge.net>
2013-07-15 09:24:20 +0000

    basic support for JSON export
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6316 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/engine/export/JsonExporter.java
@@ -0,0 +1,352 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.export;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.jasperreports.engine.DefaultJasperReportsContext;
+import net.sf.jasperreports.engine.JRAbstractExporter;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JRGenericPrintElement;
+import net.sf.jasperreports.engine.JRPrintElement;
+import net.sf.jasperreports.engine.JRPrintFrame;
+import net.sf.jasperreports.engine.JRPrintPage;
+import net.sf.jasperreports.engine.JRPropertiesUtil;
+import net.sf.jasperreports.engine.JasperReportsContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Lucian Chirita (lucianc@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JsonExporter extends JRAbstractExporter
+{
+	
+	private static final Log log = LogFactory.getLog(JsonExporter.class);
+	
+	public static final String JSON_EXPORTER_KEY = JRPropertiesUtil.PROPERTY_PREFIX + "json";
+	
+	protected static final String JSON_EXPORTER_PROPERTIES_PREFIX = JRPropertiesUtil.PROPERTY_PREFIX + "export.json.";
+	
+	protected JRExportProgressMonitor progressMonitor;
+	protected String encoding;
+	protected boolean flushOutput;
+	protected Writer writer;
+	protected int reportIndex;
+	protected int pageIndex;
+	private boolean gotFirstJsonFragment;
+	
+	protected JsonExporterContext exporterContext = new ExporterContext();
+	
+	public JsonExporter()
+	{
+		this(DefaultJasperReportsContext.getInstance());
+	}
+
+	public JsonExporter(JasperReportsContext jasperReportsContext)
+	{
+		super(jasperReportsContext);
+	}
+	
+	@Override
+	protected String getExporterKey()
+	{
+		return JSON_EXPORTER_KEY;
+	}
+
+	@Override
+	public void exportReport() throws JRException
+	{
+		progressMonitor = (JRExportProgressMonitor)parameters.get(JRExporterParameter.PROGRESS_MONITOR);
+
+		setOffset();
+		try
+		{
+			setExportContext();
+			setInput();
+			
+			if (!parameters.containsKey(JRExporterParameter.FILTER))
+			{
+				filter = createFilter(JSON_EXPORTER_PROPERTIES_PREFIX);
+			}
+
+			/*   */
+			if (!isModeBatch)
+			{
+				setPageRange();
+			}
+	
+			encoding = 
+				getStringParameterOrDefault(
+					JRExporterParameter.CHARACTER_ENCODING, 
+					JRExporterParameter.PROPERTY_CHARACTER_ENCODING
+					);
+			
+			flushOutput = getBooleanParameter(JRHtmlExporterParameter.FLUSH_OUTPUT, 
+					JRHtmlExporterParameter.PROPERTY_FLUSH_OUTPUT, 
+					true);
+
+			//FIXMENOW check all exporter properties that are supposed to work at report level
+			
+			StringBuffer sb = (StringBuffer)parameters.get(JRExporterParameter.OUTPUT_STRING_BUFFER);
+			if (sb != null)
+			{
+				try
+				{
+					writer = new StringWriter();
+					exportReportToWriter();
+					sb.append(writer.toString());
+				}
+				catch (IOException e)
+				{
+					throw new JRException("Error writing to StringBuffer writer : " + jasperPrint.getName(), e);
+				}
+				finally
+				{
+					if (writer != null)
+					{
+						try
+						{
+							writer.close();
+						}
+						catch(IOException e)
+						{
+						}
+					}
+				}
+			}
+			else
+			{
+				writer = (Writer)parameters.get(JRExporterParameter.OUTPUT_WRITER);
+				if (writer != null)
+				{
+					try
+					{
+						exportReportToWriter();
+					}
+					catch (IOException e)
+					{
+						throw new JRException("Error writing to writer : " + jasperPrint.getName(), e);
+					}
+				}
+				else
+				{
+					OutputStream os = (OutputStream)parameters.get(JRExporterParameter.OUTPUT_STREAM);
+					if (os != null)
+					{
+						try
+						{
+							writer = new OutputStreamWriter(os, encoding);
+							exportReportToWriter();
+						}
+						catch (IOException e)
+						{
+							throw new JRException("Error writing to OutputStream writer : " + jasperPrint.getName(), e);
+						}
+					}
+					else
+					{
+						File destFile = (File)parameters.get(JRExporterParameter.OUTPUT_FILE);
+						if (destFile == null)
+						{
+							String fileName = (String)parameters.get(JRExporterParameter.OUTPUT_FILE_NAME);
+							if (fileName != null)
+							{
+								destFile = new File(fileName);
+							}
+							else
+							{
+								throw new JRException("No output specified for the exporter.");
+							}
+						}
+	
+						try
+						{
+							os = new FileOutputStream(destFile);
+							writer = new OutputStreamWriter(os, encoding);
+						}
+						catch (IOException e)
+						{
+							throw new JRException("Error creating to file writer : " + jasperPrint.getName(), e);
+						}
+	
+						try
+						{
+							exportReportToWriter();
+						}
+						catch (IOException e)
+						{
+							throw new JRException("Error writing to file writer : " + jasperPrint.getName(), e);
+						}
+						finally
+						{
+							if (writer != null)
+							{
+								try
+								{
+									writer.close();
+								}
+								catch(IOException e)
+								{
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		finally
+		{
+			resetExportContext();
+		}
+	}
+	
+	protected void exportReportToWriter() throws JRException, IOException
+	{
+		writer.write("{\n");
+
+		for(reportIndex = 0; reportIndex < jasperPrintList.size(); reportIndex++)
+		{
+			setJasperPrint(jasperPrintList.get(reportIndex));
+
+			List<JRPrintPage> pages = jasperPrint.getPages();
+			if (pages != null && pages.size() > 0)
+			{
+				if (isModeBatch)
+				{
+					startPageIndex = 0;
+					endPageIndex = pages.size() - 1;
+				}
+
+				JRPrintPage page = null;
+				for(pageIndex = startPageIndex; pageIndex <= endPageIndex; pageIndex++)
+				{
+					if (Thread.interrupted())
+					{
+						throw new JRException("Current thread interrupted.");
+					}
+
+					page = pages.get(pageIndex);
+
+					exportPage(page);
+
+					if (reportIndex < jasperPrintList.size() - 1 || pageIndex < endPageIndex)
+					{
+						writer.write("\n");
+					}
+				}
+			}
+		}
+
+		writer.write("\n}");
+
+		if (flushOutput)
+		{
+			writer.flush();
+		}
+	}
+	
+	protected void exportPage(JRPrintPage page) throws IOException
+	{
+		Collection<JRPrintElement> elements = page.getElements();
+		exportElements(elements);
+	}
+	
+	protected void exportElements(Collection<JRPrintElement> elements) throws IOException
+	{
+		if (elements != null && elements.size() > 0)
+		{
+			for(Iterator<JRPrintElement> it = elements.iterator(); it.hasNext();)
+			{
+				JRPrintElement element = it.next();
+
+				if (filter == null || filter.isToExport(element))
+				{
+					if (element instanceof JRPrintFrame)
+					{
+						exportFrame((JRPrintFrame)element);
+					}
+					else if (element instanceof JRGenericPrintElement)
+					{
+						exportGenericElement((JRGenericPrintElement) element);
+					}
+				}
+			}
+		}
+	}
+	
+	protected void exportFrame(JRPrintFrame frame) throws IOException
+	{
+		exportElements(frame.getElements());
+	}
+
+	/**
+	 *
+	 */
+	protected void exportGenericElement(JRGenericPrintElement element) throws IOException
+	{
+		GenericElementJsonHandler handler = (GenericElementJsonHandler) 
+				GenericElementHandlerEnviroment.getInstance(getJasperReportsContext()).getElementHandler(
+						element.getGenericType(), JSON_EXPORTER_KEY);
+		
+		if (handler != null)
+		{
+			if (gotFirstJsonFragment) {
+				writer.write(",\n");
+			} else {
+				gotFirstJsonFragment = true;
+			}
+			writer.write(handler.getJsonFragment(exporterContext, element));
+		}
+		else
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("No JSON generic element handler for " 
+						+ element.getGenericType());
+			}
+		}
+	}
+	
+	protected class ExporterContext extends BaseExporterContext implements JsonExporterContext
+	{
+		public String getExportPropertiesPrefix()
+		{
+			return JSON_EXPORTER_PROPERTIES_PREFIX;
+		}
+	}
+
+}
