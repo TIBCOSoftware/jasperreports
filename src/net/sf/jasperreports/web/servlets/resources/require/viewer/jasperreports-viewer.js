f5b05956afc81ad49ef413435de022e994a41cf4
Narcis Marcu <narcism@users.sourceforge.net>
2013-07-16 08:43:54 +0000

    added support for events
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6322 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/web/servlets/resources/require/viewer/jasperreports-viewer.js
+++ src/net/sf/jasperreports/web/servlets/resources/require/viewer/jasperreports-viewer.js
@@ -17,38 +17,83 @@ define(["jasperreports-report", "jquery"], function(Report, $) {
         loadReport: function() {
             var it = this;
 
-            if (it.config.toolbar) {
-                // register toolbar
-            }
-
             it.reportInstance = new Report({
                 reporturi: it.config.reporturi,
                 async: it.config.async,
                 page: it.config.page
             });
 
-            it.reportInstance.init().then(
-                function(html) {
-                    it._render(html);
-                    console.log("report rendered");
-                },
-                function(err) {
-                    console.error(err);
-                }
-            );
+            it._setupEventsForReport(it.reportInstance);
 
-            return it.reportInstance;
+            return it.reportInstance.init();
         },
+
+        /*
+         Series of test that can be called from the console.
+         */
+        test: function(testName) {
+            var Report = this.reportInstance;
+            console.info(Report);
+
+            switch(testName) {
+                case 'goto':
+                    Report.gotoPage(1);
+
+                    return 'Test description: navigate to 2nd page.';
+                    break;
+                case 'sort':
+                    Report.components.table[0].column[0].sort({order: 'Asc'});
+
+                    return 'Test description: sort 1st column in ascending order.';
+                    break;
+                case 'move':
+                    Report.components.table[0].column[1].move({index: 3});
+
+                    return 'Test description: move 2nd column to 4th position.';
+                    break;
+                case 'resize':
+                    Report.components.table[0].column[0].resize({width: 200});
+
+                    return 'Test description: resize 1st column to 200.';
+                    break;
+                case 'filter':
+                    Report.components.table[0].column[1].filter({fieldValueStart: 'Robert', filterTypeOperator: 'CONTAINS'});
+
+                    return 'Test description: filter 2nd column for "Roberts"';
+                    break;
+                case 'hide':
+                    Report.components.table[0].column[1].hide();
+
+                    return 'Test description: hide 2nd column';
+                    break;
+                case 'unhide':
+                    Report.components.table[0].column[1].unhide();
+
+                    return 'Test description: unhide 2nd column';
+                    break;
+            }
+        },
+
+        // internal functions
         _render: function(htmlOutput) {
             var it = this;
             // place output into container
             var container = $('#' + it.config.at);
-
             if (container.size() === 0) {
                 container = $('.' + it.config.at);
             }
-
             container.html(htmlOutput);
+        },
+        _setupEventsForReport: function(report) {
+            var it = this;
+
+            report.on("reportHtmlReady", function() {
+                it._render(this.html);
+            }).on("undo, redo, undoall, action", function() {
+                this.gotoPage(0);
+            }).on("pageModified", function() {
+                this.refreshPage(this.currentpage);
+            });
         }
     };
 
