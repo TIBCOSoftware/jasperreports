d89f51820954a0457916e6aff3a79bcdcedc42d3
Narcis Marcu <narcism@users.sourceforge.net>
2013-08-14 09:29:48 +0000

    fixed check for pageModified
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6382 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/web/servlets/resources/require/report/jasperreports-status-checker.js
+++ jasperreports/src/net/sf/jasperreports/web/servlets/resources/require/report/jasperreports-status-checker.js
@@ -12,7 +12,7 @@ define(["jquery-1.10.2"], function($) {
             var it = this,
                 deferred = $.Deferred();
 
-            it._timedCheckPageModified(false, page, pageTimestamp, deferred);
+            it._timedCheckPageModified(false, page, pageTimestamp, deferred, null);
 
             return deferred.promise();
         },
@@ -21,21 +21,21 @@ define(["jquery-1.10.2"], function($) {
         },
 
         // internal functions
-        _timedCheckPageModified: function(booleanDone, pageNo, pageTimestamp, deferredObject) {
+        _timedCheckPageModified: function(booleanDone, pageNo, pageTimestamp, deferredObject, statusResult) {
             var it = this;
             if (!booleanDone) {
             	it.timeoutId = setTimeout(function() {
         			it._getPageModifiedStatus(pageNo, pageTimestamp, deferredObject);
         		}, 3000);
             } else {
-                deferredObject.resolve();
+                deferredObject.resolve(statusResult);
             }
         },
         _getPageModifiedStatus: function(page, pageTimestamp, deferredObject) {
             var it = this;
             return it.loader.getStatusForPage(page, pageTimestamp)
                 .then(function(jsonData, textStatus, jqHXR) {
-                    it._timedCheckPageModified(jsonData.result.pageModified, page, pageTimestamp, deferredObject);
+                    it._timedCheckPageModified(jsonData.result.pageModified || jsonData.result.status == "finished", page, pageTimestamp, deferredObject, jsonData.result);
                 });
         }
 	};
