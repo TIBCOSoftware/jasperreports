b03ec794aeb9852634af8488bf9e1b38baa2d873
Lucian Chirita <lucianc@users.sourceforge.net>
2013-09-17 19:46:00 +0000

    bug 33551 - configurable bundle name for jive resource handler
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6518 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/web/util/WebResourceHandlersExtensionRegistryFactory.java
+++ src/net/sf/jasperreports/web/util/WebResourceHandlersExtensionRegistryFactory.java
@@ -44,7 +44,7 @@ public class WebResourceHandlersExtensionRegistryFactory implements ExtensionsRe
 	{
 		List<WebResourceHandler> extensions = new ArrayList<WebResourceHandler>();
 
-		JiveWebResourceHandler jiveHandler = new JiveWebResourceHandler();
+		JiveWebResourceHandler jiveHandler = new JiveWebResourceHandler("net.sf.jasperreports.components.headertoolbar.messages");
 		jiveHandler.addMapping("jive.templates.tmpl", "net/sf/jasperreports/components/headertoolbar/htmlv2/resources/require/jive.templates.tmpl");
 		jiveHandler.addMapping("jive.vm.css", "net/sf/jasperreports/components/headertoolbar/resources/jive.vm.css");
 		jiveHandler.addMapping("jive.i18n.vm.js", "net/sf/jasperreports/components/headertoolbar/htmlv2/resources/require/jive.i18n.vm.js");
