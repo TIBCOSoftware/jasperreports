28739ceaa762f53609a26220cc4f02930e25bb3e
Teodor Danciu <teodord@users.sourceforge.net>
2013-09-27 10:57:04 +0000

    map based html resource handler added
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6546 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/engine/export/MapHtmlResourceHandler.java
@@ -0,0 +1,108 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.engine.export;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class MapHtmlResourceHandler implements HtmlResourceHandler 
+{
+	/**
+	 * 
+	 */
+	private HtmlResourceHandler parentHandler;
+	private Map<String, byte[]> resourcesMap;
+
+	/**
+	 * 
+	 */
+	public MapHtmlResourceHandler(HtmlResourceHandler parentHandler, Map<String, byte[]> resourcesMap)
+	{
+		this.parentHandler = parentHandler;
+		this.resourcesMap = resourcesMap;
+	}
+
+	/**
+	 * 
+	 */
+	public MapHtmlResourceHandler(HtmlResourceHandler parentHandler)
+	{
+		this(parentHandler, new HashMap<String, byte[]>());
+	}
+
+	/**
+	 * 
+	 */
+	public MapHtmlResourceHandler(Map<String, byte[]> resourcesMap)
+	{
+		this(null, resourcesMap);
+	}
+
+	/**
+	 * 
+	 */
+	public MapHtmlResourceHandler()
+	{
+		this((HtmlResourceHandler)null);
+	}
+
+	/**
+	 * 
+	 */
+	public String getResourcePath(String id)
+	{
+		if (parentHandler == null)
+		{
+			return id;
+		}
+		return parentHandler.getResourcePath(id);
+	}
+
+	/**
+	 * 
+	 */
+	public void handleResource(String id, byte[] data)
+	{
+		if (parentHandler != null)
+		{
+			parentHandler.handleResource(id, data);
+		}
+		if (resourcesMap != null)
+		{
+			resourcesMap.put(id, data);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public Map<String, byte[]> getResourcesMap()
+	{
+		return resourcesMap;
+	}
+}
