fbfed211a267b7c5cbb1c8686295408599d98d0b
Teodor Danciu <teodord@users.sourceforge.net>
2013-10-14 11:00:18 +0000

    version update
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6585 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- src/net/sf/jasperreports/engine/JRConstants.java
+++ src/net/sf/jasperreports/engine/JRConstants.java
@@ -104,5 +104,5 @@ public interface JRConstants
 	
 	public static final String VERSION_5_1_2 = "5.1.2";
 	
-	public static final String VERSION_5_3_0 = "5.3.0";
+	public static final String VERSION_5_5_0 = "5.5.0";
 }
