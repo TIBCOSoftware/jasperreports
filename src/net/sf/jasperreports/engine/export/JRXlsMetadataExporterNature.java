ce9ebb0a5a104ea7de8cbecfb290c66b545d0acb
Sanda Zaharia <shertage@users.sourceforge.net>
2013-10-30 13:24:17 +0000

    XLS Metadata exporter based on Apache POI library
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6646 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/engine/export/JRXlsMetadataExporterNature.java
@@ -0,0 +1,60 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Contributors:
+ * Greg Hilton 
+ */
+
+package net.sf.jasperreports.engine.export;
+
+import net.sf.jasperreports.engine.JRPrintElement;
+import net.sf.jasperreports.engine.JasperReportsContext;
+
+
+/**
+ * @author sanda zaharia (shertage@users.sourceforge.net)
+ * @version $Id$
+ */
+public class JRXlsMetadataExporterNature extends JRXlsExporterNature
+{
+
+	/**
+	 * 
+	 */
+	public JRXlsMetadataExporterNature(
+		JasperReportsContext jasperReportsContext,
+		ExporterFilter filter, 
+		boolean isIgnoreGraphics, 
+		boolean isIgnorePageMargins
+		)
+	{
+		super(jasperReportsContext, filter, isIgnoreGraphics, isIgnorePageMargins);
+	}
+
+	public boolean isToExport(JRPrintElement element)
+	{
+		return element.hasProperties() && element.getPropertiesMap().containsProperty(JRXlsAbstractMetadataExporterParameter.PROPERTY_COLUMN_NAME) && super.isToExport(element);
+	}
+}
