17fd0b9e478ed102eea07db5acd4a435830dc612
Teodor Danciu <teodord@users.sourceforge.net>
2013-10-30 13:13:58 +0000

    exporter configuration refactoring draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6645 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JRCsvExporterParameter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRCsvExporterParameter.java
@@ -29,16 +29,11 @@
 package net.sf.jasperreports.engine.export;
 
 import net.sf.jasperreports.engine.JRExporterParameter;
-import net.sf.jasperreports.engine.JRPropertiesUtil;
+import net.sf.jasperreports.export.CsvExporterConfiguration;
 
 
 /**
- * Contains parameters useful for export in CSV format.
- * <p>
- * The CSV exporter can send data to a string buffer, output stream, character stream or file on disk. The engine looks
- * among the export parameters in order to find the selected output type in this order: OUTPUT_STRING_BUFFER, OUTPUT_WRITER,
- * OUTPUT_STREAM, OUTPUT_FILE, OUTPUT_FILE_NAME.
- *
+ * @deprecated Replaced by {@link CsvExporterConfiguration}.
  * @author Teodor Danciu (teodord@users.sourceforge.net)
  * @version $Id$
  */
@@ -56,33 +51,27 @@ public class JRCsvExporterParameter extends JRExporterParameter
 
 
 	/**
-	 * A string representing the character or sequence of characters used to delimit two fields on the same line. The
-	 * default value is a comma.
+	 * @deprecated Replaced by {@link CsvExporterConfiguration#getFieldDelimiter()}.
 	 */
 	public static final JRCsvExporterParameter FIELD_DELIMITER = new JRCsvExporterParameter("Field Delimiter");
 
 
 	/**
-	 * Property whose value is used as default for the {@link #FIELD_DELIMITER FIELD_DELIMITER} export parameter.
-	 * 
-	 * @see JRPropertiesUtil
+	 * @deprecated Replaced by {@link CsvExporterConfiguration#PROPERTY_FIELD_DELIMITER}.
 	 */
-	public static final String PROPERTY_FIELD_DELIMITER = JRPropertiesUtil.PROPERTY_PREFIX + "export.csv.field.delimiter";
+	public static final String PROPERTY_FIELD_DELIMITER = CsvExporterConfiguration.PROPERTY_FIELD_DELIMITER;
 
 
 	/**
-	 * A string representing the character or sequence of characters used to delimit two lines. The default value is a
-	 * character return (\n).
+	 * @deprecated Replaced by {@link CsvExporterConfiguration#getRecordDelimiter()}.
 	 */
 	public static final JRCsvExporterParameter RECORD_DELIMITER = new JRCsvExporterParameter("Record Delimiter");
 	
 	
 	/**
-	 * Property whose value is used as default for the {@link #RECORD_DELIMITER RECORD_DELIMITER} export parameter.
-	 * 
-	 * @see JRPropertiesUtil
+	 * @deprecated Replaced by {@link CsvExporterConfiguration#PROPERTY_RECORD_DELIMITER}.
 	 */
-	public static final String PROPERTY_RECORD_DELIMITER = JRPropertiesUtil.PROPERTY_PREFIX + "export.csv.record.delimiter";
+	public static final String PROPERTY_RECORD_DELIMITER = CsvExporterConfiguration.PROPERTY_RECORD_DELIMITER;
 
 
 }
