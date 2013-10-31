14f89aad3992eb5e3c86907b37081ddc660a987d
Teodor Danciu <teodord@users.sourceforge.net>
2013-10-31 11:09:21 +0000

    exporter configuration refactoring draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6659 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JRGraphics2DExporterParameter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRGraphics2DExporterParameter.java
@@ -24,11 +24,14 @@
 package net.sf.jasperreports.engine.export;
 
 import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.export.Graphics2DExporterConfiguration;
+import net.sf.jasperreports.export.Graphics2DExporterOutput;
 
 
 /**
  * Contains parameters useful for export to an AWT <tt>Graphics2D</tt> object.
  *
+ * @deprecated Replaced by {@link Graphics2DExporterConfiguration} and {@link Graphics2DExporterOutput}.
  * @author Teodor Danciu (teodord@users.sourceforge.net)
  * @version $Id$
  */
@@ -46,30 +49,17 @@ public class JRGraphics2DExporterParameter extends JRExporterParameter
 
 
 	/**
-	 * The <tt>java.awt.Graphics2D</tt> instance used for export.
+	 * @deprecated Replaced by {@link Graphics2DExporterOutput#getGraphics2D()}.
 	 */
 	public static final JRGraphics2DExporterParameter GRAPHICS_2D = new JRGraphics2DExporterParameter("Graphics2D");
 
 	/**
-	 * The zoom ratio used for the export. The default value is 1.
+	 * @deprecated Replaced by {@link Graphics2DExporterConfiguration#getZoomRatio()}.
 	 */
 	public static final JRGraphics2DExporterParameter ZOOM_RATIO = new JRGraphics2DExporterParameter("Zoom Ratio");
 
 	/**
-	 * Flag to control the use of an AWT rendering fix which causes the printer job size to be reduced when
-	 * the exporter draws onto a printer graphic context.
-	 *
-	 * The fix was introduced to solve an old Java printing problem related to the size of printer spool jobs.
-	 * However, it causes problems when bidirectional text is rendered, by losing text direction information.
-	 *
-	 * This flag is true, by default and should be set to false when bidirectional writing is present in
-	 * the document that is sent to the printer.
-	 *
-	 * This flag can be set system-wide using the
-	 * {@link net.sf.jasperreports.engine.export.JRGraphics2DExporter#MINIMIZE_PRINTER_JOB_SIZE MINIMIZE_PRINTER_JOB_SIZE} property.
-	 * This export parameter overrides the property value.
-	 *
-	 * @see net.sf.jasperreports.engine.export.JRGraphics2DExporter#MINIMIZE_PRINTER_JOB_SIZE
+	 * @deprecated Replaced by {@link Graphics2DExporterConfiguration#isMinimizePrinterJobSize()}.
 	 */
 	public static final JRGraphics2DExporterParameter MINIMIZE_PRINTER_JOB_SIZE = new JRGraphics2DExporterParameter("Minimize Printer Job Size");
 
---
+++ jasperreports/src/net/sf/jasperreports/export/Graphics2DExporterConfiguration.java
@@ -0,0 +1,72 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.export;
+
+import net.sf.jasperreports.engine.JRPropertiesUtil;
+import net.sf.jasperreports.engine.export.JRGraphics2DExporter;
+import net.sf.jasperreports.engine.util.JRStyledText;
+import net.sf.jasperreports.export.annotations.ExporterProperty;
+
+
+/**
+ * Interface containing settings used by the Graphics2D exporter.
+ *
+ * @see JRGraphics2DExporter
+ * 
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public interface Graphics2DExporterConfiguration extends ExporterConfiguration
+{
+	/**
+	 * Property that provides a default value for the {@link #isMinimizePrinterJobSize()} exporter configuration setting.
+	 */
+	public static final String MINIMIZE_PRINTER_JOB_SIZE = JRPropertiesUtil.PROPERTY_PREFIX + "export.graphics2d.min.job.size";
+
+	/**
+	 * The zoom ratio used for the export. The default value is 1.
+	 */
+	public Float getZoomRatio();
+	
+	/**
+	 * Flag to control the use of an AWT rendering fix which causes the printer job size to be reduced when
+	 * the exporter draws onto a printer graphic context.
+	 *
+	 * The fix was introduced to solve an old Java printing problem related to the size of printer spool jobs.
+	 * However, it causes problems when bidirectional text is rendered, by losing text direction information.
+	 *
+	 * This flag is true, by default and should be set to false when bidirectional writing is present in
+	 * the document that is sent to the printer.
+	 *
+	 * @see #MINIMIZE_PRINTER_JOB_SIZE
+	 */
+	@ExporterProperty(MINIMIZE_PRINTER_JOB_SIZE)
+	public Boolean isMinimizePrinterJobSize();
+	
+	/**
+	 * 
+	 */
+	@ExporterProperty(JRStyledText.PROPERTY_AWT_IGNORE_MISSING_FONT)
+	public Boolean isIgnoreMissingFont();
+}
