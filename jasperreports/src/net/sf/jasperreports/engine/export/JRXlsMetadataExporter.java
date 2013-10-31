9a7437c8888188b24ee944062cde1f9461bf7ede
Teodor Danciu <teodord@users.sourceforge.net>
2013-10-31 18:29:05 +0000

    make image settings a nested class in xls metadata exporter
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@6666 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/engine/export/JRXlsMetadataExporter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRXlsMetadataExporter.java
@@ -2128,3 +2128,24 @@ public class JRXlsMetadataExporter extends JRXlsAbstractMetadataExporter {
 	}
 }
 
+class ImageSettings {
+
+	private int index;
+	private int anchorType;
+	
+	public ImageSettings() {
+	}
+	
+	public ImageSettings(int index, int anchorType) {
+		this.index = index;
+		this.anchorType = anchorType;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+	
+	public int getAnchorType() {
+		return anchorType;
+	}
+}
