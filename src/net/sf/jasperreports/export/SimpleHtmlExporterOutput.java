e1159d9aaebe5b58be3f4fac64dc041f60b7128a
Teodor Danciu <teodord@users.sourceforge.net>
2013-11-08 10:22:28 +0000

    exporter configuration refactoring draft
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk/jasperreports@6700 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ src/net/sf/jasperreports/export/SimpleHtmlExporterOutput.java
@@ -0,0 +1,165 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.export;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.io.Writer;
+
+import net.sf.jasperreports.engine.export.HtmlResourceHandler;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class SimpleHtmlExporterOutput extends SimpleWriterExporterOutput implements HtmlExporterOutput
+{
+	/**
+	 * 
+	 */
+	private HtmlResourceHandler imageHandler;
+	private HtmlResourceHandler fontHandler;
+	private HtmlResourceHandler resourceHandler;
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(StringBuffer sbuffer)
+	{
+		super(sbuffer);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(Writer writer)
+	{
+		super(writer);
+	}
+	
+
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(OutputStream outputStream)
+	{
+		super(outputStream);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(OutputStream outputStream, String encoding)
+	{
+		super(outputStream, encoding);
+	}
+	
+
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(File file)
+	{
+		super(file);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(File file, String encoding)
+	{
+		super(file, encoding);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(String fileName)
+	{
+		super(fileName);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleHtmlExporterOutput(String fileName, String encoding)
+	{
+		super(fileName, encoding);
+	}
+	
+	/**
+	 * 
+	 */
+	public HtmlResourceHandler getImageHandler() 
+	{
+		return imageHandler;
+	}
+
+	/**
+	 * 
+	 */
+	public void setImageHandler(HtmlResourceHandler imageHandler)
+	{
+		this.imageHandler = imageHandler;
+	}
+	
+	/**
+	 * 
+	 */
+	public HtmlResourceHandler getFontHandler() 
+	{
+		return fontHandler;
+	}
+
+	/**
+	 * 
+	 */
+	public void setFontHandler(HtmlResourceHandler fontHandler)
+	{
+		this.fontHandler = fontHandler;
+	}
+	
+	/**
+	 * 
+	 */
+	public HtmlResourceHandler getResourceHandler() 
+	{
+		return resourceHandler;
+	}
+
+	/**
+	 * 
+	 */
+	public void setResourceHandler(HtmlResourceHandler resourceHandler)
+	{
+		this.resourceHandler = resourceHandler;
+	}
+}
---
+++ src/net/sf/jasperreports/export/SimpleXmlExporterOutput.java
@@ -0,0 +1,131 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.export;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.io.Writer;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id$
+ */
+public class SimpleXmlExporterOutput extends SimpleWriterExporterOutput implements XmlExporterOutput
+{
+	/**
+	 * 
+	 */
+	private Boolean isEmbeddingImages;
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(StringBuffer sbuffer)
+	{
+		super(sbuffer);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(Writer writer)
+	{
+		super(writer);
+	}
+	
+
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(OutputStream outputStream)
+	{
+		super(outputStream);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(OutputStream outputStream, String encoding)
+	{
+		super(outputStream, encoding);
+	}
+	
+
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(File file)
+	{
+		super(file);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(File file, String encoding)
+	{
+		super(file, encoding);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(String fileName)
+	{
+		super(fileName);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public SimpleXmlExporterOutput(String fileName, String encoding)
+	{
+		super(fileName, encoding);
+	}
+
+	
+	/**
+	 * 
+	 */
+	public Boolean isEmbeddingImages()
+	{
+		return isEmbeddingImages;
+	}
+
+	
+	/**
+	 * 
+	 */
+	public void setEmbeddingImages(Boolean isEmbeddingImages)
+	{
+		this.isEmbeddingImages = isEmbeddingImages;
+	}
+}
