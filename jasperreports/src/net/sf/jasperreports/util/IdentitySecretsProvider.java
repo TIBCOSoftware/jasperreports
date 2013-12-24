f4d99057c52a9c6c3d5de221c5fa100738ece673
Teodor Danciu <teodord@users.sourceforge.net>
2013-12-24 10:36:59 +0000

    identity secrets provider
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/branches/jr-5-5-1-x@6797 76e25fc1-081a-0410-bb1b-c96bc70638b7

---
+++ jasperreports/src/net/sf/jasperreports/util/IdentitySecretsProvider.java
@@ -0,0 +1,44 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jasperreports.util;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ * @version $Id: AbstractDataAdapterService.java 6233 2013-06-06 11:43:11Z teodord $
+ */
+public class IdentitySecretsProvider implements SecretsProvider 
+{
+	@Override
+	public boolean hasSecret(String key) 
+	{
+		return true;
+	}
+	
+	@Override
+	public String getSecret(String key) 
+	{
+		return key;
+	}
+}
