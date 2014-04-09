2268ba0193fcdf26ab34492b8ef2546180e62f2d
Sanda Zaharia <shertage@users.sourceforge.net>
2014-04-09 11:46:27 +0000

    javadoc update
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@7006 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/compilers/package-info.java
+++ jasperreports/src/net/sf/jasperreports/compilers/package-info.java
@@ -23,59 +23,62 @@
  */
 
 /**
-* Contains classes for report compiling and expressions evaluating.
-* <br/>
-* <h2>Compiling Report Templates</h2>
-* Source report templates, created either by using the API or by parsing JRXML files, are 
-* subject to the report compilation process before they are filled with data. 
-* <p>
-* This is necessary to make various consistency validations and to incorporate into these 
-* report templates data used to evaluate all report expressions at runtime.
-* </p><p> 
-* The compilation process transforms 
-* {@link net.sf.jasperreports.engine.design.JasperDesign JasperDesign} objects into 
-* {@link net.sf.jasperreports.engine.JasperReport JasperReport} objects. Both classes are 
-* implementations of the same basic {@link net.sf.jasperreports.engine.JRReport JRReport} 
-* interface. However, <code>JasperReport</code> objects cannot be modified once they are produced, 
-* while <code>JasperDesign</code> objects can. This is because some modifications made on the 
-* report template would probably require re-validation, or if a report expression is 
-* modified, the compiler-associated data stored inside the report template would have to be 
-* updated. 
-* </p><p>
-* <code>JasperDesign</code> objects are produced when parsing JRXML files using the 
-* {@link net.sf.jasperreports.engine.xml.JRXmlLoader JRXmlLoader} or created directly by the parent 
-* application if dynamic report templates are required. The GUI tools for editing 
-* JasperReports templates also work with this class to make in-memory modifications to 
-* the report templates before storing them on disk. 
-* </p><p>
-* A <code>JasperDesign</code> object must be subject to the report compilation process to produce a 
-* JasperReport object. 
-* </p><p>
-* Central to this process is the {@link net.sf.jasperreports.engine.design.JRCompiler JRCompiler} 
-* interface, which defines two methods, one being the following: 
-* </p>
-* <pre>
-* public JasperReport compileReport(JasperDesign design) throws JRException;</pre> 
-* There are several implementations for this compiler interface depending on the language 
-* used for the report expressions or the mechanism used for their runtime evaluation. 
-* <p/>
-* <h2>Expressions Scripting Language</h2>
-* The default language for the report expressions is Java, but report expressions
-* can be written in Groovy, JavaScript or any other scripting language as long as a report
-* compiler implementation that can evaluate them at runtime is available.
-* <p>
-* JasperReports currently ships report compiler implementations for the Groovy scripting
-* language (<a href="http://groovy.codehaus.org">http://groovy.codehaus.org</a>), 
-* JavaScript (<a href="http://www.mozilla.org/rhino">http://www.mozilla.org/rhino</a>), and
-* the BeanShell scripting library (<a href="http://www.beanshell.org">http://www.beanshell.org</a>). 
-* The compiler implementation classes are:
-* <ul>
-* <li>{@link net.sf.jasperreports.compilers.JRGroovyCompiler JRGroovyCompiler}</li>
-* <li>{@link net.sf.jasperreports.compilers.JavaScriptCompiler JavaScriptCompiler}</li>
-* <li>{@link net.sf.jasperreports.compilers.JRBshCompiler JRBshCompiler}</li>
-* </ul>
-* Historically, these compiler implementations used to be shipped as separate samples, 
-* but now they are part of the core library.
-* </p>
+ * Contains classes for report compiling and expressions evaluating.
+ * <br/>
+ * <h2>Compiling Report Templates</h2>
+ * Source report templates, created either by using the API or by parsing JRXML files, are 
+ * subject to the report compilation process before they are filled with data. 
+ * <p>
+ * This is necessary to make various consistency validations and to incorporate into these 
+ * report templates data used to evaluate all report expressions at runtime.
+ * </p><p> 
+ * The compilation process transforms 
+ * {@link net.sf.jasperreports.engine.design.JasperDesign JasperDesign} objects into 
+ * {@link net.sf.jasperreports.engine.JasperReport JasperReport} objects. Both classes are 
+ * implementations of the same basic {@link net.sf.jasperreports.engine.JRReport JRReport} 
+ * interface. However, <code>JasperReport</code> objects cannot be modified once they are produced, 
+ * while <code>JasperDesign</code> objects can. This is because some modifications made on the 
+ * report template would probably require re-validation, or if a report expression is 
+ * modified, the compiler-associated data stored inside the report template would have to be 
+ * updated. 
+ * </p><p>
+ * <code>JasperDesign</code> objects are produced when parsing JRXML files using the 
+ * {@link net.sf.jasperreports.engine.xml.JRXmlLoader JRXmlLoader} or created directly by the parent 
+ * application if dynamic report templates are required. The GUI tools for editing 
+ * JasperReports templates also work with this class to make in-memory modifications to 
+ * the report templates before storing them on disk. 
+ * </p><p>
+ * A <code>JasperDesign</code> object must be subject to the report compilation process to produce a 
+ * JasperReport object. 
+ * </p><p>
+ * Central to this process is the {@link net.sf.jasperreports.engine.design.JRCompiler JRCompiler} 
+ * interface, which defines two methods, one being the following: 
+ * </p>
+ * <pre>
+ * public JasperReport compileReport(JasperDesign design) throws JRException;</pre> 
+ * There are several implementations for this compiler interface depending on the language 
+ * used for the report expressions or the mechanism used for their runtime evaluation. 
+ * <p/>
+ * <h2>Expressions Scripting Language</h2>
+ * The default language for the report expressions is Java, but report expressions
+ * can be written in Groovy, JavaScript or any other scripting language as long as a report
+ * compiler implementation that can evaluate them at runtime is available.
+ * <p>
+ * JasperReports currently ships report compiler implementations for the Groovy scripting
+ * language (<a href="http://groovy.codehaus.org">http://groovy.codehaus.org</a>), 
+ * JavaScript (<a href="http://www.mozilla.org/rhino">http://www.mozilla.org/rhino</a>), and
+ * the BeanShell scripting library (<a href="http://www.beanshell.org">http://www.beanshell.org</a>). 
+ * The compiler implementation classes are:
+ * <ul>
+ * <li>{@link net.sf.jasperreports.compilers.JRGroovyCompiler JRGroovyCompiler}</li>
+ * <li>{@link net.sf.jasperreports.compilers.JavaScriptCompiler JavaScriptCompiler}</li>
+ * <li>{@link net.sf.jasperreports.compilers.JRBshCompiler JRBshCompiler}</li>
+ * </ul>
+ * Historically, these compiler implementations used to be shipped as separate samples, 
+ * but now they are part of the core library.
+ * </p>
+ * <h2>Related Documentation</h2>
+ * <a href="http://community.jaspersoft.com/wiki/jasperreports-library-tutorial">JasperReports Tutorial</a>
+
 */
 package net.sf.jasperreports.compilers;
