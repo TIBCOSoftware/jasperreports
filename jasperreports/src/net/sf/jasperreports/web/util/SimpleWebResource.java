e4d522477e0d80c2b0cce0b56e9aeddbc6416967
Teodor Danciu <teodord@users.sourceforge.net>
2014-08-27 08:42:29 +0000

    add copyright header
    git-svn-id: http://code.jaspersoft.com/svn/repos/jasperreports/trunk@7191 76e25fc1-081a-0410-bb1b-c96bc70638b7

--- jasperreports/src/net/sf/jasperreports/web/util/SimpleWebResource.java
+++ jasperreports/src/net/sf/jasperreports/web/util/SimpleWebResource.java
@@ -1,7 +1,25 @@
 /*
- * Copyright (C) 2005 - 2009 Jaspersoft Corporation. All rights reserved.
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2013 Jaspersoft Corporation. All rights reserved.
  * http://www.jaspersoft.com
- * Licensed under commercial Jaspersoft Subscription License Agreement
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
  */
 package net.sf.jasperreports.web.util;
 
