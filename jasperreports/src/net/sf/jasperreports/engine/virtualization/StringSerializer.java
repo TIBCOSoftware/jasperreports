5cdc8ec4e08d6018a67bb57286213a3bc3607cca
Lucian Chirita <lucianc@users.sourceforge.net>
2015-05-07 17:00:11 +0300

    bug 41388 - serialize strings bigger than 65535 bytes in the virtualizer

--- jasperreports/src/net/sf/jasperreports/engine/virtualization/StringSerializer.java
+++ jasperreports/src/net/sf/jasperreports/engine/virtualization/StringSerializer.java
@@ -25,11 +25,16 @@ package net.sf.jasperreports.engine.virtualization;
 
 import java.io.IOException;
 
+import net.sf.jasperreports.engine.JRRuntimeException;
+
 /**
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
  */
 public class StringSerializer implements ObjectSerializer<String>
 {
+	// smaller than 65535/3, see writeUTF
+	private static final int CHUNK_SIZE = 20000;
+	
 	@Override
 	public int typeValue()
 	{
@@ -51,13 +56,79 @@ public class StringSerializer implements ObjectSerializer<String>
 	@Override
 	public void write(String value, VirtualizationOutput out) throws IOException
 	{
-		out.writeUTF(value);
+		int length = value.length();
+		if (length < CHUNK_SIZE)
+		{
+			out.writeUTF(value);
+		}
+		else
+		{
+			out.writeUTF(value.substring(0, CHUNK_SIZE));
+			// writing the length so that we know when to stop in read()
+			out.writeInt(length);
+			
+			for (int index = CHUNK_SIZE; index < length; index += CHUNK_SIZE)
+			{
+				out.writeUTF(value.substring(index, Math.min(length, index + CHUNK_SIZE)));
+			}
+		}
 	}
 
 	@Override
 	public String read(VirtualizationInput in) throws IOException
 	{
-		return in.readUTF();
+		//TODO write some unit tests
+		String chunk = in.readUTF();
+		int chunkLength = chunk.length();
+		if (chunkLength < CHUNK_SIZE)
+		{
+			// single chunk
+			return chunk;
+		}
+		
+		// should be of exact max chunk size
+		if (chunkLength != CHUNK_SIZE)
+		{
+			throw new JRRuntimeException("Read string chunk of unexpected length " + chunkLength);
+		}
+		
+		// we have several chunks, read the length
+		int length = in.readInt();
+		if (length < CHUNK_SIZE) 
+		{
+			throw new JRRuntimeException("Read unexpected string chunk length " + length);
+		}
+		
+		//FIXME check against an upper limit to prevent memory being allocated when the data is corrupted?
+		char[] chars = new char[length];
+		chunk.getChars(0, chunkLength, chars, 0);
+		
+		int remaining = length - chunkLength;
+		while (remaining > 0)
+		{
+			chunk = in.readUTF();
+			chunkLength = chunk.length();
+			
+			if (chunkLength < CHUNK_SIZE)
+			{
+				// we should be on the last chunk
+				if (chunkLength != remaining)
+				{
+					throw new JRRuntimeException("Read string chunk of unexpected length " + chunkLength
+							+ ", was expecting " + remaining);
+				}
+			}
+			else if (chunkLength != CHUNK_SIZE)
+			{
+				// should be of exact max chunk size
+				throw new JRRuntimeException("Read string chunk of unexpected length " + chunkLength);
+			}
+			
+			chunk.getChars(0, chunkLength, chars, length - remaining);
+			remaining -= chunkLength;
+		}
+		
+		return new String(chars);
 	}
 
 }
