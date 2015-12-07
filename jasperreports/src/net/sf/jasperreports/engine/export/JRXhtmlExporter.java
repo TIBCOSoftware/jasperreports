ff8d399c28de2483f6ced8d91826024a134b368d
Teodor Danciu <teodord@users.sourceforge.net>
2015-12-07 13:09:54 +0200

    tracker #5586 & #4055 : fix IE comment

--- jasperreports/src/net/sf/jasperreports/engine/export/JRXhtmlExporter.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/JRXhtmlExporter.java
@@ -370,12 +370,12 @@ public class JRXhtmlExporter extends AbstractHtmlExporter<HtmlReportConfiguratio
 		
 //		if (!isOutputResourcesToDir)
 		{
-			writer.write("<![if IE]>\n");
+			writer.write("<!--[if IE]>\n");
 			writer.write("<script>\n");
 			writer.write("var links = document.querySelectorAll('link.jrWebFont');\n");
 			writer.write("setTimeout(function(){ if (links) { for (var i = 0; i < links.length; i++) { links.item(i).href = links.item(i).href; } } }, 0);\n");
 			writer.write("</script>\n");
-			writer.write("<![endif]>\n");
+			writer.write("<![endif]-->\n");
 		}
 
 		if (htmlFooter == null)
