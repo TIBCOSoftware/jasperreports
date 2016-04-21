992ecae97881c7c5d47991a6206c6fb733500088
gtoffoli <gtoffoli@tibco.com>
2016-04-21 16:52:53 +0000

    Changed Copyrights to 2016
    git-svn-id: https://svnserver.jaspersoft.com/eng/jasperreports-pro/trunk@29869 700bc090-4de7-0310-9c15-8ee41d4a1810

--- src/main/java/com/jaspersoft/jasperreports/customvisualization/export/CVElementPhantomJSImageProvider.java
+++ src/main/java/com/jaspersoft/jasperreports/customvisualization/export/CVElementPhantomJSImageProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
+ * Copyright (C) 2005 - 2016 TIBCO Software Inc. All rights reserved.
  * http://www.jaspersoft.com.
  * 
  * Unless you have purchased  a commercial license agreement from Jaspersoft,
--- src/main/java/com/jaspersoft/jasperreports/customvisualization/scripts/div2svg.js
+++ src/main/java/com/jaspersoft/jasperreports/customvisualization/scripts/div2svg.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
+ * Copyright (C) 2005 - 2016 TIBCO Software Inc. All rights reserved.
  * http://www.jaspersoft.com
  * Licensed under commercial Jaspersoft Subscription License Agreement
  */
