beb9447a45b46a6a75dfebec2dcbdec0ce7d367f
Teodor Danciu <teodord@users.sourceforge.net>
2016-08-31 16:16:58 +0300

    add http data adapter sample

---
+++ jasperreports/demo/samples/httpdataadapter/src/HttpDataAdapterApp.java
@@ -0,0 +1,127 @@
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2016 TIBCO Software Inc. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.AbstractSampleApp;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+
+/**
+ * @author Teodor Danciu (teodord@users.sourceforge.net)
+ */
+public class HttpDataAdapterApp extends AbstractSampleApp
+{
+	
+
+	/**
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		main(new HttpDataAdapterApp(), args);
+	}
+	
+	
+	/**
+	 *
+	 */
+	public void test() throws JRException
+	{
+		fill();
+		pdf();
+		html();
+	}
+
+
+	/**
+	 *
+	 */
+	public void fill() throws JRException
+	{
+		File[] files = getFiles(new File("build/reports"), "jasper");
+		for(int i = 0; i < files.length; i++)
+		{
+			File reportFile = files[i];
+			long start = System.currentTimeMillis();
+			String fileName = reportFile.getAbsolutePath();
+			JasperFillManager.fillReportToFile(
+				fileName, 
+				fileName.substring(0, fileName.indexOf(".jasper")) + ".jrprint",
+				null
+				);
+			System.err.println("Filling time : " + (System.currentTimeMillis() - start));
+		}
+	}
+
+
+	/**
+	 *
+	 */
+	public void pdf() throws JRException
+	{
+		File[] files = getFiles(new File("build/reports"), "jrprint");
+		for(int i = 0; i < files.length; i++)
+		{
+			File reportFile = files[i];
+			long start = System.currentTimeMillis();
+			String fileName = reportFile.getAbsolutePath();
+			JasperExportManager.exportReportToPdfFile(
+				fileName, 
+				fileName.substring(0, fileName.indexOf(".jrprint")) + ".pdf"
+				);
+			System.err.println("PDF creation time : " + (System.currentTimeMillis() - start));
+		}
+	}
+
+
+	/**
+	 *
+	 */
+	public void html() throws JRException
+	{
+		File[] files = getFiles(new File("build/reports"), "jrprint");
+		for(int i = 0; i < files.length; i++)
+		{
+			File reportFile = files[i];
+			long start = System.currentTimeMillis();
+			String fileName = reportFile.getAbsolutePath();
+			JasperExportManager.exportReportToHtmlFile(
+				fileName, 
+				fileName.substring(0, fileName.indexOf(".jrprint")) + ".html"
+				);
+			System.err.println("HTML creation time : " + (System.currentTimeMillis() - start));
+		}
+		
+	}
+
+
+}
