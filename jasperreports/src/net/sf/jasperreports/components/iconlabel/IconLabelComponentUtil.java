TIBCOSoftware/jasperreports

commit 0f74ede690a632cdadfca445e107d98d485a02aa

fix gh-20, tracker 10486 - set static text on each evaluate
Summary: fix NPE on static text after rewind

Test Plan: https://community.jaspersoft.com/jasperreports-library/issues/10486

Reviewers: tdanciu

Differential Revision: http://phabricator.jaspersoft.com/D4677

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillStaticText.java
@@ -36,6 +36,7 @@ import net.sf.jasperreports.engine.JRVisitor;
 public class JRFillStaticText extends JRFillTextElement implements JRStaticText
 {
 
+	private final String text;
 
 	/**
 	 *
@@ -53,7 +54,7 @@ public class JRFillStaticText extends JRFillTextElement implements JRStaticText
 			{
 				text = "";
 			}
-			setRawText(text);
+			this.text = text;
 		}
 
 
@@ -61,12 +62,7 @@ public class JRFillStaticText extends JRFillTextElement implements JRStaticText
 	{
 		super(staticText, factory);
 
-		String text = processMarkupText(staticText.getText());
-		if (text == null)
-		{
-			text = "";
-		}
-		setRawText(text);
+		this.text = staticText.text;
 	}
 
 
@@ -110,6 +106,8 @@ public class JRFillStaticText extends JRFillTextElement implements JRStaticText
 		evaluateProperties(evaluation);
 		evaluateStyle(evaluation);
 
+		//setting the text each time so that super.rewind() works fine
+		setRawText(this.text);
 		resetTextChunk();
 		
 		setValueRepeating(true);

commit 5f763c21a30b237d7f5602fe7d090925620b225d

fix static text as crosstab row group header
broken in 0f74ede690a632cdadfca445e107d98d485a02aa as getText() called
for JRFillStaticText was returning null

fixed by implementing getText() in JRFillStaticText to return the
original/design text

also renamed getText() in JRFillTextElement to getTextString (for lack of
a better name) so that there's no collision with the JRFillStaticText method

also preserved the static text markup type in the icon label component

jasperreports/src/net/sf/jasperreports/components/iconlabel/IconLabelComponentUtil.java
@@ -177,6 +177,7 @@ public class IconLabelComponentUtil
 		labelTextField.setHorizontalTextAlign(parentElement.getOwnHorizontalTextAlign());
 		labelTextField.setVerticalTextAlign(parentElement.getOwnVerticalTextAlign());
 		labelTextField.setRotation(parentElement.getOwnRotationValue());//FIXMEICONLABEL how does it work?
+		labelTextField.setMarkup(parentElement.getMarkup());
 //		JRBoxUtil.copy(parentElement.getLineBox(), labelTextField.getLineBox());
 		labelTextField.getLineBox().setRightPadding(0);
 		labelTextField.getLineBox().getPen().setLineWidth(0);
@@ -212,7 +213,6 @@ public class IconLabelComponentUtil
 		{
 			labelTextField.setStretchWithOverflow(textField.isStretchWithOverflow());
 			labelTextField.setBlankWhenNull(textField.isBlankWhenNull());
-			labelTextField.setMarkup(textField.getMarkup());
 			labelTextField.setPattern(textField.getPattern());
 			labelTextField.setPatternExpression(textField.getPatternExpression());
 			labelTextField.setBookmarkLevel(textField.getBookmarkLevel());

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillStaticText.java
@@ -249,4 +249,11 @@ public class JRFillStaticText extends JRFillTextElement implements JRStaticText
 		return false;
 	}
 
+
+	@Override
+	public String getText()
+	{
+		return ((JRStaticText) parent).getText();
+	}
+
 }

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillTextElement.java
@@ -626,7 +626,7 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 	/**
 	 *
 	 */
-	public String getText()
+	public String getTextString()
 	{
 		JRStyledText tmpStyledText = getStyledText();
 

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillTextField.java
@@ -680,7 +680,7 @@ public class JRFillTextField extends JRFillTextElement implements JRTextField
 				}
 
 				if (
-					getTextEnd() >= getText().length()
+					getTextEnd() >= getTextString().length()
 					|| !isStretchWithOverflow()
 					|| !getRotationValue().equals(RotationEnum.NONE)
 					)
@@ -752,7 +752,7 @@ public class JRFillTextField extends JRFillTextElement implements JRTextField
 					// the available vertical space is sufficient
 
 					if (
-						getTextEnd() < getText().length() 
+						getTextEnd() < getTextString().length() 
 						|| getTextEnd() == 0
 						)
 					{
@@ -768,7 +768,7 @@ public class JRFillTextField extends JRFillTextElement implements JRTextField
 							// display all its content
 
 							chopTextElement(availableHeight - getRelativeY() - getHeight());
-							if (getTextEnd() < getText().length())// - 1)
+							if (getTextEnd() < getTextString().length())// - 1)
 							{
 								// even after the current chop operation there is some text left
 								// that will overflow on the next page
@@ -810,7 +810,7 @@ public class JRFillTextField extends JRFillTextElement implements JRTextField
 			if (
 				isToPrint &&
 				isRemoveLineWhenBlank() &&	//FIXME if the line won't be removed due to other elements 
-				getText().substring(		// present on that line, the background does not appear
+				getTextString().substring(		// present on that line, the background does not appear
 					getTextStart(),
 					getTextEnd()
 					).trim().length() == 0