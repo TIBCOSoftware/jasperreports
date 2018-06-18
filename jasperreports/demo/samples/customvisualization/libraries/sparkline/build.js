b802756b7b09a94cced2dbbe28da598a18cea810
Giulio Toffoli <giulio@saysource.com>
2018-06-18 12:46:03 +0100

    Refactored example sparkline to use d3 5.5.0 and setup component to be compiled

---
+++ jasperreports/demo/samples/customvisualization/libraries/sparkline/build.js
@@ -0,0 +1,16 @@
+({
+    optimize: 'none', // Uncomment this property to disable the uglify of the libraries
+    baseUrl: '',
+    paths: {
+			'd3': 'd3-5.5.0.min' , 		
+			'sparkline': 'sparkline' 		
+	}, 
+		
+	wrap: {
+        start: "(function(root){\n\nvar define = root.define;\n\n",
+        end: "\n\n}(typeof __visualize__ != 'undefined' ? __visualize__ : (typeof __jrio__ != 'undefined' ? __jrio__ : window)));"
+    },
+    
+    name: "sparkline",
+    out: "sparkline.min.js"
+})
