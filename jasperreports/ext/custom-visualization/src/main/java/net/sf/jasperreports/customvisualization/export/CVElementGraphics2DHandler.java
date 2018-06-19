ccaa9efe4a54a0125ac543e3866d23fd2269c14e
Giulio Toffoli <giulio@saysource.com>
2018-06-19 16:23:09 +0100

    Updated legal notice in the source code

--- jasperreports/ext/custom-visualization/src/main/java/net/sf/jasperreports/customvisualization/export/CVElementGraphics2DHandler.java
+++ jasperreports/ext/custom-visualization/src/main/java/net/sf/jasperreports/customvisualization/export/CVElementGraphics2DHandler.java
@@ -1,29 +1,26 @@
-/*******************************************************************************
- * Copyright (C) 2005 - 2016 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
- * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
- * 
- * The Custom Visualization Component program and the accompanying materials
- * has been dual licensed under the the following licenses:
- * 
- * Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * GNU Lesser General Public License as published by
+/*
+ * JasperReports - Free Java Reporting Library.
+ * Copyright (C) 2001 - 2018 TIBCO Software Inc. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of JasperReports.
+ *
+ * JasperReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The Custom Visualization Component is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * JasperReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License.
- * If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.jasperreports.customvisualization.export;
 
 import java.awt.Graphics2D;
