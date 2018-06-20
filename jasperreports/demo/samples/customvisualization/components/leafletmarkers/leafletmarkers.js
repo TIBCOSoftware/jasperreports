1b4e0242237aa2e15d7b509e5c71876d797249b5
Giulio Toffoli <giulio@saysource.com>
2018-06-20 15:44:13 +0100

    Adjusted formatting

--- jasperreports/demo/samples/customvisualization/components/leafletmarkers/leafletmarkers.js
+++ jasperreports/demo/samples/customvisualization/components/leafletmarkers/leafletmarkers.js
@@ -22,108 +22,103 @@
  * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define('leafletmarkers',['leaflet','leaflet-providers','leaflet-ajax', 'icon'], function (L) {
+define('leafletmarkers', ['leaflet', 'leaflet-providers', 'leaflet-ajax', 'icon'], function (L) {
 
-	 return function (instanceData) {
-		 
-         // Ignore styles is no longer required if the component is set to be exported directly as PNG
-         //window.cvcIgnoreSVGStyles = true;	
+    return function (instanceData) {
 
-         // Flag used by the server side renderer to check when the component is actually ready
-         window.componentRendered = false;
-         
-         // Operations completed counter
-         var fully_loaded = 0;
+        // Ignore styles is no longer required if the component is set to be exported directly as PNG
+        //window.cvcIgnoreSVGStyles = true;	
 
-         // When we export a report on the server side, we want to set the flag window.componentRendered to signal that
-         // all the rendering is complete and ready to be exported.
-         // For this we use a function that increment a counter of the operations that needs to be completed asynchronously
-         // such as tiles loading and json data loading
-         // The operations are 2, so as the counter reach 2, we are ready to render.
-         function loadedCompleted() {
-                   fully_loaded++;
+        // Flag used by the server side renderer to check when the component is actually ready
+        window.componentRendered = false;
 
-                   if (fully_loaded >= 2)
-                   {
-                       window.componentRendered = true;
-                   }
-         }
-         
-         
-         // We use the animation flag set by the CVC exporter to determine if the export is performed on server or client.
-         // The CVC tells us to avoid animations in case of server side export, so if instanceData.animation and true, we
-         // are rendering on the server.
-         var serverSideExport = false;
-         if (typeof instanceData.animation != 'undefined' && instanceData.animation == false)
-         {
+        // Operations completed counter
+        var fully_loaded = 0;
+
+        // When we export a report on the server side, we want to set the flag window.componentRendered to signal that
+        // all the rendering is complete and ready to be exported.
+        // For this we use a function that increment a counter of the operations that needs to be completed asynchronously
+        // such as tiles loading and json data loading
+        // The operations are 2, so as the counter reach 2, we are ready to render.
+        function loadedCompleted() {
+            fully_loaded++;
+
+            if (fully_loaded >= 2) {
+                window.componentRendered = true;
+            }
+        }
+
+
+        // We use the animation flag set by the CVC exporter to determine if the export is performed on server or client.
+        // The CVC tells us to avoid animations in case of server side export, so if instanceData.animation and true, we
+        // are rendering on the server.
+        var serverSideExport = false;
+        if (typeof instanceData.animation != 'undefined' && instanceData.animation == false) {
             serverSideExport = true;
-         }
-         
-         // Start of the real component implementation ------------------------------------
-         
-         var map = L.map(instanceData.id, {
-                    fadeAnimation: !serverSideExport, // Don't use fade effects on server side export
-                    zoomControl: !serverSideExport    // Don't show any control on server side export
-                });
-         
- 
+        }
+
+        // Start of the real component implementation ------------------------------------
+
+        var map = L.map(instanceData.id, {
+            fadeAnimation: !serverSideExport, // Don't use fade effects on server side export
+            zoomControl: !serverSideExport    // Don't show any control on server side export
+        });
+
+
         // Free layer very cool!
         //L.tileLayer.provider('Stamen.Watercolor').addTo(map);
-        if (typeof instanceData.provider == 'undefined' || instanceData.provider == "")
-        {
-        	instanceData.provider = 'OpenTopoMap';
- 		}
-        
+        if (typeof instanceData.provider == 'undefined' || instanceData.provider == "") {
+            instanceData.provider = 'OpenTopoMap';
+        }
+
         // The map provider is specified by the user.
         // We store the layer in order to attach a listener that will tell us
         // when the tiles are fully ready.
         var tile_layer = L.tileLayer.provider(instanceData.provider).addTo(map);
-        tile_layer.on("load",function() {  
-                loadedCompleted();          
+        tile_layer.on("load", function () {
+            loadedCompleted();
         });
-         
-        // Set the center of the map based on the user preferences...
-        map.setView([instanceData.lat, instanceData.lon ], instanceData.zoom);
 
-         
+        // Set the center of the map based on the user preferences...
+        map.setView([instanceData.lat, instanceData.lon], instanceData.zoom);
+
+
         // Loading the GeoJSON via ajax...
         // "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
         // "https://raw.githubusercontent.com/datasets/geo-boundaries-us-110m/master/json/ne_110m_admin_1_states_provinces_shp_scale_rank.geojson"
-        if (typeof instanceData.dataUrl != 'undefined' && instanceData.dataUrl != "")
-        {
+        if (typeof instanceData.dataUrl != 'undefined' && instanceData.dataUrl != "") {
             console.log("Loading geoJSON from: " + instanceData.dataUrl);
-            L.Util.ajax(instanceData.dataUrl).then(function(data){
-            		L.geoJson(data, {
-                        pointToLayer: function(feature, latlng) {
-                           return L.marker(latlng, {icon: myIcon()});
-                        }
-                    }).addTo(map);
-                
-                    // Markers loaded and displayed
-                    loadedCompleted();
+            L.Util.ajax(instanceData.dataUrl).then(function (data) {
+                L.geoJson(data, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.marker(latlng, { icon: myIcon() });
+                    }
+                }).addTo(map);
+
+                // Markers loaded and displayed
+                loadedCompleted();
             });
         }
         else // If we are not loading anything, we need to mark this process completed.
         {
-              // This call will increment the counter of processes completed.
-              loadedCompleted();
+            // This call will increment the counter of processes completed.
+            loadedCompleted();
         }
-        
+
         // If tiles are not loaded in less than 10 seconds, still declare the report to be completed...
-        setTimeout(10000, function() {
+        setTimeout(10000, function () {
             loadedCompleted();
         });
 
-		// Add markers from the data series, if it is there
-		if (typeof instanceData.series != 'undefined' && instanceData.series[0].length > 0)
-        {
-                instanceData.series[0].forEach(function(marker) {
-                    L.marker([marker.lat, marker.lon], { icon: myIcon() }).addTo(map)
+        // Add markers from the data series, if it is there
+        if (typeof instanceData.series != 'undefined' && instanceData.series[0].length > 0) {
+            instanceData.series[0].forEach(function (marker) {
+                L.marker([marker.lat, marker.lon], { icon: myIcon() }).addTo(map)
                     .bindPopup(marker.label);
-                });
+            });
         }
-	};
-    
-    
+    };
+
+
 });
 
--- jasperreports/demo/samples/customvisualization/components/sparkline/sparkline.js
+++ jasperreports/demo/samples/customvisualization/components/sparkline/sparkline.js
@@ -22,107 +22,107 @@
  * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
  */
 
-define('sparkline',['d3'], function (d3) {
+define('sparkline', ['d3'], function (d3) {
 
-	 return function (instanceData) {	
-		
-	 	var w = instanceData.width,
-            h = instanceData.height;
+	return function (instanceData) {
 
-        var margin = 20;
-        var diameter = Math.min(w,h) - margin;
-        
-        // Options:
-        // Sparkline interpolation: linear, step-before, step-after, basis, cardinal, monotone
-        // line color
-        // Fill area color
-        // Final circle color
-        
-        // extend function (similar to jquery)
-        function extend(a, b){
-					for(var key in b)
-						if(b.hasOwnProperty(key))
-							a[key] = b[key];
-					return a;
-				}
-        
-        var default_options = {
-        	interpolation: "basis",
-        	lineColor:  "#000000",
-        	lineOpacity:  "1",
-        	lineStroke: "0.5",
-        	fillColor:   "#0000FF",
-        	fillOpacity:   "0.1",
-        	circleColor: "#0000FF",
-        	circleOpacity: "1",
-        	circleRadius: 2
-        }
-				
-				var interpolations = { 'linear': d3.curveLinear, 
-															 'step-before': d3.curveStepBefore,
-															 'step-after': d3.curveStepAfter,
-															 'basis': d3.curveBasis,
-															 'cardinal': d3.curveCardinal,
-															 'monotone': d3.curveMonotoneX,
-															 'catmull-rom': d3.curveCatmullRom
-														};
-			  var options = extend(default_options, instanceData);
-				
-				if (typeof interpolations[options.interpolation] === 'undefined') {
-					options.interpolation = 'basis';
-				}
+		var w = instanceData.width,
+			h = instanceData.height;
 
-        options.circleRadius = +options.circleRadius; // cast to number from string.
-        
+		var margin = 20;
+		var diameter = Math.min(w, h) - margin;
+
+		// Options:
+		// Sparkline interpolation: linear, step-before, step-after, basis, cardinal, monotone
+		// line color
+		// Fill area color
+		// Final circle color
+
+		// extend function (similar to jquery)
+		function extend(a, b) {
+			for (var key in b)
+				if (b.hasOwnProperty(key))
+					a[key] = b[key];
+			return a;
+		}
+
+		var default_options = {
+			interpolation: "basis",
+			lineColor: "#000000",
+			lineOpacity: "1",
+			lineStroke: "0.5",
+			fillColor: "#0000FF",
+			fillOpacity: "0.1",
+			circleColor: "#0000FF",
+			circleOpacity: "1",
+			circleRadius: 2
+		}
+
+		var interpolations = {
+			'linear': d3.curveLinear,
+			'step-before': d3.curveStepBefore,
+			'step-after': d3.curveStepAfter,
+			'basis': d3.curveBasis,
+			'cardinal': d3.curveCardinal,
+			'monotone': d3.curveMonotoneX,
+			'catmull-rom': d3.curveCatmullRom
+		};
+		var options = extend(default_options, instanceData);
+
+		if (typeof interpolations[options.interpolation] === 'undefined') {
+			options.interpolation = 'basis';
+		}
+
+		options.circleRadius = +options.circleRadius; // cast to number from string.
+
 		var svg = d3.select("#" + instanceData.id).insert("svg")
-		    .attr("id", instanceData.id + "svg")
-		    .attr("width", w)
-		    .attr("height", h)
-		    .append('g')
-            .attr('transform', 'translate(0, 2)');
-		 
-        var data =  instanceData.series[0]; 
-        
-        if (data.length > 0)
-		{
-			var xScale = d3.scaleLinear().range([options.circleRadius, w - 2 - options.circleRadius]).domain([0, data.length-1]);
-			var yScale = d3.scaleLinear().range([ h - 2 - options.circleRadius, options.circleRadius]).domain(d3.extent(data, function(d) { return +d.value; }));
-			
-			var line = d3.line()
-				 .x(function(d,i) { return xScale(i); })
-				 .y(function(d) { return yScale( +d.value); })
-				 .curve(interpolations[options.interpolation]);
-			
-			var svgPathString = line(data);
-			svgPathString = "M" + xScale(0) + "," + yScale(0) + "L" + svgPathString.substring(1) + "L" + xScale(data.length-1) + "," + yScale(0);
+			.attr("id", instanceData.id + "svg")
+			.attr("width", w)
+			.attr("height", h)
+			.append('g')
+			.attr('transform', 'translate(0, 2)');
 
-            // No line or circle is printed if there is no data.
-            if (data.length == 0) return;
-            
+		var data = instanceData.series[0];
+
+		if (data.length > 0) {
+			var xScale = d3.scaleLinear().range([options.circleRadius, w - 2 - options.circleRadius]).domain([0, data.length - 1]);
+			var yScale = d3.scaleLinear().range([h - 2 - options.circleRadius, options.circleRadius]).domain(d3.extent(data, function (d) { return +d.value; }));
+
+			var line = d3.line()
+				.x(function (d, i) { return xScale(i); })
+				.y(function (d) { return yScale(+d.value); })
+				.curve(interpolations[options.interpolation]);
+
+			var svgPathString = line(data);
+			svgPathString = "M" + xScale(0) + "," + yScale(0) + "L" + svgPathString.substring(1) + "L" + xScale(data.length - 1) + "," + yScale(0);
+
+			// No line or circle is printed if there is no data.
+			if (data.length == 0) return;
+
 			svg.append('path')
-				 .attr('d', svgPathString)
-				 .attr('fill',options.fillColor)
-				 .attr('fill-opacity',options.fillOpacity)
-				 .attr('stroke','none');
-				 
+				.attr('d', svgPathString)
+				.attr('fill', options.fillColor)
+				.attr('fill-opacity', options.fillOpacity)
+				.attr('stroke', 'none');
+
 			svg.append('path')
-				 .datum(data)
-				 .attr('d', line)
-				 .attr('fill','none')
-				 .attr('stroke','#000000')
-				 .attr('stroke-opacity',options.lineOpacity)
-				 .attr('stroke-width', options.lineStroke + 'px');	
-				 
-		
+				.datum(data)
+				.attr('d', line)
+				.attr('fill', 'none')
+				.attr('stroke', '#000000')
+				.attr('stroke-opacity', options.lineOpacity)
+				.attr('stroke-width', options.lineStroke + 'px');
+
+
 			svg.append('circle')
-				 .attr('cx', xScale(data.length-1))
-				 .attr('cy', yScale(data[data.length-1].value))
-				 .attr('r', options.circleRadius)
-				 .attr('fill',options.circleColor)
-				 .attr('fill-opacity',options.circleOpacity)
-				 .attr('stroke','none');
+				.attr('cx', xScale(data.length - 1))
+				.attr('cy', yScale(data[data.length - 1].value))
+				.attr('r', options.circleRadius)
+				.attr('fill', options.circleColor)
+				.attr('fill-opacity', options.circleOpacity)
+				.attr('stroke', 'none');
 		}
 	};
-		
+
 });
 
