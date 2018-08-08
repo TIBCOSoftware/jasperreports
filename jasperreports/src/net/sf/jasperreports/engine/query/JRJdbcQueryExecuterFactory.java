51addb836bf9a3a4b4b09a319c2fae43d27c7ef3
Clayton Bodendein <clayton2@hotmail.com>
2018-08-07 20:15:29 -0500

    Related to gh-34 - Clarified JDBC query timeout config default value

--- jasperreports/src/net/sf/jasperreports/engine/query/JRJdbcQueryExecuterFactory.java
+++ jasperreports/src/net/sf/jasperreports/engine/query/JRJdbcQueryExecuterFactory.java
@@ -117,6 +117,7 @@ public class JRJdbcQueryExecuterFactory extends AbstractQueryExecuterFactory imp
 	 */
 	@Property(
 			category = PropertyConstants.CATEGORY_DATA_SOURCE,
+			defaultValue = "0",
 			scopes = {PropertyScope.CONTEXT, PropertyScope.DATASET},
 			scopeQualifications = {QUERY_EXECUTER_NAME},
 			valueType = Integer.class
