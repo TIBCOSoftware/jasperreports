TIBCOSoftware/jasperreports

commit 649150ca4dc073255d41eb12001be43b30066e97

tracker 8186 - proper hyperlinkWhen test

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillHyperlinkHelper.java
@@ -109,7 +109,7 @@ public final class JRFillHyperlinkHelper
 		}
 		
 		Boolean hyperlinkWhen = (Boolean) expressionEvaluator.evaluate(hyperlink.getHyperlinkWhenExpression(), evaluationType);
-		if (hyperlink.getHyperlinkWhenExpression() != null && hyperlinkWhen != Boolean.TRUE)
+		if (hyperlink.getHyperlinkWhenExpression() != null && Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			return null;
 		}

jasperreports/src/net/sf/jasperreports/components/spiderchart/FillSpiderChart.java
@@ -227,7 +227,7 @@ public class FillSpiderChart extends BaseFillComponent implements JRFillCloneabl
 	{
 		printImage.setRenderer(getRenderable());
 		printImage.setAnchorName(getAnchorName());
-		if (getChartSettings().getHyperlinkWhenExpression() == null || hyperlinkWhen == Boolean.TRUE)
+		if (getChartSettings().getHyperlinkWhenExpression() == null || Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			printImage.setHyperlinkReference(getHyperlinkReference());
 			printImage.setHyperlinkAnchor(getHyperlinkAnchor());

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillChart.java
@@ -941,7 +941,7 @@ public class JRFillChart extends JRFillElement implements JRChart
 	{
 		printImage.setRenderer(getRenderable());
 		printImage.setAnchorName(getAnchorName());
-		if (getHyperlinkWhenExpression() == null || hyperlinkWhen == Boolean.TRUE)
+		if (getHyperlinkWhenExpression() == null || Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			printImage.setHyperlinkReference(getHyperlinkReference());
 			printImage.setHyperlinkAnchor(getHyperlinkAnchor());

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillImage.java
@@ -938,7 +938,7 @@ public class JRFillImage extends JRFillGraphicElement implements JRImage
 		
 		printImage.setRenderer(getRenderable());
 		printImage.setAnchorName(getAnchorName());
-		if (getHyperlinkWhenExpression() == null || hyperlinkWhen == Boolean.TRUE)
+		if (getHyperlinkWhenExpression() == null || Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			printImage.setHyperlinkReference(getHyperlinkReference());
 			printImage.setHyperlinkAnchor(getHyperlinkAnchor());

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillTextField.java
@@ -909,7 +909,7 @@ public class JRFillTextField extends JRFillTextElement implements JRTextField
 		setPrintText(text);
 
 		text.setAnchorName(getAnchorName());
-		if (getHyperlinkWhenExpression() == null || hyperlinkWhen == Boolean.TRUE)
+		if (getHyperlinkWhenExpression() == null || Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			text.setHyperlinkReference(getHyperlinkReference());
 			text.setHyperlinkAnchor(getHyperlinkAnchor());

commit cdfd9dba1419d5d6c82e273f3fb207bdac33a6dd

use correct test for chart hyperlink when expressions
broken since 649150ca4dc073255d41eb12001be43b30066e97

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillHyperlinkHelper.java
@@ -109,7 +109,7 @@ public final class JRFillHyperlinkHelper
 		}
 		
 		Boolean hyperlinkWhen = (Boolean) expressionEvaluator.evaluate(hyperlink.getHyperlinkWhenExpression(), evaluationType);
-		if (hyperlink.getHyperlinkWhenExpression() != null && Boolean.TRUE.equals(hyperlinkWhen))
+		if (hyperlink.getHyperlinkWhenExpression() != null && !Boolean.TRUE.equals(hyperlinkWhen))
 		{
 			return null;
 		}