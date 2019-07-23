4194f41f2cf4a5dc7f070053466ab4b6e3f1c51a
Sanda Zaharia <szaharia@tibco.com>
2019-07-23 15:43:11 +0300

    JRL-53 - update jquery to v.3.4.1

--- jasperreports/demo/samples/webapp-repo/web/scripts/require.config.js
+++ jasperreports/demo/samples/webapp-repo/web/scripts/require.config.js
@@ -3,7 +3,7 @@ requirejs.config({
         "async": "scripts/require/async",
         "text": "scripts/require/text",
         "csslink": "scripts/require/requirejs-csslink-plugin",
-        "jquery": "scripts/jquery/core/jquery-3.3.1.min",
+        "jquery": "scripts/jquery/core/jquery-3.4.1.min",
         "jquery-ui": "scripts/jquery/ui",
         "jquery-ui/widgets/timepicker": "scripts/jquery/ui/jquery-ui-timepicker-addon-1.6.3.min",
         "jr.ReportExecution": "scripts/hyperlinkHandlers/jr.ReportExecution",
