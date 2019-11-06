b76f2a85f99b5d142bdcc151922b8b1f14da2dfb
Lucian Chirita <lchirita@tibco.com>
2019-11-06 18:57:58 +0200

    add Chrome resource manager methods to add data resources

--- jasperreports/src/net/sf/jasperreports/chrome/ResourceManager.java
+++ jasperreports/src/net/sf/jasperreports/chrome/ResourceManager.java
@@ -101,6 +101,26 @@ public class ResourceManager
 		}
 		return resourceMapping.getName();
 	}
+
+	public String copyDataResource(String resourceName, JasperReportsContext jasperReportsContext,
+			InputStream data)
+	{
+		ContextMappings mappings = contextMappings(jasperReportsContext);
+		File resourceFile = mappings.dataResourceMapping(resourceName, data, jasperReportsContext);
+		return resourceFile.getName();
+	}
+
+	public String getDataResourceFilename(String resourceName, JasperReportsContext jasperReportsContext)
+	{
+		ContextMappings mappings = contextMappings(jasperReportsContext);
+		File resourceFile = mappings.dataResourceMapping(resourceName, jasperReportsContext);
+		if (resourceFile == null)
+		{
+			throw new JRRuntimeException(RepositoryUtil.EXCEPTION_MESSAGE_KEY_INPUT_STREAM_NOT_FOUND,
+					new Object[]{resourceName});
+		}
+		return resourceFile.getName();
+	}
 	
 	protected File resourceMapping(String resourceLocation, JasperReportsContext jasperReportsContext)
 	{
@@ -125,27 +145,10 @@ public class ResourceManager
 
 	protected File copyResource(String resourceLocation, JasperReportsContext jasperReportsContext, File tempFolder)
 	{
-		String resourceName = getResourceName(resourceLocation);
 		try(InputStream input = RepositoryUtil.getInstance(jasperReportsContext).getInputStreamFromLocation(resourceLocation))
 		{
-			File file = File.createTempFile(TEMP_FILE_PREFIX, "_" + resourceName, tempFolder);
-			file.deleteOnExit();//TODO lucianc leak
-
-			if (log.isDebugEnabled())
-			{
-				log.debug("copying " + resourceLocation + " to " + file);
-			}
-
-			byte[] buf = new byte[COPY_BUFFER_SIZE];
-			try (OutputStream output = new FileOutputStream(file))
-			{
-				int read = 0;
-				while ((read = input.read(buf)) > 0)
-				{
-					output.write(buf, 0, read);
-				}
-			}
-
+			File file = createTempFile(resourceLocation, tempFolder);
+			copyToFile(resourceLocation, input, file);
 			return file;
 		}
 		catch (JRException e)
@@ -156,6 +159,46 @@ public class ResourceManager
 			}
 			
 			throw new JRRuntimeException(e);
+		}
+		catch (IOException e)
+		{
+			throw new JRRuntimeException(e);
+		}
+	}
+
+	protected File createTempFile(String resourceLocation, File tempFolder)
+	{
+		try
+		{
+			String resourceName = getResourceName(resourceLocation);
+			File file = File.createTempFile(TEMP_FILE_PREFIX, "_" + resourceName, tempFolder);
+			file.deleteOnExit();//TODO lucianc leak
+			return file;
+		}
+		catch (IOException e)
+		{
+			throw new JRRuntimeException(e);
+		}
+	}	
+
+	protected void copyToFile(String resourceLocation, InputStream data, File file)
+	{
+		try
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("copying " + resourceLocation + " to " + file);
+			}
+
+			byte[] buf = new byte[COPY_BUFFER_SIZE];
+			try (OutputStream output = new FileOutputStream(file))
+			{
+				int read = 0;
+				while ((read = data.read(buf)) > 0)
+				{
+					output.write(buf, 0, read);
+				}
+			}
 		}
 		catch (IOException e)
 		{
@@ -175,6 +218,7 @@ public class ResourceManager
 	protected class ContextMappings
 	{
 		private final ConcurrentMapping<String, File> resourceFiles;
+		private final ConcurrentMapping<String, File> dataResourceFiles;
 		protected final File tempFolder;
 		
 		public ContextMappings(JasperReportsContext jasperReportsContext)
@@ -197,11 +241,39 @@ public class ResourceManager
 			
 			this.resourceFiles = new ConcurrentMapping<>((key, context) 
 					-> copyResource(key, context, tempFolder));
+			
+			this.dataResourceFiles = new ConcurrentMapping<>((resourceName, context)
+					-> createTempFile(resourceName, tempFolder));
 		}
 		
 		public File resourceMapping(String resourceLocation, JasperReportsContext jasperReportsContext)
 		{
 			return resourceFiles.get(resourceLocation, jasperReportsContext);
+		}
+		
+		public File dataResourceMapping(String resourceName, InputStream data, JasperReportsContext jasperReportsContext)
+		{
+			File resourceFile = dataResourceFiles.get(resourceName, jasperReportsContext);
+			synchronized (resourceFile)
+			{
+				if (resourceFile.length() == 0)
+				{
+					copyToFile(resourceName, data, resourceFile);
+				}
+				else
+				{
+					if (log.isDebugEnabled())
+					{
+						log.debug("file " + resourceFile + " already written for " + resourceName);
+					}
+				}
+			}
+			return resourceFile;
+		}
+		
+		public File dataResourceMapping(String resourceName, JasperReportsContext jasperReportsContext)
+		{
+			return dataResourceFiles.get(resourceName, jasperReportsContext);
 		}
 		
 		@Override
@@ -218,6 +290,17 @@ public class ResourceManager
 			}
 			
 			resourceFiles.clear();
+			
+			for (Iterator<File> fileIt = dataResourceFiles.currentValues(); fileIt.hasNext();)
+			{
+				File file = fileIt.next();
+				boolean deleted = file.delete();
+				if (log.isDebugEnabled())
+				{
+					log.debug("deleted " + file + ": " + deleted);
+				}
+			}
+			dataResourceFiles.clear();
 		}
 	}
 	
