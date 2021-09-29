89591d06f1417a9ada215fc67b69ba8ad43f7751
Chris James <chris@landclan.com>
2021-09-29 15:57:35 +0100

    Removed the batch file and added README.md

--- jasperreports/install-landclan.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Installing jasperreports-6.17.0-landclan.jar to local Maven repository...
-call mvn install:install-file -Dfile="dist/jasperreports-6.17.0-landclan.jar" -DpomFile="pom.xml"
-
-echo Updating landclandev01 Maven repository...
-call robocopy "C:\mvn-repository\net\sf\jasperreports\jasperreports\6.17.0-landclan" ^
-  "Y:\mvn-repository\net\sf\jasperreports\jasperreports\6.17.0-landclan" /E
---
+++ jasperreports/landclan/README.md
@@ -0,0 +1,4 @@
+The jar file and the pom must be uploaded to the nexus and then
+the nexus maven-releases repository index will need to be rebuilt.
+
+This can be done from the nexus web UI.
