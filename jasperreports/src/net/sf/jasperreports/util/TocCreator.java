890bc46a1acead61d7ec061bf20c4592b99ea193
Chris James <chris@landclan.com>
2022-01-06 14:55:48 +0000

    Added queues to TocCreator. Version 6.17.0-landclan-15.

--- jasperreports/src/net/sf/jasperreports/util/TocCreator.java
+++ jasperreports/src/net/sf/jasperreports/util/TocCreator.java
@@ -1,6 +1,8 @@
 package net.sf.jasperreports.util;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +11,9 @@ import java.util.Map;
  * This class is used to gather page numbers during report fill time. Each page number is associated to a String key.
  * During collection, key/page number pairs are added. When finished, the TOC data are available for direct retrieval
  * by TOC key.
+ *
+ * There are also a pair of queues that can be used if the key is unavailable, for example when loading the table of
+ * contents in a list (lists do not support evaluation time so the dataset will be run before the TOC is ready).
  */
 public class TocCreator {
 
@@ -16,6 +21,8 @@ public class TocCreator {
     private static final String PAGE_NUMBER = "pageNumber";
 
     private final List<Map<String, Object>> tocEntries = new ArrayList<>();
+    private final Deque<Integer> pageNumberDequeue = new ArrayDeque<>();
+    private final Deque<String> tocKeyDequeue = new ArrayDeque<>();
 
     /**
      * Returns the page number for the given key.
@@ -23,12 +30,32 @@ public class TocCreator {
      * @return
      */
     public Integer getPageNumberForTocKey(String tocKey) {
+
         for (Map<String, Object> tocEntry : this.tocEntries) {
             if (tocKey.equals(tocEntry.get(TOC_KEY))) {
                 return (Integer) tocEntry.get(PAGE_NUMBER);
             }
         }
+
         return null;
+    }
+
+    public Integer removeFirstPageNumber() {
+
+        if (!pageNumberDequeue.isEmpty()) {
+            return pageNumberDequeue.removeFirst();
+        }
+
+        return -1;
+    }
+
+    public String removeFirstTocKey() {
+
+        if (!tocKeyDequeue.isEmpty()) {
+            return tocKeyDequeue.removeFirst();
+        }
+
+        return "<Error: tocKeyDequeue is empty>";
     }
 
     /**
@@ -38,14 +65,20 @@ public class TocCreator {
      * @return
      */
     public String setPageNumberForTocKey(String tocKey, Integer pageNumber) {
+
         tocEntries.add(createTocEntry(tocKey, pageNumber));
+        tocKeyDequeue.add(tocKey);
+        pageNumberDequeue.add(pageNumber);
+
         return tocKey;
     }
 
     private Map<String, Object> createTocEntry(String tocKey, Integer pageNumber) {
+
         Map<String, Object> tocEntry = new HashMap<>();
         tocEntry.put(TOC_KEY, tocKey);
         tocEntry.put(PAGE_NUMBER, pageNumber);
+
         return tocEntry;
     }
 }
