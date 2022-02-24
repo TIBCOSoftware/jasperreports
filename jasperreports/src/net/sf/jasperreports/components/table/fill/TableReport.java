2329590fe274d0c5c1afea9335ea9a8a82f97f47
Teodor Danciu <teodord@users.sourceforge.net>
2022-02-24 11:21:45 +0200

    fix icon label styling in jive table headers

--- jasperreports/src/net/sf/jasperreports/components/table/fill/TableReport.java
+++ jasperreports/src/net/sf/jasperreports/components/table/fill/TableReport.java
@@ -1190,8 +1190,7 @@ public class TableReport implements JRReport
 					
 					componentElement.getPropertiesMap().setProperty(MatcherExporterFilter.PROPERTY_MATCHER_EXPORT_FILTER_KEY, TABLE_HEADER_ICON_LABEL_MATCHER_EXPORT_KEY);
 					
-					JRBaseElement element = (JRBaseElement)frame.getChildren().get(0);
-					element.getPropertiesMap().setProperty(MatcherExporterFilter.PROPERTY_MATCHER_EXPORT_FILTER_KEY, TABLE_HEADER_LABEL_MATCHER_EXPORT_KEY);
+					headerTextElement.getPropertiesMap().setProperty(MatcherExporterFilter.PROPERTY_MATCHER_EXPORT_FILTER_KEY, TABLE_HEADER_LABEL_MATCHER_EXPORT_KEY);
 
 					//frame.getChildren().remove(0);
 					frame.getChildren().add(componentElement);
