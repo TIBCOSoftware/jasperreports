09761861c0a650549732f80aa3ec5dc235bc8f2e
Teodor Danciu <teodord@users.sourceforge.net>
2023-04-12 13:32:15 +0300

    copyright update

--- jasperreports/src/net/sf/jasperreports/engine/fill/DatasetExecution.java
+++ jasperreports/src/net/sf/jasperreports/engine/fill/DatasetExecution.java
@@ -1,6 +1,6 @@
 /*
  * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2019 TIBCO Software Inc. All rights reserved.
+ * Copyright (C) 2001 - 2023 Cloud Software Group, Inc. All rights reserved.
  * http://www.jaspersoft.com
  *
  * Unless you have purchased a commercial license agreement from Jaspersoft,
