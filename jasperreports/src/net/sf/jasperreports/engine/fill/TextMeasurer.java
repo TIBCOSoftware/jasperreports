TIBCOSoftware/jasperreports

commit c6f81fc861e78d5693531d8e64486b5a6af2c24b

fix gh-287, gh-289, tracker #14441, tracker #14466: html markup regressions

jasperreports/src/net/sf/jasperreports/engine/fill/TextMeasurer.java
@@ -577,10 +577,10 @@ public class TextMeasurer implements JRTextMeasurer
 
 		isFirstParagraph = true;
 
-		boolean rendered = true;
+		boolean verticalSpaceRemaining = true;
 		int runLimit = remainingTextStart;
 
-		while (rendered && runLimit < allParagraphs.getEndIndex() && (runLimit = allParagraphs.getRunLimit(JRTextAttribute.HTML_LIST_ATTRIBUTES)) <= allParagraphs.getEndIndex())
+		while (verticalSpaceRemaining && runLimit < allParagraphs.getEndIndex() && (runLimit = allParagraphs.getRunLimit(JRTextAttribute.HTML_LIST_ATTRIBUTES)) <= allParagraphs.getEndIndex())
 		{
 			Map<Attribute,Object> attributes = allParagraphs.getAttributes();
 
@@ -588,44 +588,58 @@ public class TextMeasurer implements JRTextMeasurer
 
 			prepareBullet(context);
 			
-			int tokenPosition = 0;
-			int prevParagraphStart = 0;
-			String prevParagraphText = null;
+			int paragraphStart = 0;
+			boolean lastTokenWasNewline = false;
 
 			String runText = styledText.getText().substring(allParagraphs.getIndex(), runLimit);
 			StringTokenizer tkzer = new StringTokenizer(runText, "\n", true);
 
 			// text is split into paragraphs, using the newline character as delimiter
-			while(tkzer.hasMoreTokens() && rendered) 
+			while(tkzer.hasMoreTokens() && verticalSpaceRemaining) 
 			{
-				String token = tkzer.nextToken();
+				String paragraphText = tkzer.nextToken();
 
-				if ("\n".equals(token))
+				if ("\n".equals(paragraphText))
 				{
-					if (tokenPosition > 0 || context.isListItemStart() || !(context.isListItemEnd() || context.isListStart() || context.isListEnd()))
+					if (lastTokenWasNewline) // the previous newline becomes itself a paragraph when followed by another newline
 					{
-						rendered = renderParagraph(lineWrapper, allParagraphs.getIndex() + prevParagraphStart, prevParagraphText);
+						verticalSpaceRemaining = 
+							renderParagraph(
+								lineWrapper, 
+								allParagraphs.getIndex() + paragraphStart - 1, 
+								null // null paragraphText is the way to render newlines
+								);
 					}
 
-					isFirstParagraph = false;
-					prevParagraphStart = tokenPosition + (tkzer.hasMoreTokens() || tokenPosition == 0 ? 1 : 0);
-					prevParagraphText = null;
+					if (
+						paragraphStart == 0 // this newline is the first character in the first paragraph; when this is true, lastTokenWasNewline was for sure false above
+						|| paragraphStart == allParagraphs.getEndIndex() - 1 // this newline is the last character in the last paragraph; when both this and the lastTokenWasNewline was true above, two newlines are rendered
+						)
+					{
+						verticalSpaceRemaining = 
+							renderParagraph(
+								lineWrapper, 
+								allParagraphs.getIndex() + paragraphStart, 
+								null // null paragraphText is the way to render newlines
+								);
+					}
+					
+					lastTokenWasNewline = true;
 				}
 				else
 				{
-					prevParagraphStart = tokenPosition;
-					prevParagraphText = token;
+					verticalSpaceRemaining = 
+						renderParagraph(
+							lineWrapper, 
+							allParagraphs.getIndex() + paragraphStart, 
+							paragraphText
+							);
+					
+					lastTokenWasNewline = false;
 				}
 
-				tokenPosition += token.length();
-			}
-
-			if (rendered && prevParagraphStart < runText.length())
-			{
-				if (prevParagraphText != null || runLimit == allParagraphs.getEndIndex())
-				{
-					rendered = renderParagraph(lineWrapper, allParagraphs.getIndex() + prevParagraphStart, prevParagraphText);
-				}
+				paragraphStart += paragraphText.length();
+				isFirstParagraph = false;
 			}
 
 			allParagraphs.setIndex(runLimit);

jasperreports/src/net/sf/jasperreports/engine/export/AbstractTextRenderer.java
@@ -77,7 +77,6 @@ public abstract class AbstractTextRenderer
 	protected float lineHeight;
 	protected boolean isMaxHeightReached;
 	protected boolean isFirstParagraph;
-	protected boolean isLastParagraph;
 	protected List<TabSegment> segments;
 	protected int segmentIndex;
 	protected boolean indentFirstLine;
@@ -333,7 +332,6 @@ public abstract class AbstractTextRenderer
 		drawPosX = 0;
 	
 		isMaxHeightReached = false;
-		isLastParagraph = false;
 		
 		//maxFontSizeFinder = MaxFontSizeFinder.getInstance(!JRCommonText.MARKUP_NONE.equals(text.getMarkup()));
 	}
@@ -365,6 +363,9 @@ public abstract class AbstractTextRenderer
 
 			prepareBullet(context, attributes);
 
+			int paragraphStart = 0;
+			boolean lastTokenWasNewline = false;
+
 			String runText = allText.substring(allParagraphs.getIndex(), runLimit);
 			AttributedCharacterIterator runParagraphs = 
 				new AttributedString(
@@ -373,47 +374,49 @@ public abstract class AbstractTextRenderer
 						allParagraphs.getIndex() + runText.length()
 						).getIterator();
 			
-			int tokenPosition = 0;
-			int prevParagraphStart = 0;
-			String prevParagraphText = null;
-			
 			StringTokenizer tkzer = new StringTokenizer(runText, "\n", true);
 
 			// text is split into paragraphs, using the newline character as delimiter
 			while(tkzer.hasMoreTokens() && !isMaxHeightReached) 
 			{
-				String token = tkzer.nextToken();
+				String paragraphText = tkzer.nextToken();
 
-				if ("\n".equals(token))
+				if ("\n".equals(paragraphText))
 				{
-					if (tokenPosition > 0 || context.isListItemStart() || !(context.isListItemEnd() || context.isListStart() || context.isListEnd()))
+					if (lastTokenWasNewline) // the previous newline becomes itself a paragraph when followed by another newline
 					{
-						renderParagraph(runParagraphs, prevParagraphStart, prevParagraphText);
+						renderParagraph(
+							runParagraphs, 
+							paragraphStart - 1, 
+							null // null paragraphText is the way to render newlines
+							);
 					}
 
-					isFirstParagraph = false;
-					isLastParagraph = !tkzer.hasMoreTokens();
-					prevParagraphStart = tokenPosition + (tkzer.hasMoreTokens() || tokenPosition == 0 ? 1 : 0);
-					prevParagraphText = null;
+					if (
+						paragraphStart == 0 // this newline is the first character in the first paragraph  
+						|| paragraphStart == allParagraphs.getEndIndex() - 1 // this newline is the last character in the last paragraph
+						)
+					{
+						renderParagraph(
+							runParagraphs, 
+							paragraphStart, 
+							null // null paragraphText is the way to render newlines
+							);
+					}
+
+					lastTokenWasNewline = true;
 				}
 				else
 				{
-					prevParagraphStart = tokenPosition;
-					prevParagraphText = token;
+					renderParagraph(runParagraphs, paragraphStart, paragraphText);
+
+					lastTokenWasNewline = false;
 				}
 
-				tokenPosition += token.length();
+				paragraphStart += paragraphText.length();
+				isFirstParagraph = false;
 			}
 			
-			if (!isMaxHeightReached && prevParagraphStart < runText.length())
-			{
-				isLastParagraph = true;
-				if (prevParagraphText != null || runLimit == allParagraphs.getEndIndex())
-				{
-					renderParagraph(runParagraphs, prevParagraphStart, prevParagraphText);
-				}
-			}
-
 			allParagraphs.setIndex(runLimit);
 		}
 		
@@ -558,7 +561,7 @@ public abstract class AbstractTextRenderer
 		
 					if (
 						text.getHorizontalTextAlign() == HorizontalTextAlignEnum.JUSTIFIED
-						&& (lineMeasurer.getPosition() < paragraph.getEndIndex() || (isLastParagraph && justifyLastLine))
+						&& (lineMeasurer.getPosition() < paragraph.getEndIndex() || justifyLastLine)
 						)
 					{
 						layout = layout.getJustifiedLayout(availableWidth);

jasperreports/src/net/sf/jasperreports/engine/export/PdfTextRenderer.java
@@ -124,7 +124,7 @@ public class PdfTextRenderer extends AbstractPdfTextRenderer
 				- drawPosY,
 			lineHeight,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
 			0,
-			horizontalAlignment == PdfTextAlignment.JUSTIFIED && (!segment.isLastLine || (isLastParagraph && justifyLastLine)) 
+			horizontalAlignment == PdfTextAlignment.JUSTIFIED && (!segment.isLastLine || justifyLastLine) 
 				? PdfTextAlignment.JUSTIFIED_ALL : horizontalAlignment,
 			text.getRunDirectionValue() == RunDirectionEnum.LTR
 				? TextDirection.LTR : TextDirection.RTL

jasperreports/src/net/sf/jasperreports/engine/export/SimplePdfTextRenderer.java
@@ -180,7 +180,7 @@ public class SimplePdfTextRenderer extends AbstractPdfTextRenderer
 				+ bottomPadding,
 			0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
 			text.getLineSpacingFactor(),
-			horizontalAlignment == PdfTextAlignment.JUSTIFIED && (isLastParagraph && justifyLastLine) 
+			horizontalAlignment == PdfTextAlignment.JUSTIFIED && justifyLastLine 
 				? PdfTextAlignment.JUSTIFIED_ALL : horizontalAlignment,
 			text.getRunDirectionValue() == RunDirectionEnum.LTR
 				? TextDirection.LTR : TextDirection.RTL

jasperreports/src/net/sf/jasperreports/engine/fill/JRFillTextElement.java
@@ -112,7 +112,7 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 	private int textStart;
 	private int textEnd;
 	private boolean isCutParagraphOverflow;
-	private boolean isCutParagraphToContinueInOverflow;
+	private boolean isCutParagraph;
 	private short[] lineBreakOffsets;
 	private String textTruncateSuffix;
 	private String oldRawText;
@@ -478,17 +478,17 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 	/**
 	 *
 	 */
-	protected boolean isCutParagraphToContinueInOverflow()
+	protected boolean isCutParagraph()
 	{
-		return isCutParagraphToContinueInOverflow;
+		return isCutParagraph;
 	}
 		
 	/**
 	 *
 	 */
-	protected void setCutParagraphToContinueInOverflow(boolean isCutParagraphToContinueInOverflow)
+	protected void setCutParagraph(boolean isCutParagraph)
 	{
-		this.isCutParagraphToContinueInOverflow = isCutParagraphToContinueInOverflow;
+		this.isCutParagraph = isCutParagraph;
 	}
 	
 	protected short[] getLineBreakOffsets()
@@ -615,7 +615,7 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 		boolean isOverflow
 		) throws JRException
 	{
-		isCutParagraphOverflow = isCutParagraphToContinueInOverflow;
+		isCutParagraphOverflow = canOverflow() && isCutParagraph;
 		
 		return super.prepare(availableHeight, isOverflow);
 	}
@@ -783,7 +783,7 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 		
 		setTextStart(getTextEnd());
 		setTextEnd(measuredText.getTextOffset());
-		setCutParagraphToContinueInOverflow(canOverflow && measuredText.isParagraphCut());
+		setCutParagraph(measuredText.isParagraphCut());
 		setLineBreakOffsets(measuredText.getLineBreakOffsets());
 		setTextTruncateSuffix(measuredText.getTextSuffix());
 		setLineSpacingFactor(measuredText.getLineSpacingFactor());
@@ -1237,6 +1237,7 @@ public abstract class JRFillTextElement extends JRFillElement implements JRTextE
 			fullText != null
 			&& endIndex < fullText.length()
 			&& HorizontalTextAlignEnum.JUSTIFIED == getHorizontalTextAlign()
+			&& isCutParagraph
 			)
 		{
 			printText.getPropertiesMap().setProperty(JRPrintText.PROPERTY_AWT_JUSTIFY_LAST_LINE, Boolean.TRUE.toString());

jasperreports/src/net/sf/jasperreports/engine/util/JEditorPaneHtmlMarkupProcessor.java
@@ -59,6 +59,8 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 	
 	private Document document;
 	private boolean bodyOccurred = false;
+	private boolean honourBlockElementNewLine = false;
+	private Stack<HTML.Tag> htmlTagStack;
 
 	private Stack<StyledTextListInfo> htmlListStack;
 	private boolean insideLi;
@@ -80,6 +82,7 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 		{
 			JRStyledText styledText = new JRStyledText();
 			
+			htmlTagStack = new Stack<>();
 			htmlListStack = new Stack<>();
 
 			JEditorPane editorPane = new JEditorPane("text/html", srcText);
@@ -113,10 +116,37 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 
 			Object elementName = attrs.getAttribute(AbstractDocument.ElementNameAttribute);
 			Object object = (elementName != null) ? null : attrs.getAttribute(StyleConstants.NameAttribute);
-			if (object instanceof HTML.Tag) 
+			HTML.Tag htmlTag = object instanceof HTML.Tag ? (HTML.Tag) object : null;
+			
+			if (
+				bodyOccurred 
+				&& honourBlockElementNewLine 
+				&& htmlTag != Tag.UL
+				&& htmlTag != Tag.OL
+				&& htmlTag != Tag.LI
+				)
 			{
+				// since the newline introduced by a block element is always at the end of the block element,
+				// it means the block element could not have any nested child element after this newline,
+				// so it it is followed by either a sibling or a sibling of its parent or ancestor;
+				// it is before the processing of this next element that we honour the block element newline
+				// character, but only if the next element is not a list or list item element;
+				// this is because the text measurer and text renderer already treat list and list item styled
+				// text runs as paragraphs, so they start them on new lines;
+				// while the honourBlockElementNewLine flag takes care of skipping newlines introduced by the html
+				// parser at the end of the list or list item block elements, here we need to test again if the
+				// newline should be honoured because it might come from non list elements such as <p> or <div>
+				// and should be ignore if immediately follow by the current list or list item element
+				styledText.append("\n");
+				resizeRuns(styledText.getRuns(), styledText.length(), 1);
+			}
+
+			honourBlockElementNewLine = false;
+			
+			if (htmlTag != null) 
+			{
+				htmlTagStack.push(htmlTag);
 				
-				HTML.Tag htmlTag = (HTML.Tag) object;
 				if (htmlTag == Tag.BODY)
 				{
 					bodyOccurred = true;
@@ -132,6 +162,7 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 					processElement(styledText, element);
 					styledText.addRun(new JRStyledText.Run(new HashMap<>(), startIndex, styledText.length()));
 
+					// a second newline is added in case the <br> tag was not empty, as it is usually used 
 					if (startIndex < styledText.length()) {
 						styledText.append("\n");
 						resizeRuns(styledText.getRuns(), startIndex, 1);
@@ -228,6 +259,34 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 						}
 					}
 					
+					honourBlockElementNewLine = false;
+					
+					if ("\n".equals(chunk)) 
+					{
+						// chunk is a newline char only when the parser processes the end of a block element;
+						// inline elements do not have newline chars at the end and the html parser always ignores
+						// newline chars in text content
+
+						honourBlockElementNewLine = true;
+
+						HTML.Tag grandParentHtmlTag = htmlTagStack.size() >= 3 ? htmlTagStack.get(htmlTagStack.size() - 3) : null;
+						if (
+							grandParentHtmlTag == Tag.UL
+							|| grandParentHtmlTag == Tag.OL
+							|| grandParentHtmlTag == Tag.LI
+							)
+						{
+							// ignoring newline characters introduced by the html parser at the end of list or list item elements,
+							// because they are the only ones which cannot be skipped by the text measurer and text renderer;
+							// this is because the text measurer and the text renderer are responsible for skipping newline
+							// characters that are actually in the text content at the end of the <ul>, <ol> and <li> tags in styled text markup;
+							// if two newline characters would be found at the end of the <ul>, <ol> or <li> tag, with the second one being
+							// introduced by the html parser, then only one of them would get skipped during text measuring and rendering,
+							// which would not match html browser behavior, that is able to ignore <br> tags placed at the end of <ul>, <ol> and <li> tags
+							honourBlockElementNewLine = false;
+						}
+					}
+					
 					if (
 						chunk != null
 						&& !"\n".equals(chunk) 
@@ -268,6 +327,8 @@ public class JEditorPaneHtmlMarkupProcessor extends JEditorPaneMarkupProcessor
 						processElement(styledText, element);
 					}
 				}
+				
+				htmlTagStack.pop();
 			}
 		}
 	}

jasperreports/tests/net/sf/jasperreports/markup/bulletedlist/repo/BulletedListReport.18.html.sha
@@ -1,1 +1,1 @@
-c20e7c60578fd71e8b6e6d9b25c809241f7cffcb
+91a50321a01c66ef72300d3a9756ae25c9b26847

jasperreports/tests/net/sf/jasperreports/markup/bulletedlist/repo/BulletedListReport.18.svg.sha
@@ -1,1 +1,1 @@
-c8d620e888ed61f598cd61a419891ed0395aef85
+a66a31bbdc0438c354f20f205f216d2f8d2f5998

commit 1d6476a1a7b0da69dc8aa4d4a51ec67fbd9a03a3

fix text overflow regression introduced by commit c6f81fc861e78d5693531d8e64486b5a6af2c24b

jasperreports/src/net/sf/jasperreports/engine/fill/TextMeasurer.java
@@ -613,7 +613,7 @@ public class TextMeasurer implements JRTextMeasurer
 
 					if (
 						paragraphStart == 0 // this newline is the first character in the first paragraph; when this is true, lastTokenWasNewline was for sure false above
-						|| paragraphStart == allParagraphs.getEndIndex() - 1 // this newline is the last character in the last paragraph; when both this and the lastTokenWasNewline was true above, two newlines are rendered
+						|| allParagraphs.getIndex() + paragraphStart == allParagraphs.getEndIndex() - 1 // this newline is the last character in the last paragraph; when both this and the lastTokenWasNewline was true above, two newlines are rendered
 						)
 					{
 						verticalSpaceRemaining = 

jasperreports/tests/net/sf/jasperreports/textfields/repo/TextFieldsReport.4.jrpxml.sha
@@ -1,1 +1,1 @@
-6c44e8e2a3803a89975ac8e4bc00befa5b944068
+6d3b047b3a55df82cd8bd8f169ddc912309bf2e5