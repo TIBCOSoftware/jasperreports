TIBCOSoftware/jasperreports

commit ac262cf9b586cd98251d15b5d19bb991f5f66248

directly transfer sublist to virtualized frame
this avoids copying the elements to a different virtualized list in some cases
such as table components

for JRL-1765

jasperreports/src/net/sf/jasperreports/engine/base/ElementStore.java
@@ -24,6 +24,7 @@
 package net.sf.jasperreports.engine.base;
 
 import net.sf.jasperreports.engine.JRPrintElement;
+import net.sf.jasperreports.engine.fill.JRVirtualizationContext;
 
 /**
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
@@ -45,4 +46,6 @@ public interface ElementStore extends VirtualizablePageElements
 	void dispose();
 
 	void updatePage(JRVirtualPrintPage page);
+	
+	void updateContext(JRVirtualizationContext context, JRVirtualPrintPage page);
 }

jasperreports/src/net/sf/jasperreports/engine/base/ElementsBlock.java
@@ -106,6 +106,13 @@ public class ElementsBlock implements JRVirtualizable<VirtualElementsData>, Elem
 		context.updateParent(newContext);
 	}
 
+	@Override
+	public void updateContext(JRVirtualizationContext context, JRVirtualPrintPage page)
+	{
+		this.context = context;
+		this.page = page;
+	}
+
 	private void lockContext()
 	{
 		//FIXME locking the whole context is too much, ideally we'd have a lock for this object only

jasperreports/src/net/sf/jasperreports/engine/base/ElementsBlockList.java
@@ -31,6 +31,7 @@ import org.apache.commons.logging.LogFactory;
 
 import net.sf.jasperreports.engine.JRConstants;
 import net.sf.jasperreports.engine.JRPrintElement;
+import net.sf.jasperreports.engine.fill.JRVirtualizationContext;
 
 /**
  * @author Lucian Chirita (lucianc@users.sourceforge.net)
@@ -70,6 +71,18 @@ public class ElementsBlockList implements ElementStore, Serializable
 			if (block != null)
 			{
 				block.updatePage(page);
+			}
+		}
+	}
+
+	@Override
+	public void updateContext(JRVirtualizationContext context, JRVirtualPrintPage page)
+	{
+		for (ElementsBlock block : blocks)
+		{
+			if (block != null)
+			{
+				block.updateContext(context, page);
 			}
 		}
 	}

jasperreports/src/net/sf/jasperreports/engine/base/VirtualizableElementList.java
@@ -56,7 +56,7 @@ public class VirtualizableElementList extends AbstractList<JRPrintElement> imple
 		initStore(page);
 	}
 	
-	protected VirtualizableElementList(JRVirtualizationContext virtualizationContext, ElementStore store)
+	public VirtualizableElementList(JRVirtualizationContext virtualizationContext, ElementStore store)
 	{
 		this.virtualizationContext = virtualizationContext;
 		this.store = store;
@@ -177,6 +177,14 @@ public class VirtualizableElementList extends AbstractList<JRPrintElement> imple
 	public void updatePage(JRVirtualPrintPage page)
 	{
 		store.updatePage(page);
+	}
+	
+	public ElementStore transferStore(JRVirtualizationContext context, JRVirtualPrintPage page)
+	{
+		ElementStore existingStore = store;
+		initStore(store.getPage());
+		existingStore.updateContext(context, page);
+		return existingStore;
 	}
 	
 	//FIXME implement faster bulk methods such as addAll

jasperreports/src/net/sf/jasperreports/engine/fill/VirtualizableFrame.java
@@ -35,6 +35,7 @@ import net.sf.jasperreports.engine.JRPrintElementContainer;
 import net.sf.jasperreports.engine.JRPrintPage;
 import net.sf.jasperreports.engine.JRPropertiesUtil;
 import net.sf.jasperreports.engine.PrintElementId;
+import net.sf.jasperreports.engine.base.ElementStore;
 import net.sf.jasperreports.engine.base.JRVirtualPrintPage;
 import net.sf.jasperreports.engine.base.VirtualizableElementList;
 import net.sf.jasperreports.engine.util.VirtualizableElementCounter;
@@ -105,9 +106,40 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 	
 	public void fill()
 	{
-		if (virtualizationPageElementSize > 0 && deepSize > virtualizationPageElementSize)
+		PrintElementId frameID = PrintElementId.forElement(frame);
+		boolean virtualized = virtualizationPageElementSize > 0 && deepSize > virtualizationPageElementSize;
+
+		if (elements.size() == 1 && elements.get(0) instanceof OffsetElements)
 		{
-			PrintElementId frameID = PrintElementId.forElement(frame);
+			OffsetElements offsetElements = (OffsetElements) elements.get(0);
+			if (offsetElements.getOffsetX() == 0 && offsetElements.getOffsetY() == 0)
+			{
+				Collection<? extends JRPrintElement> elementsList = offsetElements.getElements();
+				if (elementsList instanceof VirtualizableElementList)
+				{
+					if (log.isDebugEnabled())
+					{
+						log.debug("transferring virtualized list for frame " + frame.getUUID() + " (" + frameID + ")");
+					}
+
+					VirtualizableElementList originalElements = (VirtualizableElementList) offsetElements.getElements();
+					JRVirtualizationContext framesContext = virtualizationContext.getFramesContext();
+					ElementStore elementStore = originalElements.transferStore(framesContext, page);
+					VirtualizableElementList virtualizableList = new VirtualizableElementList(framesContext, elementStore);
+					frame.setElementsList(virtualizableList);
+					framesContext.cacheVirtualizableList(frameID, virtualizableList);
+					return;
+				}
+				else if (!virtualized)
+				{
+					frame.setElementsList(new ArrayList<>(elementsList));
+					return;
+				}
+			}
+		}
+		
+		if (virtualized)
+		{
 			if (log.isDebugEnabled())
 			{
 				log.debug("creating virtualized list for frame " + frame.getUUID() + " (" + frameID + ")");
@@ -120,7 +152,6 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 			framesContext.cacheVirtualizableList(frameID, virtualizableList);
 		}
 		
-		//TODO lucian optimize case of a single subreport with zero offsets (e.g. a table)
 		for (OffsetElementsIterator it = new OffsetElementsIterator(elements); it.hasNext();)
 		{
 			JRPrintElement element = it.next();

jasperreports/src/net/sf/jasperreports/engine/fill/JRVirtualizationContext.java
@@ -506,6 +506,15 @@ public class JRVirtualizationContext implements Serializable, VirtualizationList
 		}
 		
 		@Override
+		protected void visitFrameElements(List<JRPrintElement> elements, Void v)
+		{
+			if (!(elements instanceof VirtualizableElementList))
+			{
+				super.visitFrameElements(elements, v);
+			}
+		}
+		
+		@Override
 		protected void visitElement(JRPrintElement element, Void arg)
 		{
 			if (element instanceof JRTemplatePrintElement)

jasperreports/src/net/sf/jasperreports/engine/fill/OffsetElementsIterator.java
@@ -61,7 +61,7 @@ public class OffsetElementsIterator implements Iterator<JRPrintElement>
 				element = subIterator.next();
 				// remove the element from the sublist.  this helps with virtualized subreport pages 
 				// by releasing the external data and allowing the master page to reuse the storage.
-				subIterator.remove();
+				subIterator.remove();//FIXME quadratic time
 				
 				// apply the offsets
 				setSubOffsets(element);

jasperreports/src/net/sf/jasperreports/engine/util/DeepPrintElementCounter.java
@@ -28,6 +28,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import net.sf.jasperreports.engine.JRPrintElement;
 import net.sf.jasperreports.engine.JRPrintFrame;
+import net.sf.jasperreports.engine.base.VirtualizableElementList;
 
 /**
  * Print element visitor that counts deep elements by recursively visiting
@@ -72,7 +73,8 @@ public class DeepPrintElementCounter extends UniformPrintElementVisitor<AtomicIn
 	protected static int count(Collection<? extends JRPrintElement> elements, 
 			UniformPrintElementVisitor<AtomicInteger> counter)
 	{
-		if (elements == null || elements.isEmpty())
+		if (elements == null || elements.isEmpty()
+				|| elements instanceof VirtualizableElementList)
 		{
 			return 0;
 		}

commit 7b6550f28d24ff7a4200719abb4ea90ee227a6f2

don't virtualize small frames separately
regression since ac262cf9b586cd98251d15b5d19bb991f5f66248

jasperreports/src/net/sf/jasperreports/engine/base/ElementStore.java
@@ -34,6 +34,8 @@ import net.sf.jasperreports.engine.fill.JRVirtualizationContext;
 public interface ElementStore extends VirtualizablePageElements
 {
 	int size();
+	
+	int deepSize();
 
 	JRPrintElement get(int index);
 

jasperreports/src/net/sf/jasperreports/engine/base/ElementsBlock.java
@@ -156,6 +156,12 @@ public class ElementsBlock implements JRVirtualizable<VirtualElementsData>, Elem
 		return size;
 	}
 
+	@Override
+	public int deepSize()
+	{
+		return deepElementCount;
+	}
+
 	public boolean isEmpty()
 	{
 		return size == 0;

jasperreports/src/net/sf/jasperreports/engine/base/ElementsBlockList.java
@@ -137,6 +137,17 @@ public class ElementsBlockList implements ElementStore, Serializable
 	}
 
 	@Override
+	public int deepSize()
+	{
+		int deepSize = 0;
+		for (int idx = 0; idx < blockCount; ++idx)
+		{
+			deepSize += blocks[idx].deepSize();
+		}
+		return deepSize;
+	}
+
+	@Override
 	public JRPrintElement get(int index)
 	{
 		int blockIndex = blockIndex(index);

jasperreports/src/net/sf/jasperreports/engine/base/VirtualizableElementList.java
@@ -102,6 +102,11 @@ public class VirtualizableElementList extends AbstractList<JRPrintElement> imple
 		return store.size();
 	}
 
+	public synchronized int deepSize()
+	{
+		return store.deepSize();
+	}
+
 	@Override
 	public synchronized JRPrintElement set(int index, JRPrintElement element)
 	{

jasperreports/src/net/sf/jasperreports/engine/fill/VirtualizableFrame.java
@@ -57,7 +57,6 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 	private JRVirtualizationContext virtualizationContext;
 	private JRVirtualPrintPage page;
 	private int virtualizationPageElementSize;
-	private boolean virtualizedElements;
 	private int deepSize;
 
 	public VirtualizableFrame(JRTemplatePrintFrame frame, 
@@ -101,7 +100,7 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 		
 		if (elements instanceof VirtualizableElementList)
 		{
-			virtualizedElements = true;
+			deepSize += ((VirtualizableElementList) elements).deepSize();
 		}
 		else
 		{
@@ -115,8 +114,7 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 	public void fill()
 	{
 		PrintElementId frameID = PrintElementId.forElement(frame);
-		boolean virtualized = virtualizationPageElementSize > 0 
-				&& (virtualizedElements || deepSize > virtualizationPageElementSize);
+		boolean virtualized = virtualizationPageElementSize > 0 && deepSize > virtualizationPageElementSize;
 
 		if (elements.size() == 1 && elements.get(0) instanceof OffsetElements)
 		{
@@ -126,18 +124,11 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 				Collection<? extends JRPrintElement> elementsList = offsetElements.getElements();
 				if (elementsList instanceof VirtualizableElementList)
 				{
-					if (log.isDebugEnabled())
+					if (virtualized)
 					{
-						log.debug("transferring virtualized list for frame " + frame.getUUID() + " (" + frameID + ")");
+						transferVirtualizedList((VirtualizableElementList) elementsList, frameID);
+						return;
 					}
-
-					VirtualizableElementList originalElements = (VirtualizableElementList) offsetElements.getElements();
-					JRVirtualizationContext framesContext = virtualizationContext.getFramesContext();
-					ElementStore elementStore = originalElements.transferStore(framesContext, page);
-					VirtualizableElementList virtualizableList = new VirtualizableElementList(framesContext, elementStore);
-					frame.setElementsList(virtualizableList);
-					framesContext.cacheVirtualizableList(frameID, virtualizableList);
-					return;
 				}
 				else if (!virtualized)
 				{
@@ -162,6 +153,20 @@ public class VirtualizableFrame implements JRPrintElementContainer, OffsetElemen
 		}
 		
 		OffsetElementsUtil.transfer(elements, frame::addElement);
+	}
+
+	protected void transferVirtualizedList(VirtualizableElementList originalElements, PrintElementId frameID) 
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug("transferring virtualized list for frame " + frame.getUUID() + " (" + frameID + ")");
+		}
+
+		JRVirtualizationContext framesContext = virtualizationContext.getFramesContext();
+		ElementStore elementStore = originalElements.transferStore(framesContext, page);
+		VirtualizableElementList virtualizableList = new VirtualizableElementList(framesContext, elementStore);
+		frame.setElementsList(virtualizableList);
+		framesContext.cacheVirtualizableList(frameID, virtualizableList);
 	}
 
 	@Override